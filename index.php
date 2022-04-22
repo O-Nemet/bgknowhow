0ddb45b558229b6096aa39a007ce3fa79b34701c
O-Nemet <gh@nemet.online>
2022-04-22 13:13:30 +0200

    Updated intro page

--- index.php
+++ index.php
@@ -9,13 +9,17 @@ include_once('header.php');
         <br><br>
         The game of Hearthstone Battlegrounds is a fascinating one...
         <br><br>
-        I hacked this website together during my latest vacation. Therefore, it is pretty basic, not mobile friendly at all and there will be bugs of course. But I wanted to get it out there and provide the community with some tools that were missing up until now.
+        While playing and watching BG streams I picked up many small tidbits of information that improved me as a player. But with over 80 heros and every always around the corner this strategic insight was kinda fleeting. I wanted a place to store this information, so naturally I set up a Google Doc, and quickly learned that
+        </br><br>
+        If you happen to be totally new to Battlegrounds, I suggest reading about the basics in this excellent <a href="https://hearthstone.fandom.com/wiki/Battlegrounds" target="_blank">introduction article</a>, first.
         <br><br>
-        If you want to engage in a discussion about this site, how to improve it, additional features and stuff like that, consider checking out the discussion board on <a href="https://github.com/O-Nemet/bgknowhow/discussions" target="_blank">https://github.com/O-Nemet/bgknowhow/discussions</a> or join the Competitive Battleground Discord server via <a href="https://discord.gg/BYSY6XnY">https://discord.gg/BYSY6XnY</a>.
+        I hacked this website together during my latest vacation. Therefore, it is pretty basic, not mobile friendly at all and there will be bugs of course. But I wanted to get it out there and provide the community with some tools that were missing up until now. Over time many enhancements will be implemented, but rest assured that all the data you enter today will still be here tomorrow.
         <br><br>
-        If you happen to stumble upon a bug, please be so kind to submit a bug report on <a href="https://github.com/O-Nemet/bgknowhow/issues" target="_blank">https://github.com/O-Nemet/bgknowhow/issues</a>.
+        If you want to engage in a discussion about this site, how to improve it, additional features and stuff like that, consider checking out the <a href="https://github.com/O-Nemet/bgknowhow/discussions" target="_blank">discussion board on GitHub</a> or join the Competitive Battleground Discord server via <a href="https://discord.gg/BYSY6XnY">https://discord.gg/BYSY6XnY</a>.
         <br><br>
-        This page was inspired by <a href="https://mtgjson.com" target="_blank">https://mtgjson.com</a>, <a href="https://hearthstonejson.com" target="_blank">https://hearthstonejson.com</a> and supported by data provided by <a href="https://hearthstone.fandom.com/" target="_blank">https://hearthstone.fandom.com/</a>. A special shoutout to Tyler and RDU, whose streams are always inspiring and informative.
+        If you happen to stumble upon a bug, please be so kind to submit a <a href="https://github.com/O-Nemet/bgknowhow/issues" target="_blank">bug report on GitHub</a>.
+        <br><br>
+        This page was inspired by <a href="https://mtgjson.com" target="_blank">MTGJSON</a>, <a href="https://hearthstonejson.com" target="_blank">HearthstoneJSON</a> and supported by data provided by the <a href="https://hearthstone.fandom.com/" target="_blank">Hearthstone Wiki</a>. A special shoutout to <a href="twitch.tv/tylerootd" target="_blank">Tyler</a> and <a href="twitch.tv/rdulive" target="_blank">Rdu</a>, whose streams are always inspiring and informative.
         <br><br>
         "Have a good battle, friend!"
         <br>
