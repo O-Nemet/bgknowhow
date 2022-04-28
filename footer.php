98454a47c077eb8297809a187f187a2bd0b54e72
O-Nemet <gh@nemet.online>
2022-04-28 13:49:42 +0200

    Removed copyright notice

--- footer.php
+++ footer.php
@@ -3,7 +3,6 @@
         <footer>
             <div style="float: left;"><img src="<?= IMG_PATH ?>/icons/logo_html5.png" width="62" alt="HTML5 logo" title="Produced with HTML5"></div>
             <div style="float: right;"><a href="https://playhearthstone.com/battlegrounds" target="_blank"><img src="<?= IMG_PATH ?>/icons/logo_blizzard-entertainment.png" width="140" alt="Blizzard Entertainment logo" title="For more information about Blizzard Entertainment or any of Blizzards' trademarks or other intellectual property, please visit their website at playhearthstone.com/battlegrounds"></a></div>
-            <small>Copyright @2022 Battlegrounds Know How</small>
             <nav style="margin-top: 10px; margin-bottom: 10px;">
                 <small>
                     <span style="padding-right: 5px; border-right: 1px solid #777777;"><a href="?tos">Terms of Service</a></span>
