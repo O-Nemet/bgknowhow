f298b789f42c5f1c07a7bd36abcbebf87e09b0d1
O-Nemet <gh@nemet.online>
2022-06-03 02:04:30 +0200

    Added placeholder page for custom simulations

---
+++ bgsim/custom.php
@@ -0,0 +1,12 @@
+<?php
+include_once('../header.php');
+?>
+
+    <h2 class="page_title">Simulation: Custom Board</h2>
+    <p>
+        In the future you'll be able to set up any board state and run combat simulations.
+    </p>
+
+<?php
+include_once('../footer.php');
+?>
