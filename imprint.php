f031fcbcf2e1c6285d18ef329e9729a9affe46f2
O-Nemet <gh@nemet.online>
2022-06-04 13:10:01 +0200

    Fixed spelling of website name

--- imprint.php
+++ imprint.php
@@ -2,22 +2,22 @@
 include_once('header.php');
 ?>
 
-<h2 class="page_title">Imprint</h2>
-<p>
-    Under German law we are obligated to publish the following information:
-    <br><br>
-    Accountable for the content according to §5 TMG
-    <br><br>
-    <strong>Battlegrounds Know How</strong>
-    <br><br>
-    Oliver Nemet<br>
-    Am Wasserturm 23<br>
-    24943 Flensburg<br>
-    <br>
-    Fax: +49 (0) 461 59362220<br>
-    E-Mail: <a href="mailto:info@bgknowhow.com">info@bgknowhow.com</a>
-    <br>
-</p>
+    <h2 class="page_title">Imprint</h2>
+    <p>
+        Under German law we are obligated to publish the following information:
+        <br><br>
+        Accountable for the content according to §5 TMG
+        <br><br>
+        <strong>Battlegrounds Know-How</strong>
+        <br><br>
+        Oliver Nemet<br>
+        Am Wasserturm 23<br>
+        24943 Flensburg<br>
+        <br>
+        Fax: +49 (0) 461 59362220<br>
+        E-Mail: <a href="mailto:info@bgknowhow.com">info@bgknowhow.com</a>
+        <br>
+    </p>
 
 <?php
 include_once('footer.php');
