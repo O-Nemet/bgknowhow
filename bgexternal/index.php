efa557532ad53518daaf68a224290843092d0b0d
O-Nemet <gh@nemet.online>
2022-06-04 13:12:44 +0200

    Added exemplary streamer links to external page

--- bgexternal/index.php
+++ bgexternal/index.php
@@ -2,7 +2,7 @@
 include_once('../header.php');
 ?>
 <h2 class="page_title">External Resources</h2>
-<br>
+<h3>Information</h3>
 <div>
     <ul style="list-style-type: disc; margin-left: 50px; font-size: 20px; line-height: 2;">
         <li><a href="https://www.bgcurvesheet.com/" target="_blank">Battlegrounds CheatSheet</a></li>
@@ -10,6 +10,17 @@ include_once('../header.php');
         <li><a href="https://hearthstone.fandom.com/" target="_blank">Hearthstone Wiki</a></li>
         <li><a href="https://hearthstonejson.com" target="_blank">HearthstoneJSON</a></li>
     </ul>
+</div>
+<h3>Streamers</h3>
+<ul style="list-style-type: disc; margin-left: 50px; font-size: 20px; line-height: 2;">
+    <li><a href="https://twitch.tv/rdulive" target="_blank">RDU</a></li>
+    <li><a href="https://twitch.tv/xqn_thesad" target="_blank">XQN</a></li>
+    <li><a href="https://twitch.tv/PockyPlays" target="_blank">Pocky</a></li>
+    <li><a href="https://twitch.tv/tylerootd" target="_blank">Tyler</a></li>
+</ul>
+
+<div>
+
 </div>
 
 <?php
