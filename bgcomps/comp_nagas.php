aa27fd1cba7eff8dad0118f0c0745eeea8d74d29
O-Nemet <gh@nemet.online>
2022-06-04 13:10:46 +0200

    Optimized naga comps

--- bgcomps/comp_nagas.php
+++ bgcomps/comp_nagas.php
@@ -10,13 +10,13 @@ include_once('../header.php');
 <div class="comp_wrapper">
     <p>Spellcraft Comp:</p>
     <?php
-    $board   = ['Leeroy', 'Myrmidon', '*Eventide Brute', 'Athissa', 'Athissa', 'Wrangler', 'Glowscale'];
+    $board   = ['Waverider', 'Myrmidon', '*Eventide Brute', 'Athissa', 'Athissa', 'Glowscale', 'Glowscale'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>Big Stats Comp:</p>
     <?php
-    $board   = ['Athissa', 'Siren', 'Wrangler', 'Orgozoa', 'Orgozoa', 'Glowscale', 'Glowscale'];
+    $board   = ['Athissa', 'Wrangler', 'Wrangler', 'Orgozoa', 'Orgozoa', 'Glowscale', 'Glowscale'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
