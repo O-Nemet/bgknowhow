b985cb9a63637633c2513e8880be29f736e44248
O-Nemet <gh@nemet.online>
2022-06-04 13:23:14 +0200

    Converted the comp index page to an image based selection

---
+++ bgcomps/index.php
@@ -0,0 +1,22 @@
+<?php
+include_once('../header.php');
+?>
+    <h2 class="page_title">Select your minion type</h2>
+
+    <div class="comp_selection">
+        <div><a href="comp_beasts.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_beasts.webp"><span>Beasts</span></a></div>
+        <div><a href="comp_demons.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_demons.webp"><span>Demons</span></a></div>
+        <div><a href="comp_dragons.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_dragons.webp"><span>Dragons</span></a></div>
+        <div><a href="comp_elementals.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_elementals.webp"><span>Elementals</span></a></div>
+        <div><a href="comp_mechs.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_mechs.webp"><span>Mechs</span></a></div>
+        <div><a href="comp_murlocs.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_murlocs.webp"><span>Murlocs</span></a></div>
+        <div><a href="comp_nagas.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_nagas.webp"><span>Nagas</span></a></div>
+        <div><a href="comp_pirates.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_pirates.webp"><span>Pirates</span></a></div>
+        <div><a href="comp_quilboars.php"><img src="<?= PICTURE_LOCAL ?>misc/comps_quilboars.webp"><span>Quilboars</span></a></div>
+    </div>
+
+    <!--    <div class="cf"></div>-->
+
+<?php
+include_once('../footer.php');
+?>
