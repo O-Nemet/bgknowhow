4ea39e9c80fa9887b2706b7dbabca06344eb0d0a
O-Nemet <gh@nemet.online>
2022-06-04 13:16:22 +0200

    Switched intro text to its own subpage and converted the main index page to an image based selection

--- index.php
+++ index.php
@@ -1,32 +1,37 @@
 <?php
 include_once('header.php');
 ?>
+    <h2 class="page_title">Choose your path</h2>
 
-    <h2 class="page_title">Introduction</h2>
-
-    <p class="intro">
-        <b style="font-weight: bold;">"Seven fresh opponents. Are you ready?"</b>
-        <br><br>
-        The game of Hearthstone Battlegrounds is a fascinating one... From the hero selection, the matchup lottery, the tavern rolls all the way to the random attacks, every game of Battlegrounds is a different beast of odds, which we aim to tame.
-        <br>
-        And yet
-        <br><br>
-        While playing and watching BG streams I picked up many small tidbits of information that improved me as a player. But with over 80 heros and every always around the corner this strategic insight was kinda fleeting. I wanted a place to store and find this information quickly. So naturally I set up a Google Doc, and quickly learned the limitations of that approach.
-        </br><br>
-        If you happen to be totally new to Battlegrounds, I suggest reading about the basics in this excellent <a href="https://hearthstone.fandom.com/wiki/Battlegrounds" target="_blank">introduction article</a>, first.
-        <br><br>
-        I hacked this website together during my latest vacation. Therefore, it is pretty basic, not mobile friendly at all and there will be bugs of course. But I wanted to get it out there and provide the community with some tools that were missing up until now. Over time many enhancements will be implemented, but rest assured that all the data you enter today will still be here tomorrow.
-        <br><br>
-        If you want to engage in a discussion about this site, how to improve it, additional features and stuff like that, consider checking out the <a href="https://github.com/O-Nemet/bgknowhow/discussions" target="_blank">discussion board on GitHub</a> or join the Competitive Battleground Discord server via <a href="https://discord.gg/BYSY6XnY">https://discord.gg/BYSY6XnY</a>.
-        <br><br>
-        If you happen to stumble upon a bug, please be so kind to submit a <a href="https://github.com/O-Nemet/bgknowhow/issues" target="_blank">bug report on GitHub</a>.
-        <br><br>
-        This page was inspired by <a href="https://mtgjson.com" target="_blank">MTGJSON</a>, <a href="https://hearthstonejson.com" target="_blank">HearthstoneJSON</a> and supported by data provided by the <a href="https://hearthstone.fandom.com/" target="_blank">Hearthstone Wiki</a>. A special shoutout to <a href="twitch.tv/tylerootd" target="_blank">Tyler</a> and <a href="twitch.tv/rdulive" target="_blank">Rdu</a>, whose streams are always inspiring and informative.
-        <br><br>
-        "Have a good battle, friend!"
-        <br>
-        <i style="font-style: italic">- Oliver aka blade on BNet/Discord</i>
-    </p>
+    <div class="pathes2">
+        <a href="bgstrategy/?show=heroes&mode=gfx">
+            <div id="strategy_heroes"><span>Hero<br>Strategy</span></div>
+        </a>
+        <a href="introduction.php">
+            <div id="intro"><span>What?<br>Why?<br>How?</span></div>
+        </a>
+        <a href="bgstrategy/?show=minions&mode=gfx">
+            <div id="strategy_minions"><span>Minion<br>Strategy</span></div>
+        </a>
+        <a href="bgcurves/index.php">
+            <div id="curves"><span>Curves</span></div>
+        </a>
+        <a href="bgsim/index.php">
+            <div id="simulator"><span>Simulator</span></div>
+        </a>
+        <a href="bgcomps/index.php">
+            <div id="comps"><span>Endgame<br>Comps</span></div>
+        </a>
+        <a href="bgjson/index.php">
+            <div id="bgjson"><span>BGJSON</span></div>
+        </a>
+        <a href="bglegends/index.php">
+            <div id="lobbylegends"><span>Lobby<br>Legends</span></div>
+        </a>
+        <a href="bgexternal/index.php">
+            <div id="external"><span>External<br>Resources</span></div>
+        </a>
+    </div>
 
 <?php
 include_once('footer.php');
