557e1bdcc8e2f721e45e50a1f2b2dc6db49f78a9
O-Nemet <gh@nemet.online>
2022-06-06 15:24:58 +0200

    Enhanced imprint page

--- imprint.php
+++ imprint.php
@@ -13,10 +13,12 @@ include_once('header.php');
         Oliver Nemet<br>
         Am Wasserturm 23<br>
         24943 Flensburg<br>
+        Germany<br>
         <br>
         Fax: +49 (0) 461 59362220<br>
         E-Mail: <a href="mailto:info@bgknowhow.com">info@bgknowhow.com</a>
-        <br>
+        <br><br>
+        So that this was not a totally wasted click, here is my <a href="https://letterboxd.com/Zorc/" target="_blank">Letterboxd profile</a> ;-).
     </p>
 
 <?php
