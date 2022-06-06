fff828b9d364c9eb16743ca6d1abb77c27eae976
O-Nemet <gh@nemet.online>
2022-06-06 15:28:10 +0200

    Switched external links page from text to icon images Optimized lots of CSS formatting

--- bgexternal/index.php
+++ bgexternal/index.php
@@ -3,24 +3,27 @@ include_once('../header.php');
 ?>
 <h2 class="page_title">External Resources</h2>
 <h3>Information</h3>
-<div>
-    <ul style="list-style-type: disc; margin-left: 50px; font-size: 20px; line-height: 2;">
-        <li><a href="https://www.bgcurvesheet.com/" target="_blank">Battlegrounds CheatSheet</a></li>
-        <li><a href="https://hs-tools.github.io/bg-probabilities/" target="_blank">Lii's Hearthstone Battlegrounds Probability Calculator</a></li>
-        <li><a href="https://hearthstone.fandom.com/" target="_blank">Hearthstone Wiki</a></li>
-        <li><a href="https://hearthstonejson.com" target="_blank">HearthstoneJSON</a></li>
+<div class="websites">
+    <ul style="list-style-type: none;">
+        <li><a href="https://www.bgcurvesheet.com/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_bgcheatsheet.jpg" title="Battlegrounds CheatSheet"></a></li>
+        <li><a href="https://hs-tools.github.io/bg-probabilities/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_liis_calculator.png" title="Lii's Hearthstone Battlegrounds Probability Calculator"></a></li>
+        <li><a href="https://hearthstone.fandom.com/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_hearthstone_wiki.webp" title="Hearthstone Wiki"></a></li>
+        <li><a href="https://hearthstonejson.com" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_hsjson.png" title="HearthstoneJSON"></a></li>
+        <!--        <li><a href="https://www.bgcurvesheet.com/" target="_blank">Battlegrounds CheatSheet</a></li>-->
+        <!--        <li><a href="https://hs-tools.github.io/bg-probabilities/" target="_blank">Lii's Hearthstone Battlegrounds Probability Calculator</a></li>-->
+        <!--        <li><a href="https://hearthstone.fandom.com/" target="_blank">Hearthstone Wiki</a></li>-->
+        <!--        <li><a href="https://hearthstonejson.com" target="_blank">HearthstoneJSON</a></li>-->
     </ul>
 </div>
 <h3>Streamers</h3>
-<ul style="list-style-type: disc; margin-left: 50px; font-size: 20px; line-height: 2;">
-    <li><a href="https://twitch.tv/rdulive" target="_blank">RDU</a></li>
-    <li><a href="https://twitch.tv/xqn_thesad" target="_blank">XQN</a></li>
-    <li><a href="https://twitch.tv/PockyPlays" target="_blank">Pocky</a></li>
-    <li><a href="https://twitch.tv/tylerootd" target="_blank">Tyler</a></li>
-</ul>
-
-<div>
-
+<div class="streamers cf">
+    <ul style="list-style-type: none;">
+        <li><a href="https://twitch.tv/rdulive" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_rdu.jpg" title="RDU"></a></li>
+        <li><a href="https://twitch.tv/xqn_thesad" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_xqn.jpg" title="XQN"></a></li>
+        <li><a href="https://twitch.tv/PockyPlays" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_pocky.jpg" title="Pocky"></a></li>
+        <li><a href="https://twitch.tv/benice92" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_benice.jpg" title="BeNice"></a></li>
+        <!--    <li><a href="https://twitch.tv/tylerootd" target="_blank"><img src="--><? //= PICTURE_LOCAL ?><!--misc/streamer_tyler.jpg"></a></li>-->
+    </ul>
 </div>
 
 <?php
