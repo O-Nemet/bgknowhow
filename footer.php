e77615f55d2c22fd81069dc81b334bd74f81af27
O-Nemet <gh@nemet.online>
2022-06-13 00:50:04 +0200

    Added additional css classes to footer

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 </div>
 
 <footer>
-    <div style="float: left;"><img src="<?= IMG_PATH ?>icons/logo_html5_small.webp" width="62" alt="HTML5 logo" title="Produced with HTML5"></div>
-    <div style="float: right;"><a href="https://playhearthstone.com/battlegrounds" target="_blank"><img src="<?= IMG_PATH ?>icons/logo_blizzard-entertainment_small.webp" width="140" alt="Blizzard Entertainment logo" title="For more information about Blizzard Entertainment or any of Blizzards' trademarks or other intellectual property, please visit their website at playhearthstone.com/battlegrounds"></a></div>
+    <div class="logo_footer" style="float: left;"><img src="<?= IMG_PATH ?>icons/logo_html5_small.webp" width="62" alt="HTML5 logo" title="Produced with HTML5"></div>
+    <div class="logo_footer" style="float: right;"><a href="https://playhearthstone.com/battlegrounds" target="_blank"><img src="<?= IMG_PATH ?>icons/logo_blizzard-entertainment_small.webp" width="140" alt="Blizzard Entertainment logo" title="For more information about Blizzard Entertainment or any of Blizzards' trademarks or other intellectual property, please visit their website at playhearthstone.com/battlegrounds"></a></div>
     <nav style="margin-top: 10px; margin-bottom: 10px;">
         <small>
             <span style="padding-right: 5px; border-right: 1px solid #777777;"><a href="?tos">Terms of Service</a></span>
@@ -10,14 +10,12 @@
             <span style="padding-left: 5px;"> <a href="//bgknowhow.com/imprint.php">Imprint</a></span>
         </small>
     </nav>
-    <small style="font-size: 10px;">
+    <small class="legal_text">
         Hearthstone®: Heroes of Warcraft™ ©2014 Blizzard Entertainment, Inc. All rights reserved. Heroes of Warcraft is a trademark, and Hearthstone is a registered trademark of Blizzard Entertainment, Inc. in the U.S. and/or other countries.
         <br>
-                This website is not affiliated with, endorsed, sponsored, or specifically approved by Blizzard Entertainment, Inc. This website may use the trademarks and other intellectual property of Blizzard Entertainment, Inc., which is permitted under Blizzards' fan site policy.
-            </small>
+        This website is not affiliated with, endorsed, sponsored, or specifically approved by Blizzard Entertainment, Inc. This website may use the trademarks and other intellectual property of Blizzard Entertainment, Inc., which is permitted under Blizzards' fan site policy.
+    </small>
+</footer>
 
-        </footer>
-
-    </body>
-
+</body>
 </html>
