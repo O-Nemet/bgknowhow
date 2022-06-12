fb3d2a8ae83ab9b1fc1e859504bc6295c9f9a4e0
O-Nemet <gh@nemet.online>
2022-06-12 12:49:13 +0200

    Updated the Discord invite link Updated formatting and text

--- introduction.php
+++ introduction.php
@@ -4,23 +4,23 @@ include_once('header.php');
     <h2 class="page_title">Introduction</h2>
 
     <p class="intro">
-        <b style="font-weight: bold;">"Seven fresh opponents. Are you ready?"</b>
+        <b style="font-weight: bold;">“Seven fresh opponents. Are you ready?”</b>
         <br><br>
-        The game of Hearthstone Battlegrounds is a fascinating one... From the hero selection, the minion type bannings, the matchup lottery, the tavern rolls all the way to the random attacks, every game of Battlegrounds is a different beast of odds, which all of us players aim to tame.<br>The ebbs and flows of randomness strike us again and again, and yet we can not do anything but return, always hoping to get kissed by lady luck again.
+        The game of Hearthstone Battlegrounds is a fascinating one... From the hero selection, the minion type bans, the matchup lottery, the tavern rolls all the way to the random attacks, every game of Battlegrounds is a different beast of odds, which all of us players aim to tame.<br>The ebbs and flows of randomness strike us again and again, and yet we can not do anything but return, always hoping to get kissed by lady luck again.
         <br><br>
-        While playing and while watching BG streams I picked up many small tidbits of information that improved me as a player. But with over 80 heroes, over 140 minions and the next game always around the corner this strategic insight was kinda fleeting. I wanted a place to store and find this knowledge quickly and a Google Doc was just not enough anymore. Therefore, I did create this website to be a hub of knowledge and know-how.
+        While playing and while watching streams I picked up many small tidbits of information that improved me as a player. But with over 80 heroes, over 160 minions and the next game always around the corner this strategic insight was kinda fleeting. I wanted a place to store and find this knowledge quickly and a Google Doc was just not good enough anymore. Therefore, I did create this website to be a hub of knowledge and know-how.
         <br><br>
         If you happen to be totally new to Battlegrounds, I suggest reading about the basics in this excellent <a href="https://hearthstone.fandom.com/wiki/Battlegrounds" target="_blank">introduction article</a> on Hearthstone Wiki, first. But if you are looking for strategic insight or are already experienced enough to provide your own input about the know-how of any hero or minion head over to the <a href="https://bgknowhow.com/bgstrategy/?show=heroes&mode=gfx">strategy section</a> to read, vote or contribute.
         <br><br>
         I hacked this website together during my latest vacation. Therefore, it is pretty basic, not mobile friendly at all and there will be bugs of course. But I wanted to get it out there and provide the community with some tools that were missing up until now. Over time many enhancements will be implemented, but rest assured that all the data you enter today will still be here tomorrow.
         <br><br>
-        If you want to engage in a discussion about this site, how to improve it, additional features and stuff like that, consider checking out the <a href="https://github.com/O-Nemet/bgknowhow/discussions" target="_blank">discussion board on GitHub</a> or join the Competitive Battleground Discord server via <a href="https://discord.gg/8XtAqHkJ" target="_blank">https://discord.gg/8XtAqHkJ</a>.
+        If you want to engage in a discussion about this site, how to improve it, additional features and stuff like that, consider checking out the <a href="https://github.com/O-Nemet/bgknowhow/discussions" target="_blank">discussion board on GitHub</a> or join the Competitive Battleground Discord server via <a href="https://discord.gg/RUsfdacMwA" target="_blank">https://discord.gg/RUsfdacMwA</a>.
         <br><br>
         One of the key features of this website is the provisioning of data, defining the different Battleground entities (heroes, minions, buddies) and their properties in JSON/CSV format. You can find these files and basic examples of implementation under the <a href="https://bgknowhow.com/bgjson/index.php">BGJSON</a> link. Feel free to use this for any content you'd like to produce. If you do stumble upon a bug, please be so kind to submit a <a href="https://github.com/O-Nemet/bgknowhow/issues" target="_blank">bug report on GitHub</a>.
         <br><br>
-        This project was inspired by <a href="https://mtgjson.com" target="_blank">MTGJSON</a> and supported by data and images provided by <a href="https://hearthstonejson.com" target="_blank">HearthstoneJSON</a> and the <a href="https://hearthstone.fandom.com/" target="_blank">Hearthstone Wiki</a>. Also, a special shoutout to <a href="https://twitch.tv/rdulive" target="_blank">Rdu</a> and <a href="https://twitch.tv/xqn_thesad" target="_blank">XQN</a> , whose streams are always inspiring and informative.
+        This project was inspired by <a href="https://mtgjson.com" target="_blank">MTGJSON</a> and supported by data and images provided by <a href="https://hearthstonejson.com" target="_blank">HearthstoneJSON</a> and the <a href="https://hearthstone.fandom.com/" target="_blank">Hearthstone Wiki</a>. Also, a special shout-out to <a href="https://twitch.tv/rdulive" target="_blank">Rdu</a> and <a href="https://twitch.tv/xqn_thesad" target="_blank">XQN</a>, whose streams are always inspiring and informative.
         <br><br>
-        "Have a good battle, friend!"
+        “Have a good battle, friend!”
         <br>
         <i style="font-style: italic">- Oliver aka blade on BNet/Discord</i>
     </p>
