f02a02d94cde54ac119b3dee46e8cdacb76c2f91
O-Nemet <gh@nemet.online>
2022-06-16 21:49:24 +0200

    Added youtuber section Refactored html from list to div

--- bgexternal/index.php
+++ bgexternal/index.php
@@ -1,29 +1,62 @@
 <?php
 include_once('../header.php');
 ?>
+
 <h2 class="page_title">External Resources</h2>
 <h3>Information</h3>
 <div class="websites">
-    <ul style="list-style-type: none;">
-        <li><a href="https://www.bgcurvesheet.com/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_bgcheatsheet.jpg" title="Battlegrounds CheatSheet"></a></li>
-        <li><a href="https://hs-tools.github.io/bg-probabilities/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_liis_calculator.png" title="Lii's Hearthstone Battlegrounds Probability Calculator"></a></li>
-        <li><a href="https://hearthstone.fandom.com/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_hearthstone_wiki.webp" title="Hearthstone Wiki"></a></li>
-        <li><a href="https://hearthstonejson.com" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_hsjson.png" title="HearthstoneJSON"></a></li>
-        <!--        <li><a href="https://www.bgcurvesheet.com/" target="_blank">Battlegrounds CheatSheet</a></li>-->
-        <!--        <li><a href="https://hs-tools.github.io/bg-probabilities/" target="_blank">Lii's Hearthstone Battlegrounds Probability Calculator</a></li>-->
-        <!--        <li><a href="https://hearthstone.fandom.com/" target="_blank">Hearthstone Wiki</a></li>-->
-        <!--        <li><a href="https://hearthstonejson.com" target="_blank">HearthstoneJSON</a></li>-->
-    </ul>
+    <div>
+        <a href="https://www.bgcurvesheet.com/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_bgcheatsheet.jpg" title="Battlegrounds CheatSheet"></a>
+    </div>
+    <div>
+        <a href="https://hs-tools.github.io/bg-probabilities/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_liis_calculator.png" title="Lii's Hearthstone Battlegrounds Probability Calculator"></a>
+    </div>
+    <div>
+        <a href="https://hearthstone.fandom.com/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_hearthstone_wiki.webp" title="Hearthstone Wiki"></a>
+    </div>
+    <div>
+        <a href="https://hearthstonejson.com" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_hsjson.png" title="HearthstoneJSON"></a>
+    </div>
 </div>
-<h3>Streamers</h3>
-<div class="streamers cf">
-    <ul style="list-style-type: none;">
-        <li><a href="https://twitch.tv/rdulive" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_rdu.jpg" title="RDU"></a></li>
-        <li><a href="https://twitch.tv/xqn_thesad" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_xqn.jpg" title="XQN"></a></li>
-        <li><a href="https://twitch.tv/PockyPlays" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_pocky.jpg" title="Pocky"></a></li>
-        <li><a href="https://twitch.tv/benice92" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_benice.jpg" title="BeNice"></a></li>
-        <!--    <li><a href="https://twitch.tv/tylerootd" target="_blank"><img src="--><? //= PICTURE_LOCAL ?><!--misc/streamer_tyler.jpg"></a></li>-->
-    </ul>
+<h3 class="cf">Streamers</h3>
+<div class="streamers">
+    <div>
+        <a href="https://twitch.tv/rdulive" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_rdu.jpg" title="RDU"></a>
+        <span>Popular Bulgarian streamer. Qualified for LL top48 two times.</span>
+    </div>
+    <div>
+        <a href="https://twitch.tv/xqn_thesad" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_xqn.jpg" title="XQN"></a>
+        <span>Popular Bulgarian streamer. Qualified for LL top48 two times.</span>
+    </div>
+    <div>
+        <a href="https://twitch.tv/PockyPlays" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_pocky.jpg" title="Pocky"></a>
+        <span>y1</span>
+    </div>
+    <div>
+        <a href="https://twitch.tv/benice92" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_benice.jpg" title="BeNice"></a>
+        <span>z</span>
+    </div>
+    <!--    <li><a href="https://twitch.tv/tylerootd" target="_blank"><img src="--><? //= PICTURE_LOCAL ?><!--misc/streamer_tyler.jpg"></a></li>-->
+</div>
+
+<h3 class="cf">Youtubers</h3>
+<div class="streamers">
+    <div>
+        <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/youtube_mewwy.jpg" title="MewwyHS"></a>
+        <span>???</span>
+    </div>
+    <div>
+        <a href="https://www.youtube.com/playlist?list=PL3kmT1mmaRnGdkZGVJbcLfg_oGk32vk2v" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/youtube_kripp.jpg" title="Kripparrian"></a>
+        <span>???</span>
+    </div>
+    <div>
+        <a href="https://www.youtube.com/channel/UCb2EqHXk3x9T71QUtLIEXsw/videos" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/youtube_ninaisnoob.jpg" title="Pocky"></a>
+        <span>???</span>
+    </div>
+    <div>
+        <a href="https://www.youtube.com/playlist?list=PLuChyl5Zve-EFCN8RYwjOkWdhizZ4givw" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/youtube_trolden.jpg" title="Trolden"></a>
+        <span>???</span>
+    </div>
 </div>
 
 <?php
