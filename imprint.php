323d61c5c6dd48d8368a257fe2d80d0b448056fc
O-Nemet <gh@nemet.online>
2022-06-20 16:35:17 +0200

    Added cat pictures to imprint page

--- imprint.php
+++ imprint.php
@@ -18,8 +18,20 @@ include_once('header.php');
         Fax: +49 (0) 461 59362220<br>
         E-Mail: <a href="mailto:info@bgknowhow.com">info@bgknowhow.com</a>
         <br><br>
-        So that this was not a totally wasted click, here is my <a href="https://letterboxd.com/Zorc/" target="_blank">Letterboxd profile</a> ;-).
+        So that this was not a totally wasted click, here is my <a href="https://letterboxd.com/Zorc/" target="_blank">Letterboxd profile</a> and pictures of my cats. ;-)
     </p>
+
+    <div class="cats" style="width: fit-content; margin-top: 65px;">
+        <div>
+            <img src="<?= PICTURE_LOCAL ?>misc/cat_mia2.webp" title="Mia">
+        </div>
+        <div>
+            <img src="<?= PICTURE_LOCAL ?>misc/cat_yuna3.webp" title="Yuna">
+        </div>
+        <div>
+            <img src="<?= PICTURE_LOCAL ?>misc/cat_kiru2.webp" title="Kiru">
+        </div>
+    </div>
 
 <?php
 include_once('footer.php');
