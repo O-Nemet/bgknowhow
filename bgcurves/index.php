c8ba181ed870b93f057a337fda6fd4eb9b38abea
O-Nemet <gh@nemet.online>
2022-06-24 16:00:11 +0200

    Added external curve link.

--- bgcurves/index.php
+++ bgcurves/index.php
@@ -7,6 +7,7 @@ include_once('../header.php');
     <p>
         When to level to the next tavern tier and when to buy minions or utilize your hero power instead is a whole lot of theory on its own and greatly depends on the hero you are playing and also on the cost of its hero power.<br><br>
         The standard/basic curve is going to level 2 on turn 2 (4 gold), going to level 3 on turn 5 (7 gold) and going to level 4 on turn 7 (9 gold). After that, deciding to level to 5 or 6 will heavily depend on your board and even your opponents board.
+        <br><br>For the time being, please refer to the <a href="https://docs.google.com/spreadsheets/d/1Pis44oSp3jXylJnF6Xd55m_ZnWP7pNcmWhpW56CvWM4/edit#gid=0" target="_blank">Battlegrounds Curvesheet by Minder</a>.
         <br><br>More information coming soon&trade;.
     </p>
 </div>
