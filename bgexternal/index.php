7d833a01f5d78ae77619eae458e43260632913be
O-Nemet <gh@nemet.online>
2022-06-24 16:14:13 +0200

    Total restyling/reordering/refactoring of external resource page

--- bgexternal/index.php
+++ bgexternal/index.php
@@ -3,63 +3,142 @@ include_once('../header.php');
 ?>
 
 <h2 class="page_title">External Resources</h2>
-<h3>Information</h3>
-<div class="websites">
-    <div>
-        <a href="https://www.bgcurvesheet.com/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_bgcheatsheet.jpg" title="Battlegrounds CheatSheet"></a>
-    </div>
-    <div>
-        <a href="https://hs-tools.github.io/bg-probabilities/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_liis_calculator.png" title="Lii's Hearthstone Battlegrounds Probability Calculator"></a>
-    </div>
-    <div>
-        <a href="https://hearthstone.fandom.com/" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_hearthstone_wiki.webp" title="Hearthstone Wiki"></a>
-    </div>
-    <div>
-        <a href="https://hearthstonejson.com" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/website_hsjson.png" title="HearthstoneJSON"></a>
-    </div>
-</div>
 <h3 class="cf">Streamers</h3>
 <div class="streamers">
     <div>
-        <a href="https://twitch.tv/rdulive" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_rdu.jpg" title="RDU"></a>
-        <span>Popular Bulgarian streamer. Qualified for LL top48 two times.</span>
+        <a href="https://twitch.tv/rdulive" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/streamer_rdu.jpg" alt="RDU">
+            <span>RDU</span>
+        </a>
+        <p>
+            Longtime and popular HS player from Romania, streaming in english.<br><br>
+            Mainstay in the EU top50.<br><br>
+            Qualified for LL#2 top48, LL#3 top48.
+        </p>
     </div>
     <div>
-        <a href="https://twitch.tv/xqn_thesad" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_xqn.jpg" title="XQN"></a>
-        <span>Popular Bulgarian streamer. Qualified for LL top48 two times.</span>
+        <a href="https://twitch.tv/xqn_thesad" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/streamer_xqn.jpg" alt="XQN">
+            <span>XQN</span>
+        </a>
+        <p>
+            Longtime and high-skilled BG player from Ukraine, streaming in english.<br><br>
+            Mainstay in the EU top20.<br><br>
+            Qualified for LL#1 top24, LL#2 top24, LL#3 top16, LL#4 top16.
+        </p>
     </div>
     <div>
-        <a href="https://twitch.tv/PockyPlays" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_pocky.jpg" title="Pocky"></a>
-        <span>y1</span>
+        <a href="https://twitch.tv/benice92" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/streamer_benice.jpg" alt="BeNice">
+            <span>BeNice</span>
+        </a>
+        <p>
+            Longtime German BG player, streaming in english.<br><br>
+            Mainstay in the EU top100.<br><br>
+            Winner of LL#1.<br>
+        </p>
     </div>
     <div>
-        <a href="https://twitch.tv/benice92" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/streamer_benice.jpg" title="BeNice"></a>
-        <span>z</span>
+        <a href="https://twitch.tv/PockyPlays" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/streamer_pocky.jpg" alt="Pocky">
+            <span>Pocky</span>
+        </a>
+        <p>
+            High MMR streamer from the United States.<br><br>
+            Writer of educational BG guides.
+        </p>
     </div>
     <!--    <li><a href="https://twitch.tv/tylerootd" target="_blank"><img src="--><? //= PICTURE_LOCAL ?><!--misc/streamer_tyler.jpg"></a></li>-->
+    <!--    https://www.twitch.tv/guddummit  russian -->
+    <!--    https://www.twitch.tv/ixxdeee-->
+    <!--    https://www.twitch.tv/beterbabbit us-->
+    <!--    EducatedCollins US-->
 </div>
 
 <h3 class="cf">Youtubers</h3>
 <div class="streamers">
     <div>
-        <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/youtube_mewwy.jpg" title="MewwyHS"></a>
-        <span>???</span>
+        <a href="https://www.youtube.com/playlist?list=PL3kmT1mmaRnGdkZGVJbcLfg_oGk32vk2v" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/youtube_kripp.jpg" alt="Kripparrian">
+            <span>Kripparrian</span>
+        </a>
+        <p>
+            Longtime skilled and very popular US HS player, featuring a variety of (medium-rank) BG videos.<br><br>
+            Also famous for his Diablo and PoE content.
+        </p>
     </div>
     <div>
-        <a href="https://www.youtube.com/playlist?list=PL3kmT1mmaRnGdkZGVJbcLfg_oGk32vk2v" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/youtube_kripp.jpg" title="Kripparrian"></a>
-        <span>???</span>
+        <a href="https://www.youtube.com/playlist?list=PLuChyl5Zve-EFCN8RYwjOkWdhizZ4givw" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/youtube_trolden.jpg" alt="Trolden">
+            <span>Trolden</span>
+        </a>
+        <p>
+            Weekly edited high-quality videos capturing the insanity of the funny and lucky moments in HS.<br><br>
+            Also featuring BG special episodes.
+        </p>
     </div>
     <div>
-        <a href="https://www.youtube.com/channel/UCb2EqHXk3x9T71QUtLIEXsw/videos" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/youtube_ninaisnoob.jpg" title="Pocky"></a>
-        <span>???</span>
+        <a href="https://www.youtube.com/channel/UCb2EqHXk3x9T71QUtLIEXsw/videos" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/youtube_ninaisnoob.jpg" alt="Ninaisnoob">
+            <span>Ninaisnoob</span>
+        </a>
+        <p>
+            High-quality educational and gameplay videos.
+        </p>
     </div>
     <div>
-        <a href="https://www.youtube.com/playlist?list=PLuChyl5Zve-EFCN8RYwjOkWdhizZ4givw" target="_blank"><img src="<?= PICTURE_LOCAL ?>misc/youtube_trolden.jpg" title="Trolden"></a>
-        <span>???</span>
+        <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/youtube_mewwy.jpg" alt="MewwyHS">
+            <span>MewwyHS</span>
+        </a>
+        <p>
+            US based high MMR BG player, producing short educational videos.<br><br>
+            Amongst others a series called Quick Hero Tips.
+        </p>
+    </div>
+</div>
+
+<h3>Websites</h3>
+<div class="websites">
+    <div>
+        <a href="https://www.bgcurvesheet.com/" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/website_bgcheatsheet.jpg" alt="Battlegrounds CheatSheet">
+            <span>BG Curve Sheet</span>
+        </a>
+        <p>
+            Google Docs listing all heroes, their main curve and minion type benefits.<br><br>
+            Also featuring a cheat sheet for all BG curves.<br>
+        </p>
+    </div>
+    <div>
+        <a href="https://hs-tools.github.io/bg-probabilities/" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/website_liis_calculator.png" alt="Lii's Hearthstone Battlegrounds Probability Calculator">
+            <span>BG Calculator</span>
+        </a>
+        <p>
+            Interactive calculator to compute the odds of seeing a specific minion in the tavern.
+        </p>
+    </div>
+    <div>
+        <a href="https://hearthstone.fandom.com/" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/website_hearthstone_wiki.webp" alt="Hearthstone Wiki">
+            <span>HS Wiki</span>
+        </a>
+        <p>
+            A huge wiki covering everything Hearthstone and also featuring several pages about BGs.<br><br>
+        </p>
+    </div>
+    <div>
+        <a href="https://hearthstonejson.com" target="_blank">
+            <img src="<?= PICTURE_LOCAL ?>misc/website_hsjson.png" alt="HearthstoneJSON">
+            <span>HS JSON</span>
+        </a>
+        <p>
+            The Hearthstone JSON project, automatically providing card data and images.<br><br>
+        </p>
     </div>
 </div>
 
 <?php
 include_once('../footer.php');
 ?>
-
