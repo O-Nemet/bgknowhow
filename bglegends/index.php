5a7f6ed90c0b6d986b911f91081267be664b8dc0
O-Nemet <gh@nemet.online>
2022-06-24 16:03:49 +0200

    Added local pictures for lobby legends

--- bglegends/index.php
+++ bglegends/index.php
@@ -3,12 +3,12 @@ include_once('../header.php');
 ?>
 <h2 class="page_title">Lobby Legends Esports</h2>
 <div class="lobby_legends">
-    <img src="https://bnetcmsus-a.akamaihd.net/cms/blog_header/fp/FPFT3WOS07MK1644622822446.jpg">
+    <img src="<?= PICTURE_LOCAL ?>misc/ll_logo.webp" alt="Lobby Legends logo">
     <p>Lobby Legends is a monthly esports series by Blizzard, started in 2022 and supported with a total prize pool of $500,000.<br>
         Basic information can be found on the <a href="https://playhearthstone.com/en-us/esports/programs/battlegrounds" target="_blank">official website</a>.
         <br><br>More information coming soon&trade;.
     </p>
-    <img src="https://bnetcmsus-a.akamaihd.net/cms/gallery/31/3172784T7INF1644622905764.png">
+    <img src="<?= PICTURE_LOCAL ?>misc/ll_prizepool.webp" alt="Lobby Legends prize pool">
 </div>
 
 <?php
