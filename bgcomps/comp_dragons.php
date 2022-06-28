ae4ee8fda9ad2cd873004090d09863e45281ac15
O-Nemet <gh@nemet.online>
2022-06-28 21:13:47 +0200

    Updated dragon comp

--- bgcomps/comp_dragons.php
+++ bgcomps/comp_dragons.php
@@ -10,13 +10,13 @@ include_once('../header.php');
 <div class="comp_wrapper">
     <h3>Tarec/Promo Big Stats Comp:</h3>
     <?php
-    $board   = ['Nadina', 'Tarecgosa', 'Promo-Drake', 'Tarecgosa', 'Promo-Drake', 'Razorgore', 'Prestor'];
+    $board   = ['Atramedes', 'Nadina', 'Tarecgosa', 'Promo-Drake', 'Tarecgosa', 'Promo-Drake', 'Razorgore'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <h3>Kalegcos Battlecry Comp:</h3>
     <?php
-    $board   = ['Bronze Warden', 'Nadina', '*Nightmare Amalgam', 'Kaly', 'Kaly', 'Brann', 'Prestor'];
+    $board   = ['Bronze Warden', 'Nadina', 'Atramedes', '*Nightmare Amalgam', 'Kaly', 'Kaly', 'Brann'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
