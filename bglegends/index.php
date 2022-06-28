136faf48e8155dd895509ed69e3b0f2682554060
O-Nemet <gh@nemet.online>
2022-06-28 21:37:14 +0200

    Added top8 result tables for the first 3 lobby legends

--- bglegends/index.php
+++ bglegends/index.php
@@ -6,8 +6,195 @@ include_once('../header.php');
     <img src="<?= PICTURE_LOCAL ?>misc/ll_logo.webp" alt="Lobby Legends logo">
     <p>Lobby Legends is a monthly esports series by Blizzard, started in 2022 and supported with a total prize pool of $500,000.<br>
         Basic information can be found on the <a href="https://playhearthstone.com/en-us/esports/programs/battlegrounds" target="_blank">official website</a>.
-        <br><br>More information coming soon&trade;.
-    </p>
+    <p>
+    <h3>Lobby Legends #1 (Raid Leaders at 02/03.04.2022) Top 8 after 5 lobbies:</h3>
+    <table class="tier-table">
+        <thead>
+        <tr>
+            <th>Place</th>
+            <th>Player</th>
+            <th>Country</th>
+            <th>Points</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>BeNice / YEPBENIS</td>
+            <td>EU (GER)</td>
+            <td>27.5</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Hof</td>
+            <td>CN</td>
+            <td>19.5</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>KenKen</td>
+            <td>US (CND)</td>
+            <td>18.5</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Curt</td>
+            <td>US (USA)</td>
+            <td>18.0</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>BaboFat</td>
+            <td>EU (GER)</td>
+            <td>17.5</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>EducatedC</td>
+            <td>US (USA)</td>
+            <td>15.5</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>bayiu</td>
+            <td>CN</td>
+            <td>13.0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Ponpata07</td>
+            <td>AP (JPN)</td>
+            <td>10.5</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <h3>Lobby Legends #2 (Noblegarden at 14/15.05.2022) Top 8 after 6 lobbies:</h3>
+    <table class="tier-table">
+        <thead>
+        <tr>
+            <th>Place</th>
+            <th>Player</th>
+            <th>Country</th>
+            <th>Points</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>Maks7K</td>
+            <td>EU (RUS)</td>
+            <td>27.5</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>DaPeeDou</td>
+            <td>AP (TW)</td>
+            <td>24.0</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Yanase</td>
+            <td>AP (JPN)</td>
+            <td>22.0</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Awedragon</td>
+            <td>US (CND)</td>
+            <td>21.5</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>saoKing</td>
+            <td>CN</td>
+            <td>20.5</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>jag-eunsae</td>
+            <td>AP (SK)</td>
+            <td>19.5</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Xixo</td>
+            <td>EU (GER)</td>
+            <td>17.0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Alucard</td>
+            <td>US (BRA)</td>
+            <td>16.0</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <h3>Lobby Legends #3 (Eternal Night at 25/26.06.2022) Top 8 after 6 lobbies:</h3>
+    <table class="tier-table">
+        <thead>
+        <tr>
+            <th>Place</th>
+            <th>Player</th>
+            <th>Country</th>
+            <th>Points</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>Waterloooooo</td>
+            <td>US (USA)</td>
+            <td>27.0</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>DaPeeDou</td>
+            <td>AP (TW)</td>
+            <td>27.5</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Gohan</td>
+            <td>CN</td>
+            <td>24.0</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Valeera</td>
+            <td>CN</td>
+            <td>22.5</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Oliech</td>
+            <td>EU (BEL)</td>
+            <td>20.5</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Kid</td>
+            <td>CN</td>
+            <td>17.5</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>guDDummit</td>
+            <td>EU (RUS)</td>
+            <td>15.0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Keromon</td>
+            <td>AP (JPN)</td>
+            <td>14.0</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <br><br>
+
     <img src="<?= PICTURE_LOCAL ?>misc/ll_prizepool.webp" alt="Lobby Legends prize pool">
 </div>
 
