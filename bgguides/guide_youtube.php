b72e65de6b8c8f99dbda7ae6700b789d7e1af818
O-Nemet <gh@nemet.online>
2022-07-02 19:32:00 +0200

    Updated YouTube page

--- bgguides/guide_youtube.php
+++ bgguides/guide_youtube.php
@@ -11,13 +11,19 @@ include_once('../header.php');
 
     <div class="streamers">
         <div class="iframe_div">
+            <h3>“How to build Demons in Patch 23.6” by <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank">Mewwy</a></h3>
+            <iframe id="ytplayer" type="text/html" width="800" height="450"
+                    src="https://www.youtube.com/embed/mcFUhOEEQGE?autoplay=0&origin=https://bgknowhow.com">
+            </iframe>
+        </div>
+        <div class="iframe_div">
             <h3>“This Update Will Completely Change The Game (Patch 23.6)” by <a href="https://www.youtube.com/c/ItsBen321" target="_blank">ItsBen</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/yNwkOdb_Fog?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
         <div class="iframe_div">
-            <h3>“Full Patch 23.6 Note Review” by <a href="https://www.youtube.com/c/PockyPlays" target="_blank">ninaisnoob</a></h3>
+            <h3>“Full Patch 23.6 Note Review” by <a href="https://www.youtube.com/channel/UCb2EqHXk3x9T71QUtLIEXsw" target="_blank">ninaisnoob</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/QngS4E0oszY?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
