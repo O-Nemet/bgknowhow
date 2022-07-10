895dff6d6c0abf181caf6004615d220491a2bfeb
O-Nemet <gh@nemet.online>
2022-07-10 13:39:52 +0200

    Added meta information to LL paragraphs Updated LL qualifying information Updated LL timer

--- bglegends/index.php
+++ bglegends/index.php
@@ -5,16 +5,26 @@ include_once('../header.php');
 <div class="lobby_legends">
     <img src="<?= PICTURE_LOCAL ?>misc/ll_logo.webp" alt="Lobby Legends logo">
     <p>
-        Lobby Legends is a monthly esports series by Blizzard, started in 2022 and supported with a total prize pool of $500,000.<br>
-        Basic information can be found on the <a href="https://playhearthstone.com/en-us/esports/programs/battlegrounds" target="_blank">official website</a>.
+        Lobby Legends is a monthly esports series by Blizzard, started in 2022 and supported with a total prize pool of $500,000. Basic information can be found on the <a href="https://playhearthstone.com/en-us/esports/programs/battlegrounds" target="_blank">official website</a>.
         <br><br>
-        In theory everyone can qualify for the finals. The first qualification gate is securing a spot in the Top 16 of one of the official <a href="https://playhearthstone.com/en-us/community/leaderboards?region=US&leaderboardId=BG" target="_blank">regional leaderboards</a> at the end of the month.
+        In theory, everyone can qualify for the finals. The first qualification gate is securing a spot in the top 16 of one of the official <a href="https://playhearthstone.com/en-us/community/leaderboards?region=US&leaderboardId=BG" target="_blank">regional leaderboards</a> (Americas, Asia-Pacific or Europe) at the end of the month. Usually 3-4 weeks later a top 48 tournament with the qualified players from all three regions will take place. To advance further, one has to reach top 4 out of 8 (8 players per group) after three rounds of BGs, twice.
         <br><br>
-        The next Lobby Legends will happen on July 9th (top 16 semi-finals) and July 10th (top 8 finals) and will be live-streamed with moderation on the official <a href="https://www.twitch.tv/playhearthstone" target="_blank">HS Twitch channel</a>. Further information can be found on <a href="https://playhearthstone.com/en-gb/esports/tournament/lobby-legends-4/" target="_blank">Blizzards event website</a>
+        After that the LL main event happens, where the 12 remaining players and 4 qualifiers from China will battle it out over two days in the semi-finals (two groups with three rounds each) and the finals (the remaining 8 players will play lobbies until someone has accumulated 20 points and then wins a lobby).
+        <br><br>
+        The next Lobby Legends will happen on July 9th (top 16 semi-finals) and July 10th (top 8 finals), and will be live-streamed with moderation on the official <a href="https://www.twitch.tv/playhearthstone" target="_blank">HS Twitch channel</a>. Further information can be found on <a href="https://playhearthstone.com/en-gb/esports/tournament/lobby-legends-4/" target="_blank">Blizzards event website</a>.
+        <br><br>
         <script src="https://cdn.logwork.com/widget/countdown.js"></script>
-        <a href="https://logwork.com/countdown-73t7" class="countdown-timer" data-style="circles" data-timezone="Europe/Berlin" data-date="2022-07-09 17:00">Lobby Legends #4 Semi-Finals</a>
+        <a href="https://logwork.com/countdown-73ot" class="countdown-timer" data-style="circles" data-timezone="Europe/Berlin" data-date="2022-07-10 17:00" data-background="#8dbdd8">Lobby Legends #4 (“Fire Festival”) Finals</a>
     </p>
+    <br>
+
+    <hr>
+
     <h3>Lobby Legends #1 (“Raid Leaders” at 02/03.04.2022) Top 8 after 5 lobbies:</h3>
+    <p>
+        Patch 22.6.0 with <a href="https://bgknowhow.com/bgstrategy/?show=buddies">buddies</a> plus armor, but without Friend of a Friend was played.
+    </p>
+    <br>
     <p>
         <a href="https://www.youtube.com/watch?v=LAdvFKn7_bQ" target="_blank">YouTube VOD recording of day 1</a>
         <br>
@@ -40,7 +50,7 @@ include_once('../header.php');
         <tr>
             <td>2</td>
             <td>Hof</td>
-            <td>CN</td>
+            <td>CHN</td>
             <td>19.5</td>
         </tr>
         <tr>
@@ -70,7 +80,7 @@ include_once('../header.php');
         <tr>
             <td>7</td>
             <td>bayiu</td>
-            <td>CN</td>
+            <td>CHN</td>
             <td>13.0</td>
         </tr>
         <tr>
@@ -83,6 +93,10 @@ include_once('../header.php');
     </table>
 
     <h3>Lobby Legends #2 (“Noblegarden” at 14/15.05.2022) Top 8 after 6 lobbies:</h3>
+    <p>
+        Patch 23.2.0 with Naga freshly released (and still unerfed), as well as the new key minions <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=211'>Mantid Queen</a>, <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=208'>Leeroy</a>, <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=210'>Darkgaze Elder</a> and <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=209'>Young Murk-Eye</a> was played.
+    </p>
+    <br>
     <p>
         <a href="https://www.youtube.com/watch?v=ax9lhjKrU24" target="_blank">YouTube VOD recording of day 1</a>
         <br>
@@ -126,7 +140,7 @@ include_once('../header.php');
         <tr>
             <td>5</td>
             <td>saoKing</td>
-            <td>CN</td>
+            <td>CHN</td>
             <td>20.5</td>
         </tr>
         <tr>
@@ -151,6 +165,10 @@ include_once('../header.php');
     </table>
 
     <h3>Lobby Legends #3 (“Eternal Night” at 25/26.06.2022) Top 8 after 6 lobbies:</h3>
+    <p>
+        Patch 23.4.3 with rebuffed Naga, new <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=51'>Pyramad</a> and <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=101'>Tarecgosa</a> back on tier 3 was played.
+    </p>
+    <br>
     <p>
         <a href="https://www.twitch.tv/videos/1513204286" target="_blank">Twitch VOD recording of day 1</a>
         <br>
@@ -182,13 +200,13 @@ include_once('../header.php');
         <tr>
             <td>3</td>
             <td>Gohan</td>
-            <td>CN</td>
+            <td>CHN</td>
             <td>24.0</td>
         </tr>
         <tr>
             <td>4</td>
             <td>Valeera</td>
-            <td>CN</td>
+            <td>CHN</td>
             <td>22.5</td>
         </tr>
         <tr>
@@ -200,7 +218,7 @@ include_once('../header.php');
         <tr>
             <td>6</td>
             <td>Kid</td>
-            <td>CN</td>
+            <td>CHN</td>
             <td>17.5</td>
         </tr>
         <tr>
@@ -214,6 +232,76 @@ include_once('../header.php');
             <td>Keromon</td>
             <td>AP (JPN)</td>
             <td>14.0</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <h3>Lobby Legends #4 (“Fire Festival” at 09/10.07.2022) Top 8 after ? lobbies:</h3>
+    <p>
+        Patch 23.6.1 with a 25% chance of Darkmoon Prizes, as well as <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=222'>Kooky</a> but without <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=3'>Chromawing</a> was played.
+    </p>
+    <br>
+    <p>
+        <a href="https://www.twitch.tv/videos/1526886709" target="_blank">Twitch VOD recording of day 1</a>
+    </p>
+    <br>
+    <table class="tier-table">
+        <thead>
+        <tr>
+            <th>Place</th>
+            <th>Player</th>
+            <th>Country</th>
+            <th>Points</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>8</td>
+            <td>Alucard</td>
+            <td>US (BRA)</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Butterfly</td>
+            <td>CHN</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>KidFive</td>
+            <td>CHN</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Satellite</td>
+            <td>AP (JPN)</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>BeterBabbit</td>
+            <td>US (USA)</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Cytro</td>
+            <td>EU (BEL)</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>MXJF</td>
+            <td>CHN</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Ribapusa</td>
+            <td>AP (KOR)</td>
+            <td>0</td>
         </tr>
         </tbody>
     </table>
