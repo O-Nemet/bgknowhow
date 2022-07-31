0634b7507fecc853a864bee2333aafd59a4974c9
O-Nemet <gh@nemet.online>
2022-07-31 16:04:57 +0200

    Added links for minion pool and tavern tier to the minion distribution table Updated some texts

--- bgstrategy/general.php
+++ bgstrategy/general.php
@@ -5,12 +5,12 @@ include_once('../header.php');
 
     <h3 id="tiers">Tavern Tiers and upgrading</h3>
     <p>
-        The cost to upgrade to the next tavern tier will reduce by 1 gold at the beginning of each of your turns. For example if you have upgraded to tavern tier 3 the cost to upgrade to tier 4 will be 8 gold. After you enter the next fight and then reenter the tavern shop the cost to upgrade will be reduced to 7 gold.
+        The cost to upgrade to the next tavern tier will reduce by 1 gold at the beginning of each of your turns. For example if you have upgraded to tavern tier 2 the cost to upgrade to tier 3 will be 7 gold. After you enter the next fight and then reenter the tavern shop the cost to upgrade will be reduced to 6 gold.
         <!-- Notice that the cost for tavern tier 5 is 11 gold, which is more than the gold cap. So other than with a couple of special heroes it is not possible to level from 4 to 6 in one turn. -->
         <br><br>
         When upgrading to tier 2, tier 4 and tier 6 the number of minions offered per roll in the tavern will increase by 1.
         <br><br>
-        The higher the tier, the fewer copies of each minion will be available in the minion pool shared by all players. For example each tier 6 minion is only available 7 times. This includes most ways of discoveries, but not the HP of <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/heroes/?id=66">Tess</a> and <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/heroes/?id=57">Scabbs</a>.
+        The higher the tier, the fewer copies of each minion will be available in the minion pool shared by all players. For example each tier 6 minion is only available 7 times. So if you fight someone who has a golden tier 6 minion, there will only be a maximum of 4 copies left for pickup in the tavern. This includes most ways of discoveries, but not the hero powers of <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/heroes/?id=66">Tess</a> and <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/heroes/?id=57">Scabbs</a> who will generate additional minions. Minions sold during the game will be readded to the shared pool (golden minions will be reverted to three normal ones).
     </p>
     <br>
     <table class="tier-table">
@@ -77,7 +77,7 @@ foreach ($tempMinions->data as $key => $object) {
 ?>
     <h3 id="types">Minion types and distribution</h3>
     <p>
-        The different minion types are not equally distributed across the six tavern tiers. For example Murlocs only have one tier 6 minion. Even on tier 1, where each type used to have two minions, Elementals have access to three minions, since the addition of <a class="hoverimage" href='https://bgknowhow.com/bgstrategy/minion/?id=216'>Bubblette</a>. Also notice the neutral minion <a class="hoverimage" href='https://bgknowhow.com/bgstrategy/minion/?id=17'>Wrath Weaver</a>, which is the third minion in the Demon pool on tier 1.
+        The different minion types are not equally distributed across the six tavern tiers. For example Murlocs only have one tier 6 minion. Even on tier 1, where each type used to have two minions, Elementals and Beasts have access to three minions, since the addition of <a class="hoverimage" href='https://bgknowhow.com/bgstrategy/minion/?id=216'>Bubblette</a> and <a class="hoverimage" href='https://bgknowhow.com/bgstrategy/minion/?id=220'>Silverback Patriarch</a>. Also notice the neutral minion <a class="hoverimage" href='https://bgknowhow.com/bgstrategy/minion/?id=17'>Wrath Weaver</a>, which is the third minion in the Demon pool on tier 1.
         <br><br>
         The table below is automatically generated and therefore always up to date and accurate:
     </p>
@@ -90,24 +90,24 @@ foreach ($tempMinions->data as $key => $object) {
         </tr>
         <tr>
             <th>Tavern Tier</th>
-            <th>Beasts</th>
-            <th>Demons</th>
-            <th>Dragons</th>
-            <th>Elementals</th>
-            <th>Mechs</th>
-            <th>Murlocs</th>
-            <th>Naga</th>
-            <th>Pirates</th>
-            <th>Quilboars</th>
-            <th>None</th>
-            <th>Total</th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=beast">Beasts</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=demon">Demons</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=dragon">Dragons</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=elemental">Elementals</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=mech">Mechs</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=murloc">Murlocs</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=naga">Naga</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=pirates">Pirates</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=quilboar">Quilboars</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions&type=none">None</a></th>
+            <th><a href="//bgknowhow.com/bgstrategy/?show=minions">Total</a></th>
         </tr>
         </thead>
         <tbody>
         <?php
         for ($i = 1; $i <= 7; $i++) {
             echo "<tr>";
-            echo "    <td>" . ($i == 7 ? 'Total' : $i) . "</td>";
+            echo "    <td>" . ($i == 7 ? '<a href="//bgknowhow.com/bgstrategy/?show=minions">Total</a>' : '<a href="//bgknowhow.com/bgstrategy/?show=minions&tier=' . $i . '">' . $i . '</a>') . "</td>";
             echo "    <td>" . $minion_count['Beast'][$i] . " (" . ($minion_pool['Beast'][$i] - $minion_count['Beast'][$i]) . ")</td>";
             echo "    <td>" . $minion_count['Demon'][$i] . " (" . ($minion_pool['Demon'][$i] - $minion_count['Demon'][$i]) . ")</td>";
             echo "    <td>" . $minion_count['Dragon'][$i] . " (" . ($minion_pool['Dragon'][$i] - $minion_count['Dragon'][$i]) . ")</td>";
@@ -151,7 +151,7 @@ foreach ($tempMinions->data as $key => $object) {
     <div style="margin-top: -30px;">
         <a href="https://bgknowhow.com/bgstrategy/minion/?id=135"><img width="200" src="https://bgknowhow.com/images/minions/BGS_008_render_80.webp" style="float: left; margin-left: 40px; margin-right: 30px;"></a>
         <p style="width: 920px; padding-top: 90px; text-align: justify;">
-            This beast is a potent lategame minion. Therefore, it is advisable to know the odds of getting a <a class="hoverimage" href='https://bgknowhow.com/bgstrategy/minion/?id=208'>Leeroy</a> or any of the other possible Deathrattle minions. There are currently <?= $i ?> Deathrattle minions in the game, but the Coiler can not spawn itself. Therefore, the odds for a specific minion from coiler are <?= number_format(1 / ($i - 1) * 100, 2) ?>%. You get two chances independent of each other, so for example the odds of getting exactly one Leeroy from one Coiler is <?= number_format(1 / ($i - 1) * 100 * 2, 2) ?>%. Whereas the odds of getting two Leeroys is as low as <?= number_format((1 / ($i - 1) * 2) * (1 / ($i - 1) * 2) * 100, 2) ?>%.
+            This beast is a potent lategame minion. Therefore, it is advisable to know the odds of getting a <a class="hoverimage" href='https://bgknowhow.com/bgstrategy/minion/?id=208'>Leeroy</a> or any of the other possible Deathrattle minions. There are currently <?= $i ?> Deathrattle minions in the game, but the Ghastcoiler can not spawn itself. Therefore, the odds for a specific summon from Coiler are <?= number_format(1 / ($i - 1) * 100, 2) ?>%. You get two chances independent of each other, so for example the odds of getting exactly one Leeroy from one Coiler is <?= number_format(1 / ($i - 1) * 100 * 2, 2) ?>%. Whereas the odds of getting two Leeroys is as low as <?= number_format((1 / ($i - 1) * 2) * (1 / ($i - 1) * 2) * 100, 2) ?>%.
         </p>
     </div>
     <div class="comp_wrapper cf">
@@ -167,7 +167,7 @@ foreach ($tempMinions->data as $key => $object) {
         <p style="width: 920px; padding-top: 20px; text-align: justify;">
             This endgame minion will always have Poisonous, but what will take it from a good investment to a great one is the combination with Divine Shield, enabling it to take out two of your opponents biggest minions while only needing one slot on your board. If you are playing this on a board with only minions from one type or neutral ones, the odds of getting the elusive shield are a measly 25% (as there are 4 random buffs to choose from).
             <br><br>
-            In order to increase the odds you can either place more different minions types on your board (not always an option, depending on your general strategy) or buff the Queen with Taunt and/or Windfury (because if it has either of those at the beginning of combat that option will be ignored for the random roll). All but the +5/+5 buff can only be applied once, therefore the maximum chance with only one minion type will be 50% for a fully buffed Queen. Whereas adding a second minion type will bring the odds of an unbuffed Queen to nearly 48%, and adding a third to nearly 68%.
+            In order to increase the odds you can either place more different minions types on your board (not always an option, depending on your general strategy) or buff the Queen with Taunt and/or Windfury (because if it has either of those at the beginning of combat that option will be ignored for the random roll). All but the +5/+5 buff can only be applied once, therefore the maximum chance for getting a DS with only one minion type will be 50%, for a fully buffed Queen. Whereas adding a second minion type will bring the odds of an unbuffed Queen to nearly 48%, and adding a third to nearly 68%.
         </p>
     </div>
 
