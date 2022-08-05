f08f462ddde3834d09716a8e1cb2f6b5128eecd6
O-Nemet <gh@nemet.online>
2022-08-05 17:59:01 +0200

    Added Twitch Rivals 2022 tourney information

--- bglegends/index.php
+++ bglegends/index.php
@@ -5,18 +5,18 @@ include_once('../header.php');
 <div class="lobby_legends">
     <img src="<?= PICTURE_LOCAL ?>misc/ll_logo.webp" alt="Lobby Legends logo">
     <p>
-        Lobby Legends is a monthly esports series by Blizzard, started in 2022 and supported with a total prize pool of $500,000. Basic information can be found on the <a href="https://playhearthstone.com/en-us/esports/programs/battlegrounds" target="_blank">official website</a>.
+        Lobby Legends is a monthly esports series by Blizzard, started in 2022 and supported with a total prize pool of $500,000. Basic information can be found on the <a href="https://playhearthstone.com/esports/programs/battlegrounds" target="_blank">official website</a>.
         <br><br>
-        In theory, everyone can qualify for the finals. The first qualification gate is securing a spot in the top 16 of one of the official <a href="https://playhearthstone.com/en-us/community/leaderboards?region=US&leaderboardId=BG" target="_blank">regional leaderboards</a> (Americas, Asia-Pacific or Europe) at the end of the month. Usually 3-4 weeks later a top 48 tournament with the qualified players from all three regions will take place. To advance further, one has to reach top 4 out of 8 (8 players per group) after three rounds of BGs, twice.
+        In theory, everyone can qualify for the finals. The first qualification gate is securing a spot in the top 16 of one of the official <a href="https://playhearthstone.com/community/leaderboards?region=US&leaderboardId=BG" target="_blank">regional leaderboards</a> (Americas, Asia-Pacific or Europe) at the end of the month. Usually 3-4 weeks later a top 48 tournament with the qualified players from all three regions will take place. To advance further, one has to reach top 4 out of 8 (8 players per group) after three rounds of BGs, twice.
         <br><br>
         After that the LL main event happens, where the 12 remaining players and 4 qualifiers from China will battle it out over two days in the semi-finals (two groups with three rounds each) and the finals (the remaining 8 players will play lobbies until someone has accumulated 20 points and then wins a lobby).
         <br><br>
-        The next Lobby Legends Top 48 qualifier will happen on August 6th. There will be no official broadcast but it will be live-streamed by several of the players on their Twitch channels. Further information can be found on <a href="https://playhearthstone.com/en-gb/esports/tournament/lobby-legends-5/" target="_blank">Blizzards event website</a>.
+        The next Lobby Legends top 48 qualifier will happen on August 6th. There will be no official broadcast, but it will be live-streamed by several of the players on their Twitch channels. All streamers will have to obey a 30 minute delay. Further information can be found on <a href="https://playhearthstone.com/en-gb/esports/tournament/lobby-legends-5/" target="_blank">Blizzards event website</a>.
         <br><br>
         <!--        The next Lobby Legends will happen on September 3rd (top 16 semi-finals) and September 4th (top 8 finals), and will be live-streamed with moderation on the official <a href="https://www.twitch.tv/playhearthstone" target="_blank">HS Twitch channel</a>. Further information can be found on <a href="https://playhearthstone.com/en-gb/esports/tournament/lobby-legends-4/" target="_blank">Blizzards event website</a>.-->
         <!--        <br><br>-->
         <script src="https://cdn.logwork.com/widget/countdown.js"></script>
-        <a href="https://logwork.com/countdown-73ot" class="countdown-timer" data-style="circles" data-timezone="Europe/Berlin" data-date="2022-08-06 09:00" data-background="#8dbdd8">Lobby Legends #5 Top 48</a>
+        <a href="https://logwork.com/countdown-73ot" class="countdown-timer" data-style="circles" data-timezone="Europe/Berlin" data-date="2022-08-06 17:30" data-background="#8dbdd8">Lobby Legends #5 Top 48</a>
     </p>
     <br>
 
@@ -312,7 +312,86 @@ include_once('../header.php');
 
     <br><br>
 
-    <img src="<?= PICTURE_LOCAL ?>misc/ll_prizepool.webp" alt="Lobby Legends prize pool">
+    <!--    <img src="--><? //= PICTURE_LOCAL ?><!--misc/ll_prizepool.webp" alt="Lobby Legends prize pool">-->
+    <img src="<?= PICTURE_LOCAL ?>misc/tr_players.webp" alt="Twitch Rivals Invitational players">
+
+    <h3>Twitch Rivals 2022 (“Thijs's HS Tavern” at 16.07.2022) Top 8 after 6 lobbies:</h3>
+    <p>
+        At TwitchCon Amsterdam the finals of this 24 player invitational tourney with a total prize pool of $20,000 took place.<br>
+        Patch 23.6.2 with a 25% chance of Darkmoon Prizes, as well as <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=222'>Kooky</a> but without <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=3'>Chromawing</a> was played.
+    </p>
+    <br>
+    <p>
+        <a href="https://www.youtube.com/watch?v=WlBHbOR_T8w" target="_blank">YouTube recap and mood piece about the BG tournament</a>
+        <br>
+        <a href="https://www.twitch.tv/videos/1533369737?t=0h15m26s" target="_blank">Twitch VOD recording including most of the first 5 lobbies of the top8</a>
+        <br>
+        <a href="https://www.twitch.tv/videos/1533343023?t=7h23m35s" target="_blank">Twitch VOD recording of the final lobby</a>
+    </p>
+    <br>
+    <table class="tier-table">
+        <thead>
+        <tr>
+            <th>Place</th>
+            <th>Player</th>
+            <th>Country</th>
+            <th>Points</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>Zorgo</td>
+            <td>EU (GER)</td>
+            <td>36</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Xixo</td>
+            <td>EU (GER)</td>
+            <td>33</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>EducatedCollins</td>
+            <td>US (USA)</td>
+            <td>28</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>DeathItself</td>
+            <td>EU (CRO)</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>ThefishouGO</td>
+            <td>EU (FRA)</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Shadybunny</td>
+            <td>EU (BEL)</td>
+            <td>26</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Sorren</td>
+            <td>EU (ITA)</td>
+            <td>24</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Lii</td>
+            <td>US (USA)</td>
+            <td>15</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <br><br>
+
 </div>
 
 <?php
