9916192c5b4179d730761d8d77f0e04bfd17c968
O-Nemet <gh@nemet.online>
2022-08-17 08:20:15 +0200

    Update of images after patch 24.0.3

--- images/get_images.php
+++ images/get_images.php
@@ -5,34 +5,34 @@ require_once('../functions.php');
 $getActiveOnly = 1;
 
 // generate heroes files
-//if ($stmt = $mysqli->prepare("SELECT bgh.id,
-//                                     bgh.name,
-//                                     bgh.health,
-//                                     bgh.id_blizzard,
-//                                     bgh.hp_id_blizzard,
-//                                     bgh.flag_active
-//                                FROM bg_heroes bgh
-//                               WHERE bgh.flag_active = ?
-//                                 AND bgh.id_blizzard IN ('BG23_HERO_304')
-//                            ORDER BY bgh.name ASC")) {
-//    $stmt->bind_param("i", $getActiveOnly);
-//    $stmt->execute();
-//    $stmt->store_result();
-//    $stmt->bind_result($id, $name, $health, $blizzardId, $blizzardIdHp, $isActive);
-
-// generate minions files
-if ($stmt = $mysqli->prepare("SELECT bgm.id,
-                                     bgm.name,
-                                     bgm.name_short,
-                                     bgm.id_blizzard
-                                FROM bg_minions bgm
-                               WHERE bgm.flag_active = ?
-                                 AND bgm.id_blizzard IN ('BGS_037')
-                            ORDER BY bgm.tier, bgm.name ASC")) {
+if ($stmt = $mysqli->prepare("SELECT bgh.id,
+                                     bgh.name,
+                                     bgh.health,
+                                     bgh.id_blizzard,
+                                     bgh.hp_id_blizzard,
+                                     bgh.flag_active
+                                FROM bg_heroes bgh
+                               WHERE bgh.flag_active = ?
+                                 AND bgh.hp_id_blizzard IN ('TB_BaconShop_HP_049', 'BG20_HERO_242p', 'TB_BaconShop_HP_061', 'TB_BaconShop_HP_066', 'TB_BaconShop_HP_047', 'BG21_HERO_030p')
+                            ORDER BY bgh.name ASC")) {
     $stmt->bind_param("i", $getActiveOnly);
     $stmt->execute();
     $stmt->store_result();
-    $stmt->bind_result($id, $name, $nameShort, $blizzardId);
+    $stmt->bind_result($id, $name, $health, $blizzardId, $blizzardIdHp, $isActive);
+
+// generate minions files
+//if ($stmt = $mysqli->prepare("SELECT bgm.id,
+//                                     bgm.name,
+//                                     bgm.name_short,
+//                                     bgm.id_blizzard
+//                                FROM bg_minions bgm
+//                               WHERE bgm.flag_active = ?
+//                                 AND bgm.id_blizzard IN ('BG21_024', 'BGS_100', 'BGS_121', 'BG21_027', 'BGS_124')
+//                            ORDER BY bgm.tier, bgm.name ASC")) {
+//    $stmt->bind_param("i", $getActiveOnly);
+//    $stmt->execute();
+//    $stmt->store_result();
+//    $stmt->bind_result($id, $name, $nameShort, $blizzardId);
 
 // generate buddies files
 //if ($stmt = $mysqli->prepare("SELECT bgb.id,
@@ -52,33 +52,33 @@ if ($stmt = $mysqli->prepare("SELECT bgm.id,
 
     while ($stmt->fetch()) {
         // hero powers
-//        if (!copy(PICTURE_URL_RENDER . $blizzardIdHp . '.png', '../images/' . $blizzardIdHp . '_render.png')) {
-//            echo 'failed to copy ../images/' . $blizzardIdHp . '_render.png<br>';
-//        } else {
-//            echo 'copy success ../images/' . $blizzardIdHp . '_render.png<br>';
-//        }
+        if (!copy(PICTURE_URL_RENDER . $blizzardIdHp . '.png', '../images/' . $blizzardIdHp . '_render.png')) {
+            echo 'failed to copy ../images/' . $blizzardIdHp . '_render.png<br>';
+        } else {
+            echo 'copy success ../images/' . $blizzardIdHp . '_render.png<br>';
+        }
 
         // minions
-        if (!copy(PICTURE_URL_RENDER_BG . $blizzardId . '.png', '../images/' . $blizzardId . '_render.png')) {
-            echo 'failed to copy ../images/' . $blizzardId . '_render.png<br>';
-        } else {
-            echo 'copy success ../images/' . $blizzardId . '_render.png<br>';
-        }
-        if (!copy(PICTURE_URL_BIG . $blizzardId . '.webp', '../images/' . $blizzardId . '_big.webp')) {
-            echo 'failed to copy ../images/' . $blizzardId . '_big.webp<br>';
-        } else {
-            echo 'copy success ../images/' . $blizzardId . '_big.webp<br>';
-        }
-        if (!copy(PICTURE_URL_MEDIUM . $blizzardId . '.webp', '../images/' . $blizzardId . '_medium.webp')) {
-            echo 'failed to copy ../images/' . $blizzardId . '_medium.webp<br>';
-        } else {
-            echo 'copy success ../images/' . $blizzardId . '_medium.webp<br>';
-        }
-        if (!copy(PICTURE_URL_TILE . $blizzardId . '.webp', '../images/' . $blizzardId . '_tile.webp')) {
-            echo 'failed to copy ../images/' . $blizzardId . '_tile.webp<br>';
-        } else {
-            echo 'copy success ../images/' . $blizzardId . '_tile.webp<br>';
-        }
+//        if (!copy(PICTURE_URL_RENDER_BG . $blizzardId . '.png', '../images/' . $blizzardId . '_render.png')) {
+//            echo 'failed to copy ../images/' . $blizzardId . '_render.png<br>';
+//        } else {
+//            echo 'copy success ../images/' . $blizzardId . '_render.png<br>';
+//        }
+//        if (!copy(PICTURE_URL_BIG . $blizzardId . '.webp', '../images/' . $blizzardId . '_big.webp')) {
+//            echo 'failed to copy ../images/' . $blizzardId . '_big.webp<br>';
+//        } else {
+//            echo 'copy success ../images/' . $blizzardId . '_big.webp<br>';
+//        }
+//        if (!copy(PICTURE_URL_MEDIUM . $blizzardId . '.webp', '../images/' . $blizzardId . '_medium.webp')) {
+//            echo 'failed to copy ../images/' . $blizzardId . '_medium.webp<br>';
+//        } else {
+//            echo 'copy success ../images/' . $blizzardId . '_medium.webp<br>';
+//        }
+//        if (!copy(PICTURE_URL_TILE . $blizzardId . '.webp', '../images/' . $blizzardId . '_tile.webp')) {
+//            echo 'failed to copy ../images/' . $blizzardId . '_tile.webp<br>';
+//        } else {
+//            echo 'copy success ../images/' . $blizzardId . '_tile.webp<br>';
+//        }
 
     }
 
