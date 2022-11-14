f6d6d5ec46922cd839541de4c18d5bc7710cc271
O-Nemet <gh@nemet.online>
2022-11-14 23:55:04 +0100

    Updated video selection

--- bgguides/guide_youtube.php
+++ bgguides/guide_youtube.php
@@ -11,6 +11,18 @@ include_once('../header.php');
 
     <div class="streamers">
         <div class="iframe_div">
+            <h3>“Rating All the New Quests in Hearthstone Battlegrounds” by <a href="https://www.youtube.com/c/ItsBen321" target="_blank">ItsBen</a></h3>
+            <iframe id="ytplayer" type="text/html" width="800" height="450"
+                    src="https://www.youtube.com/embed/bGpy6p0O8c4?autoplay=0&origin=https://bgknowhow.com">
+            </iframe>
+        </div>
+        <!--        <div class="iframe_div">-->
+        <!--            <h3>“Quest Tierlist from Rank 1 EU” by <a href="https://www.youtube.com/channel/UCSL9YkndjEsAm0Oix3tKBTQ" target="_blank">Zorgo</a></h3>-->
+        <!--            <iframe id="ytplayer" type="text/html" width="800" height="450"-->
+        <!--                    src="https://www.youtube.com/embed/EerCBmqlv9w?autoplay=0&origin=https://bgknowhow.com">-->
+        <!--            </iframe>-->
+        <!--        </div>-->
+        <div class="iframe_div">
             <h3>“Wondrous Wisdomball Quest Reward Guide” by <a href="https://www.youtube.com/c/PockyPlays" target="_blank">Pocky</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/xU0PhK_ueKk?autoplay=0&origin=https://bgknowhow.com">
@@ -22,12 +34,6 @@ include_once('../header.php');
                     src="https://www.youtube.com/embed/AX2fZysv5_8?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
-        <div class="iframe_div">
-            <h3>“Quest Tier List for Hearthstone Battlegrounds Patch 24.2.x” by <a href="https://www.youtube.com/c/ItsBen321" target="_blank">ItsBen</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/ohv8rfo50ns?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
         <!--        <div class="iframe_div">-->
         <!--            <h3>“Quick Hero Tips: Ysera (Patch 24.0)” by <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank">Mewwy</a></h3>-->
         <!--            <iframe id="ytplayer" type="text/html" width="800" height="450"-->
@@ -35,9 +41,9 @@ include_once('../header.php');
         <!--            </iframe>https://www.youtube.com/watch?v=03Of9mmbFuU-->
         <!--        </div>-->
         <div class="iframe_div">
-            <h3>“Discussing ALL The Quests (Patch 24.2.2)” by <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank">Mewwy</a></h3>
+            <h3>“Choosing Between a Tier-5 or Tier-6 Discover” by <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank">Mewwy</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/KWK8lV1HzUM?autoplay=0&origin=https://bgknowhow.com">
+                    src="https://www.youtube.com/embed/z8VjkdXThcg?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
         <div class="iframe_div">
@@ -59,6 +65,12 @@ include_once('../header.php');
             </iframe>
         </div>
         <div class="iframe_div">
+            <h3>“How to play perfect Omega Buster Exodia” by <a href="https://www.youtube.com/c/PockyPlays" target="_blank">Pocky</a></h3>
+            <iframe id="ytplayer" type="text/html" width="800" height="450"
+                    src="https://www.youtube.com/embed/PfCFK3Uxvec?autoplay=0&origin=https://bgknowhow.com">
+            </iframe>
+        </div>
+        <div class="iframe_div">
             <h3>“3 on 3 and 4 on 4 Curves Explained” by <a href="https://www.youtube.com/channel/UCb2EqHXk3x9T71QUtLIEXsw" target="_blank">ninaisnoob</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/a3-CZBI5-IY?autoplay=0&origin=https://bgknowhow.com">
@@ -68,12 +80,6 @@ include_once('../header.php');
             <h3>“How to play the best heroes” by <a href="https://www.youtube.com/c/ItsBen321" target="_blank">ItsBen</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/vMdHL72jB08?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
-        <div class="iframe_div">
-            <h3>“How to play perfect Omega Buster Exodia” by <a href="https://www.youtube.com/c/PockyPlays" target="_blank">Pocky</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/PfCFK3Uxvec?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
         <!--        <div class="iframe_div">-->
