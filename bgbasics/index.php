3b92e0696df60fd310bb27b98fdf62a2258a944a
O-Nemet <gh@nemet.online>
2023-02-20 14:08:14 +0100

    Added page for paragraphs about the most basic topics/info (WIP)

--- bgbasics/index.php
+++ bgbasics/index.php
@@ -1,15 +1,60 @@
 <?php
 include_once('../header.php');
 ?>
-<h2 class="page_title">Hero Armor Tiers</h2>
-<br>
+<h2 class="page_title">Basic Definitions</h2>
 <div>
+    <h3>Attacks</h3>
     <p>
-        <br><br>More information coming soon&trade;.
+        In the combat phase the left-most minion of one player will attack a random minion on the opponents board. On a full board that means your attacking minion has a 1:7 (or 14.29%) chance to attack a specific minion of your opponent. This luck-factor can become very relevant when 'sniping' a minion like <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=117">Mama Bear</a> or <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=256">Titus</a> will decide over the outcome of the match.
+        <br><br>
+        After the attack has concluded the left-most minion of the player who did not attack, will begin its attack. This loop will continue until all minions on one side of the board have been killed.
+    </p>
+    <h3>Initiative</h3>
+    <p>
+        Which player will attack first is decided by the number of minions on each side at the beginning of combat. If you control more minions than your opponent your left-most minion will attack first, followed by your opponents left-most minion, after your attack has concluded. If both players control the same amount of minions at the beginning of combat, a hidden coinflip will decide who gets to attack first. It is usually an advantage to attack first, but there are some corner cases where it could be beneficial to go down from 7 to 6 minions to force your opponent to attack first.
+    </p>
+    <h3>Taunt</h3>
+    <p>
+        As a defensive measure to protect your valuable creatures providing in-combat buffs (e.g. <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=117">Mama Bear</a>) you can play minions with taunt or buff any of your minions via <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=221">Sparring Partner</a> or <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=250">Vigilant Stoneborn</a>. A basic strategy is to position a big taunt minion to the right side of your board, so that it attacks as late as possible and will block most of your opponents minions.
+        <br><br>
+        Another thing to keep in mind with taunt minions occurs when facing minions with Cleave (e.g. <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=76">Cave Hydra</a>).
+    </p>
+    <h3>Cleave</h3>
+    <p>
+    </p>
+    <h3>Windfury</h3>
+    <p>
+        A minion with Windfury will attack a second time, if it survived its first attack. Multiple instances of Windfury do not increase the number of attacks. Tripling a Windfury minion will upgrade it to Mega-Windfury, meaning it will attack four times in total (as long as it survives the previous attacks).
+    </p>
+    <h3>Divine Shield</h3>
+    <p>
+    </p>
+    <h3>Deathrattle</h3>
+    <p>
+    </p>
+    <h3>Reborn</h3>
+    <p>
+    </p>
+    <h3>Avenge</h3>
+    <p>
+    </p>
+    <h3>Cleave</h3>
+    <p>
+    </p>
+    <h3>Battlecry</h3>
+    <p>
+    </p>
+    <h3>Spellcraft</h3>
+    <p>
+    </p>
+    <h3>Damage Cap</h3>
+    <p>
+    </p>
+    <h3>Fighting 'Deadguy'</h3>
+    <p>
     </p>
 </div>
 
 <?php
 include_once('../footer.php');
 ?>
-
