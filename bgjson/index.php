b5f7492251e5216793811b69e4f4549b52bcfc23
O-Nemet <gh@nemet.online>
2023-03-25 16:28:11 +0100

    Updated JSON/CSV patch history Reactivated buddy files

--- bgjson/index.php
+++ bgjson/index.php
@@ -25,7 +25,7 @@ include_once('../header.php');
         <br>
         These datasets have several advantages over the data available from the Blizzard API or the Hearthstone JSON files:<br>
         For example <em>nameShort</em> attaches the short name the community uses to reference the entity (e.g. '<a class="hoverimage" href='https://bgknowhow.com/bgstrategy/hero/?id=131'>Eliza</a>' instead of '<a class="hoverimage" href='https://bgknowhow.com/bgstrategy/hero/?id=131'>Dread Admiral Eliza</a>'), <em>pictureSmall</em> references a webp version up to ten times smaller than the original image, while <em>websites</em> includes an array of links to the entity representation on the biggest HS community hubs.
-        For minions, <em>abilities</em> includes an array of boolean key/value pairs, indicating skills like Battlecry or Taunt, and for heroes the <em>armorTier</em> as well as the <em>picturePortrait</em> are part of the data collection.<br>
+        For minions, <em>abilities</em> includes an array of boolean key/value pairs, indicating keywords like Battlecry or Taunt, and for heroes the <em>armorTier</em> as well as the <em>picturePortrait</em> are part of the data collection.<br>
         <br>
         At least every 4 hours fresh files will be generated from the database.<br>
         Official updates and hotfixes will be integrated as soon as possible.<br>
@@ -47,9 +47,10 @@ include_once('../header.php');
         <tr>
             <td><a href="//bgknowhow.com/bgjson/output/bg_heroes_active.json"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Heroes</a></td>
             <td><a href="//bgknowhow.com/bgjson/output/bg_minions_active.json"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Minions</a></td>
-            <td>&nbsp;</td>
-            <td><a href="//bgknowhow.com/bgjson/output/bg_quests_active.json"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Quests</a></td>
-            <td><a href="//bgknowhow.com/bgjson/output/bg_rewards_active.json"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Rewards</a></td>
+            <td><a href="//bgknowhow.com/bgjson/output/bg_buddies_active.json"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> All BG Buddies</a></td>
+            <!--            <td>&nbsp;</td>-->
+            <!--            <td><a href="//bgknowhow.com/bgjson/output/bg_quests_active.json"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Quests</a></td>-->
+            <!--            <td><a href="//bgknowhow.com/bgjson/output/bg_rewards_active.json"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Rewards</a></td>-->
         </tr>
         </tbody>
     </table>
@@ -90,9 +91,10 @@ include_once('../header.php');
         <tr>
             <td><a href="//bgknowhow.com/bgjson/output/bg_heroes_active.csv"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Heroes</a></td>
             <td><a href="//bgknowhow.com/bgjson/output/bg_minions_active.csv"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Minions</a></td>
-            <td>&nbsp;</td>
-            <td><a href="//bgknowhow.com/bgjson/output/bg_quests_active.csv"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Quests</a></td>
-            <td><a href="//bgknowhow.com/bgjson/output/bg_rewards_active.csv"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Rewards</a></td>
+            <td><a href="//bgknowhow.com/bgjson/output/bg_buddies_active.csv"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Buddies</a></td>
+            <!--            <td>&nbsp;</td>-->
+            <!--            <td><a href="//bgknowhow.com/bgjson/output/bg_quests_active.csv"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Quests</a></td>-->
+            <!--            <td><a href="//bgknowhow.com/bgjson/output/bg_rewards_active.csv"><img src="//bgknowhow.com/images/icons/download_icon.png" alt="Download File"> Active BG Rewards</a></td>-->
         </tr>
         </tbody>
     </table>
@@ -123,7 +125,20 @@ include_once('../header.php');
     <p class="caption"><u>Latest Changes:</u></p>
     <br>
     <ul id="latest_changes">
-        <li>15.01.2023 - Added new hero and new minions of the upcoming BG season 3 release (isActive flag as false, of course).</li>
+        <li>23.03.2023 - Added BG changes from the <a href="https://hearthstone.blizzard.com/en-us/news/23921500/25-6-2-patch-notes" target="_blank">25.6.2</a> patch notes.</li>
+        <li>18.03.2023 - Added armor changes from the <a href="https://us.forums.blizzard.com/en/hearthstone/t/2561-patch-notes/104261" target="_blank">25.6.1</a> patch notes.</li>
+        <li>17.03.2023 - Added "armor" and "armorHighMMR" fields to, and removed "armorTier" field from the hero dataset.</li>
+        <li>15.03.2023 - Added BG changes from the <a href="https://hearthstone.blizzard.com/en-us/news/23921496/25-6-patch-notes" target="_blank">25.6.0</a> patch notes.</li>
+        <li>02.03.2023 - Added BG changes from the <a href="https://hearthstone.blizzard.com/en-us/news/23921492/25-4-3-patch-notes" target="_blank">25.4.3</a> patch notes.</li>
+        <li>17.02.2023 - Added armor changes from the <a href="https://us.forums.blizzard.com/en/hearthstone/t/2541-patch-notes/102926" target="_blank">25.4.1</a> patch notes.</li>
+        <li>13.02.2023 - Added BG changes from the <a href="https://hearthstone.blizzard.com/en-us/news/23913671/25-4-patch-notes" target="_blank">25.4.0</a> patch notes.</li>
+        <li>03.02.2023 - Added "types" and "pools" fields to the JSON format in order to support dual type minions.</li>
+        <li>26.01.2023 - Added BG changes from the <a href="https://hearthstone.blizzard.com/en-us/news/23892228/25-2-2-patch-notes" target="_blank">25.2.2</a> patch notes.</li>
+        <li>22.01.2023 - Added BG changes from the <a href="https://us.forums.blizzard.com/en/hearthstone/t/2521-patch-notes/101487" target="_blank">25.2.1</a> patch notes.</li>
+        <li>18.01.2023 - Temporarily deactivated Scabbs, Tess, Tamsin, Illidan and the minion Murozond (will be bugfixed by Blizzard).</li>
+        <li>18.01.2023 - Added dual minion types/pool (Type2 and Pool2 for CSV format).</li>
+        <li>17.01.2023 - Added BG changes from the <a href="https://hearthstone.blizzard.com/en-us/news/23892225/25-2-patch-notes" target="_blank">25.2.0</a> patch notes.</li>
+        <li>15.01.2023 - Added new hero and new minions of the upcoming BG season 3 release (isActive = false, until patchday).</li>
         <li>19.12.2022 - Added BG changes from the <a href="https://hearthstone.blizzard.com/en-us/news/23892223/25-0-4-patch-notes" target="_blank">25.0.4</a> patch notes.</li>
         <li>29.11.2022 - Added BG changes from the <a href="https://hearthstone.blizzard.com/en-us/news/23882860/25-0-patch-notes" target="_blank">25.0.0</a> patch notes.</li>
         <li>19.11.2022 - Added quest and reward datasets (finally).</li>
