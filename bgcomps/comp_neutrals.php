a382b93a97d3e635918f74151a21996a35906cc0
O-Nemet <github@nemet.online>
2023-05-08 21:41:09 +0200

    Added Fireworks Fanatic Tier4 composition Optimized Tea Master composition

--- bgcomps/comp_neutrals.php
+++ bgcomps/comp_neutrals.php
@@ -8,15 +8,21 @@ include_once('../header.php');
 </p>
 <br>
 <div class="comp_wrapper">
+    <h3>Big Stats Fireworks Fanatic Tier4 Comp:</h3>
+    <?php
+    $board = ['Hydra', '*Bristleback Knight', 'Blanchy', 'Warden', 'Fanatic', 'Fanatic', 'Ball'];
+    $minions = getMinionsForBoard($board);
+    drawBoard($minions);
+    ?>
     <h3>Big Stats Tea Master/Menagerie Comp:</h3>
     <?php
-    $board   = ['Hydra', '*Bristleback Knight', 'Blanchy', 'Warden', 'Theo', 'Theo', 'Queen'];
+    $board = ['Reaper', '*Bristleback Knight', 'Blanchy', 'Queen', 'Theo', 'Theo', 'Glowscale'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <h3>Big Stats Fort/Taunt Comp:</h3>
     <?php
-    $board   = ['Queen', 'Blanchy', 'Queen', 'Glowscale', 'Fort', 'Fort', 'Ball'];
+    $board = ['Queen', 'Blanchy', 'Queen', 'Glowscale', 'Fort', 'Fort', 'Ball'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
