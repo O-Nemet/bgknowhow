4f0c833611129b0cbef55769dbbb6c02d273dab4
O-Nemet <github@nemet.online>
2023-05-08 21:44:15 +0200

    Completed text for Deadguy Removed Mega-Windfury information

--- bgbasics/index.php
+++ bgbasics/index.php
@@ -11,27 +11,55 @@ include_once('../header.php');
     </p>
     <h3 id="initiative">Initiative</h3>
     <p>
-        Which player will attack first is decided by the number of minions on each side at the beginning of combat. If you control more minions than your opponent your left-most minion will attack first, followed by your opponents left-most minion, after your attack has concluded. If both players control the same amount of minions at the beginning of combat, a hidden coinflip will decide who gets to attack first. It is usually an advantage to attack first, but there are some corner cases where it could be beneficial to go down from 7 to 6 minions to force your opponent to attack first.
+        Which player will attack first is decided by the number of minions on each side at the beginning of combat. If
+        you control more minions than your opponent your left-most minion will attack first, followed by your opponents
+        left-most minion, after your attack has concluded. If both players control the same amount of minions at the
+        beginning of combat, a hidden coinflip will decide who gets to attack first. It is usually an advantage to
+        attack first, but there are some corner cases where it could be beneficial to go down from 7 to 6 minions to
+        force your opponent to attack first.
     </p>
     <h3 id="taunt">Taunt</h3>
     <p>
-        As a defensive measure to protect your valuable creatures providing in-combat buffs (e.g. <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=117">Mama Bear</a>) you can play minions with taunt or buff any of your minions via <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=221">Sparring Partner</a> or <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=250">Vigilant Stoneborn</a>. A basic strategy is to position a big taunt minion to the right side of your board, so that it attacks as late as possible and will block most of your opponents minions.
+        As a defensive measure to protect your valuable creatures providing in-combat buffs (e.g. <a class="hoverimage"
+                                                                                                     href="https://bgknowhow.com/bgstrategy/minion/?id=117">Mama
+            Bear</a>) you can play minions with taunt or buff any of your minions via <a class="hoverimage"
+                                                                                         href="https://bgknowhow.com/bgstrategy/minion/?id=221">Sparring
+            Partner</a> or <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=250">Vigilant
+            Stoneborn</a>. A basic strategy is to position a big taunt minion to the right side of your board, so that
+        it attacks as late as possible and will block most of your opponents minions.
         <br><br>
-        Another thing to keep in mind with taunt minions occurs when facing minions with cleave (e.g. <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=76">Cave Hydra</a>).
+        Another thing to keep in mind with taunt minions occurs when facing minions with cleave (e.g. <a
+            class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=76">Cave Hydra</a>).
     </p>
     <h3 id="windfury">Windfury</h3>
     <p>
-        A minion with Windfury will attack a second time, if it survived its first attack. Multiple instances of Windfury do not increase the number of attacks. Tripling a Windfury minion will upgrade it to Mega-Windfury, meaning it will attack four times in total (as long as it survives the previous attacks).
+        A minion with Windfury will attack a second time, if it survived its first attack. Multiple instances of
+        Windfury do not increase the number of attacks.
         <br><br>
-        As Windfury is an offensive ability only, it is usually best to place a minion with Windfury to the left to increase the chance that it can attack twice before being killed by an attack itself. Also notice, that Windfury only provides value if the minion is able to survive its first attack. Therefore, the value of Windfury increases on minions with high health or a Divine Shield.
+        As Windfury is an offensive ability only, it is usually best to place a minion with Windfury to the left to
+        increase the chance that it can attack twice before being killed by an attack itself. Also notice, that Windfury
+        only provides value if the minion is able to survive its first attack. Therefore, the value of Windfury
+        increases on minions with high health or a Divine Shield.
     </p>
     <h3 id="shield">Divine Shield</h3>
     <p>
-        Minions with a divine shield will not get damaged the first time they are hit during each combat (the shield will refresh between combats). This provides the opportunity to trade 2 for 1 if the shielded minion is big enough or to protect a key minion from dying too early.
+        Minions with a divine shield will not get damaged the first time they are hit during each combat (the shield
+        will refresh between combats). This provides the opportunity to trade 2 for 1 if the shielded minion is big
+        enough or to protect a key minion from dying too early.
         <br><br>
-        Out of the box only 9 minions in total come with shields but there a different means to buff any minion with a shield (primarily via <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=203">Glowscale</a> or <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=73">Annoy-o-Module</a>).
+        Out of the box only 9 minions in total come with shields but there a different means to buff any minion with a
+        shield (primarily via <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=203">Glowscale</a>
+        or <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=73">Annoy-o-Module</a>).
         <br><br>
-        While usually the positioning of a DS minion is less important because the shield works on offense as well as on defense you should still try to position against what your opponent has. The best counter against a shielded minion is a <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=208">Leeroy</a>, as he will kill any minion in one hit, shield or no shield. But even a small taunt minion like a <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=272">Risen Rider</a> can be played as a blocker to waste your shield. If your opponent is featuring several shielded minions instead of just one huge one you should consider picking up a <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=266">Blaster</a> and putting it in first position.
+        While usually the positioning of a DS minion is less important because the shield works on offense as well as on
+        defense you should still try to position against what your opponent has. The best counter against a shielded
+        minion is a <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=208">Leeroy</a>, as he will
+        kill any minion in one hit, shield or no shield. But even a small taunt minion like a <a class="hoverimage"
+                                                                                                 href="https://bgknowhow.com/bgstrategy/minion/?id=272">Risen
+            Rider</a> can be played as a blocker to waste your shield. If your opponent is featuring several shielded
+        minions instead of just one huge one you should consider picking up a <a class="hoverimage"
+                                                                                 href="https://bgknowhow.com/bgstrategy/minion/?id=266">Blaster</a>
+        and putting it in first position.
     </p>
     <h3 id="deathrattle">Deathrattle</h3>
     <p>
@@ -41,11 +69,22 @@ include_once('../header.php');
     </p>
     <h3 id="cleave">Cleave</h3>
     <p>
-        Cleave is short for the text "Also damages the minions next to whomever this attacks." and is featured on only two minions, namely <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=76">Cave Hydra</a> and <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=133">Foe Reaper 4000</a>. There are also <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=224">Wildfire Elemental</a> (will only cleave when killing its target and only to one side until golden) and <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=224">Atramedes</a> (with a fixed cleave-damage of 3), as honorable mentions.
+        Cleave is short for the text "Also damages the minions next to whomever this attacks." and is featured on only
+        two minions, namely <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=76">Cave Hydra</a>
+        and <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=133">Foe Reaper 4000</a>. There are
+        also <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/minion/?id=224">Wildfire Elemental</a> (will
+        only cleave when killing its target and only to one side until golden) and <a class="hoverimage"
+                                                                                      href="https://bgknowhow.com/bgstrategy/minion/?id=224">Atramedes</a>
+        (with a fixed cleave-damage of 3), as honorable mentions.
         <br><br>
-        That is most likely because it is one of the strongest offensive abilities in the game, with the potential to trade your one minion for up to three of your opponent minions. Because cleave has no defensive benefit you usually want to position your cleave minions to the left, so they can attack before getting killed. This also means that the health of a cleave-minion is way less important than its attack.
+        That is most likely because it is one of the strongest offensive abilities in the game, with the potential to
+        trade your one minion for up to three of your opponent minions. Because cleave has no defensive benefit you
+        usually want to position your cleave minions to the left, so they can attack before getting killed. This also
+        means that the health of a cleave-minion is way less important than its attack.
         <br><br>
-        A countermeasure to cleaving minions is clever positioning and taunt minions (for example if you place a minion with taunt on the outer right position, a cleaving attacker will only hit two of your minions). So be aware of lobbies featuring the above-mentioned minions and the picks or discoveries of your opponents.
+        A countermeasure to cleaving minions is clever positioning and taunted minions (for example if you place a
+        minion with taunt on the outer right position, a cleaving attacker will only hit two of your minions). So be
+        aware of lobbies featuring the above-mentioned minions and the picks or discoveries of your opponents.
     </p>
     <h3 id="avenge">Avenge</h3>
     <p>
@@ -64,20 +103,40 @@ include_once('../header.php');
     </p>
     <h3 id="damagecap">Damage Cap</h3>
     <p>
-        The Damage Cap was implemented to avoid players from taking huge amounts of damage on early turns. It is indicated by a little shield symbol you can see while hovering over heroes on the left bar. The current Damage Cap is set to 15, meaning if a player would take more than 15 damage (e.g. 20) while the cap is active, the damage will be reduced to the maximum amount of 15.
+        The Damage Cap was implemented to avoid players from taking huge amounts of damage on early turns. It is
+        indicated by a little shield symbol you can see while hovering over heroes on the left bar. The current Damage
+        Cap is set to 15, meaning if a player would take more than 15 damage (e.g. 20) while the cap is active, the
+        damage will be reduced to the maximum amount of 15.
         <br><br>
         The Damage Cap is in place until both a hero has died and the game has reached at least turn 8 (10 gold).
     </p>
     <h3 id="deadguy">Fighting 'Deadguy'</h3>
     <p>
-        Everytime an odd number of players is already defeated before a combat round, one of the remaining players will be paired with one of the dead players. These 'deadguys' will bring the board lineup from the time of their death to the fight, but will not activate hero powers or anything else.
+        Whenever an odd number of players are defeated before a combat round, one of the remaining players will be
+        randomly paired with one of the eliminated players. These eliminated players, referred to as 'deadguys' (their
+        ingame name will show as "Kel'Thuzad"), will bring their board lineup from the time of their death to the fight,
+        but will not activate hero powers or any other abilities.
         <br><br>
+        In most cases, the fight will be relatively easy to win, allowing players to use the lobby turn beforehand to
+        level up a tier or improve their board without much concern for the matchup. However, an exception can occur in
+        the endgame when only three players are left standing, and one of them will have to face the player who was just
+        eliminated and likely had an endgame-viable board. Losing to a 'deadguy' will deal the normal amount of damage
+        and could potentially lead to elimination.
+        <br><br>
+        The bar on the left of the screen represents the heroes ordered by their remaining health and armor. In the
+        event of a 'deadguy' fight, the bottom three players who were not paired with a 'deadguy' during the previous
+        round will be eligible.
     </p>
     <h3 id="gamelength">Game length</h3>
     <p>
-        Most lobbies will take about 15 turns to find a winner, which translates to around 25 to 30 minutes in real life. Of course there are exceptions to this rule, for example when many fights end in ties during the endgame it is totally possible to reach 20+ turns.
+        Most lobbies will take about 13-15 turns to find a winner, which translates to around 25 to 30 minutes in real
+        life. Of course there are exceptions to this rule, for example when many fights end in ties during the endgame
+        it is totally possible to reach 20+ turns.
         <br><br>
-        The first 5 turns are usually refered to as the early game, whereas turns 6 to 10 are the mid-game and everything after that is the end game. It is very uncommon for a player to die before turn 8 (notwithstanding conceding or disconnecting), but pretty common on turns 8 to 10. But that will also depend on the current meta game, of course.
+        The first 5 turns are usually refered to as the early game, whereas turns 6 to 10 are the mid-game and
+        everything after that is the end game. It is very uncommon for a player to die before turn 8 (notwithstanding
+        conceding or disconnecting), but pretty common on turns 9 to 10. But that will also depend on the tempo of the
+        current meta game, of course.
     </p>
     <h3 id="damage">Damage Output</h3>
     <p>
@@ -101,14 +160,23 @@ include_once('../header.php');
     </p>
     <h3 id="ratingfloor">Rating Floors</h3>
     <p>
-        There are rating floors between 2000 and 6000 for your MMR. Your rating does not drop below each floor once it is above it. Currently, there is a floor every 500 rating points: 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000.
+        There are rating floors between 2000 and 6000 for your MMR. Your rating does not drop below each floor once it
+        is above it. Currently, there is a floor every 500 rating points: 2000, 2500, 3000, 3500, 4000, 4500, 5000,
+        5500, 6000.
         <!--        (<a href=https://hearthstone.blizzard.com/en-us/news/23523064 target="_blank">source</a>)-->
     </p>
     <h3 id="ratinginternal">Internal rating</h3>
     <p>
-        At the start of each BG season (roughly every 3 months) your shown/external MMR will be reset to zero. But Hearthstone saves your internal MMR (which at the end of a season will be very close to your external MMR) and will match-make you by your internal MMR.
+        At the start of each BG season (roughly every 3 months) your shown/external MMR will be reset to zero. But
+        Hearthstone saves your internal MMR (which at the end of a season will be very close to your external MMR) and
+        will match-make you by your internal MMR.
         <br><br>
-        A lobby win at that point in time will net you up to 300 points, allowing you to climb back to your 'real' MMR faster. This bonus for point gains will decrease the closer you'll get to your internal MMR (and will also be capped at ~8000 MMR, even if you had a very high rating like 10,000+ in the last season).
+        A lobby win at that point in time will net you up to 300 points, allowing you to climb back to your 'real' MMR
+        faster. This bonus for point gains will decrease the closer you'll get to your internal MMR (and will also be
+        capped at ~8000 MMR, even if you had a very high rating like 10,000+ in the last season).
+    </p>
+    <h3 id="ratinginternal">Winning percentages</h3>
+    <p>
     </p>
     <br><br>
 </div>
