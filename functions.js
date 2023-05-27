b2a0d2c9e5ee106010a9b2ea31f9dac8fb29a963
O-Nemet <github@nemet.online>
2023-05-28 01:48:23 +0200

    Optimized perma link building

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function CopyLink(button) {
-    navigator.clipboard.writeText(location.href + '#' + button.parentElement.id).then(function () {
+    navigator.clipboard.writeText(location.href.split('#')[0] + '#' + button.parentElement.id).then(function () {
         // console.log(location.href + '#' + button.parentElement.id);
     });
 }
