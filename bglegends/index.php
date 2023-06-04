8c8ee008b0877d79c183527184942c20c2ea44db
O-Nemet <github@nemet.online>
2023-06-05 00:40:43 +0200

    Added LL 2023 and LL#10 information and results Added ids and perma-links

--- bglegends/index.php
+++ bglegends/index.php
@@ -3,28 +3,110 @@ include_once('../header.php');
 ?>
 <h2 class="page_title">Tournaments</h2>
 <div class="lobby_legends">
+    <!--    <p>-->
+    <!--    The next Lobby Legends top 48 qualifier will happen on November 19th. There will be no official broadcast, but it will be live-streamed by several of the players on their Twitch channels. All streamers will have to obey a 30-minute delay and the event will take 2x3 rounds, so about 5 hours. Further information can be found on <a href="https://playhearthstone.com/en-gb/esports/tournament/lobby-legends-9/" target="_blank">Blizzards event website</a>.-->
+    <!--    <br><br>-->
+    <!--    <script src="https://cdn.logwork.com/widget/countdown.js"></script>-->
+    <!--    <a onclick="return false;" href="https://logwork.com/countdown-73ot" class="countdown-timer" data-style="circles" data-timezone="Europe/Berlin" data-date="2022-11-19 09:30" data-background="#8dbdd8">Lobby Legends #9 Qualifier</a>-->
+    <!--    The Lobby Legends Spring Championship with a $50,000 prize pool will happen on May 27th (top 16 semi-finals) and May 28th (top 8 finals), and will be live-streamed with moderation on the official <a href="https://www.twitch.tv/playhearthstone" target="_blank">HS Twitch channel</a>. On the first day 2x3 rounds will be played, which usually takes about 6 hours. On the second day 5-8 rounds will be played. Further information can be found on <a href="https://hearthstone.blizzard.com/en-us/news/23958485" target="_blank">Blizzards event website</a> or this <a href="https://esports.gg/news/hearthstone/battlegrounds-lobby-legends-returns-this-weekend-with-the-spring-championship/" target="_blank">article on esports.gg</a>.-->
+    <!--    <br><br>-->
+    <!--    <script src="https://cdn.logwork.com/widget/countdown.js"></script>-->
+    <!--    <a onclick="return false;" href="https://logwork.com/countdown-73ot" class="countdown-timer" data-style="circles" data-timezone="Europe/Berlin" data-date="2023-05-28 18:00" data-background="#8dbdd8">Lobby Legends Spring Championship Finals</a>-->
+    <!--    <br><br>-->
+    <!--    </p>-->
+    <!--    <br>-->
     <img src="<?= PICTURE_LOCAL ?>misc/ll_logo.webp" alt="Lobby Legends logo">
+    <br>
     <p>
-        Lobby Legends was a monthly esports series by Blizzard, started in early 2022 and supported with a total prize pool of $500,000. Basic information can be found on the <a href="https://playhearthstone.com/esports/programs/battlegrounds" target="_blank">official website</a>.
+        Lobby Legends was redesigned for 2023. Only three big championships, each with a prize pool of $50,000 will be played. The original announcement from Blizzard can be found <a href="https://hearthstone.blizzard.com/en-gb/news/23904520/climb-the-ladder-with-hearthstone-esports-in-2023">here</a>.
+        <br><br>
+        In theory, everyone can qualify for the championships. The first qualification gate is securing enough points for finishing high on the official <a href="https://playhearthstone.com/community/leaderboards?region=US&leaderboardId=battlegrounds" target="_blank">regional leaderboards</a> (Americas, Asia-Pacific or Europe) at the end of each month for a period of three months successively.
+        <br><br>
+        The top 4 players from each region (by accumulated points) and the 4 players with most points from any region, will battle it out over two days in the semi-finals (two groups with three rounds each) and the finals (the remaining 8 players would play lobbies until someone has accumulated 20 points and then wins a lobby).
+    </p>
+    <h3 id="ll10">Lobby Legends #10 (“Spring Championships” at 27/28.05.2023) Top 8 after 5 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <p>
+        Patch 26.2.3 with all minion types freshly redesigned and the new hero <a class="hoverimage" href="https://bgknowhow.com/bgstrategy/hero/?id=94">Rock Master Voone</a> was played. Also, heroes had low MMR armor values and streamer-mode was enabled (meaning players were unable to see which hero was played by whom).
+    </p>
+    <br>
+    <p>
+        <a href="https://www.youtube.com/watch?v=U54y4fVI_-M" target="_blank">YouTube VOD recording of day 1</a>
+        <br>
+        <a href="https://www.youtube.com/watch?v=XKDeSuVPS3w" target="_blank">YouTube VOD recording of day 2</a>
+    </p>
+    <br>
+    <table class="tier-table">
+        <thead>
+        <tr>
+            <th>Place</th>
+            <th>Player</th>
+            <th>Country</th>
+            <th>Points</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>AlexJP</td>
+            <td>AP (JPN)</td>
+            <td>29.5</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>PenguinSan</td>
+            <td>AP (JPN)</td>
+            <td>21.5</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Yanase</td>
+            <td>AP (JPN)</td>
+            <td>18.0</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>BeterBabbit</td>
+            <td>US (USA)</td>
+            <td>17.0</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Tume</td>
+            <td>EU (FIN)</td>
+            <td>17.0</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Shirousa</td>
+            <td>AP (JPN)</td>
+            <td>14.5</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Tesura</td>
+            <td>AP (JPN)</td>
+            <td>14.0</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Satellite</td>
+            <td>AP (JPN)</td>
+            <td>8.5</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <br>
+    <hr>
+    <br>
+    <p>
+        Originally Lobby Legends was a monthly esports series by Blizzard, started in early 2022 and supported with a total prize pool of $500,000. Basic information can be found on the <a href="https://playhearthstone.com/esports/programs/battlegrounds" target="_blank">official website</a>.
         <br><br>
         In theory, everyone could qualify for the finals. The first qualification gate was securing a spot in the top 16 of one of the official <a href="https://playhearthstone.com/community/leaderboards?region=US&leaderboardId=battlegrounds" target="_blank">regional leaderboards</a> (Americas, Asia-Pacific or Europe) at the end of the month. Usually 3-4 weeks later a top 48 tournament with the qualified players from all three regions would take place. To advance further, one had to reach top 4 out of 8 (8 players per group) after three rounds of BGs, twice.
         <br><br>
         After that the LL main event happened, where the 12 remaining players and 4 qualifiers from China were battling it out over two days in the semi-finals (two groups with three rounds each) and the finals (the remaining 8 players would play lobbies until someone has accumulated 20 points and then wins a lobby).
-        <br><br>
-        <!--        The next Lobby Legends top 48 qualifier will happen on November 19th. There will be no official broadcast, but it will be live-streamed by several of the players on their Twitch channels. All streamers will have to obey a 30-minute delay and the event will take 2x3 rounds, so about 5 hours. Further information can be found on <a href="https://playhearthstone.com/en-gb/esports/tournament/lobby-legends-9/" target="_blank">Blizzards event website</a>.-->
-        <!--        <br><br>-->
-        <!--        <script src="https://cdn.logwork.com/widget/countdown.js"></script>-->
-        <!--        <a onclick="return false;" href="https://logwork.com/countdown-73ot" class="countdown-timer" data-style="circles" data-timezone="Europe/Berlin" data-date="2022-11-19 09:30" data-background="#8dbdd8">Lobby Legends #9 Qualifier</a>-->
-        <!--        The next Lobby Legends will happen on December 3rd (top 16 semi-finals) and December 4th (top 8 finals), and will be live-streamed with moderation on the official <a href="https://www.twitch.tv/playhearthstone" target="_blank">HS Twitch channel</a>. On the first day 2x3 rounds will be played, which usually takes about 6 hours. On the second day 5-8 rounds will be played. Further information can be found on <a href="https://playhearthstone.com/en-gb/esports/tournament/lobby-legends-9/" target="_blank">Blizzards event website</a>.-->
-        <!--        <br><br>-->
-        <!--        <script src="https://cdn.logwork.com/widget/countdown.js"></script>-->
-        <!--        <a onclick="return false;" href="https://logwork.com/countdown-73ot" class="countdown-timer" data-style="circles" data-timezone="Europe/Berlin" data-date="2022-12-03 00:00" data-background="#8dbdd8">Lobby Legends #9 Top16</a>-->
     </p>
-    <br>
-
-    <hr>
-
-    <h3>Lobby Legends #1 (“Raid Leaders” at 02/03.04.2022) Top 8 after 5 lobbies:</h3>
+    <h3 id="ll1">Lobby Legends #1 (“Raid Leaders” at 02/03.04.2022) Top 8 after 5 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 22.6.0 with <a href="https://bgknowhow.com/bgstrategy/?show=buddies">buddies</a> plus armor, but without Friend of a Friend was played.
     </p>
@@ -96,7 +178,7 @@ include_once('../header.php');
         </tbody>
     </table>
 
-    <h3>Lobby Legends #2 (“Noblegarden” at 14/15.05.2022) Top 8 after 6 lobbies:</h3>
+    <h3 id="ll2">Lobby Legends #2 (“Noblegarden” at 14/15.05.2022) Top 8 after 6 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 23.2.0 with Naga freshly released (and still unnerfed), as well as the new key minions <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=211'>Mantid Queen</a>, <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=208'>Leeroy</a>, <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=210'>Darkgaze Elder</a> and <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=209'>Young Murk-Eye</a> was played.
     </p>
@@ -168,7 +250,7 @@ include_once('../header.php');
         </tbody>
     </table>
 
-    <h3>Lobby Legends #3 (“Eternal Night” at 25/26.06.2022) Top 8 after 6 lobbies:</h3>
+    <h3 id="ll3">Lobby Legends #3 (“Eternal Night” at 25/26.06.2022) Top 8 after 6 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 23.4.3 with rebuffed Naga, new <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=51'>Pyramad</a> and <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=101'>Tarecgosa</a> back on tier 3 was played.
     </p>
@@ -240,7 +322,7 @@ include_once('../header.php');
         </tbody>
     </table>
 
-    <h3>Lobby Legends #4 (“Fire Festival” at 09/10.07.2022) Top 8 after 7 lobbies:</h3>
+    <h3 id="ll4">Lobby Legends #4 (“Fire Festival” at 09/10.07.2022) Top 8 after 7 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 23.6.1 with a 25% chance of Darkmoon Prizes, as well as <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=222'>Kooky</a> but without <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=3'>Chromawing</a> was played. Also, an unfixed <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=208'>Leeroy</a> Divine Shield bug was featured.
     </p>
@@ -312,7 +394,7 @@ include_once('../header.php');
         </tbody>
     </table>
 
-    <h3>Lobby Legends #5 (“Magic of Azeroth” at 04/05.09.2022) Top 8 after 8 lobbies:</h3>
+    <h3 id="ll5">Lobby Legends #5 (“Magic of Azeroth” at 04/05.09.2022) Top 8 after 8 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 24.2.1 with quests/rewards, a Demon revamp and brand-new minions like <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=242'>Tea Master Theotar</a>, <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=240'>Rendle</a> and <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=241'>Tortollan Blue Shell</a>, was played. Also, a controversy arose in lobby number 5, when Ribapusa was in a tournament winning position but the lobby server crashed (a too well known bug) with only 3 players left alive. The ruling was a no-score and a fresh lobby start, resulting in the longest Lobby Legends yet, and Ribapusa only finishing third.
     </p>
@@ -384,7 +466,7 @@ include_once('../header.php');
         </tbody>
     </table>
 
-    <h3>Lobby Legends #6 (“Castle Nathria” at 24/25.09.2022) Top 8 after 8 lobbies:</h3>
+    <h3 id="ll6">Lobby Legends #6 (“Castle Nathria” at 24/25.09.2022) Top 8 after 8 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 24.2.2 with quests/rewards, with buffed two-drops and a nerfed <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=85'>Toggwaggle</a>, was played. Also, the Twitch "Tavern Buddy" extension for multiple ways of viewer interaction was used for the very first time.
     </p>
@@ -456,7 +538,7 @@ include_once('../header.php');
         </tbody>
     </table>
 
-    <h3>Lobby Legends #7 (“Murlocized” at 15/16.10.2022) Top 8 after 7 lobbies:</h3>
+    <h3 id="ll7">Lobby Legends #7 (“Murlocized” at 15/16.10.2022) Top 8 after 7 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 24.4.0 with quests/rewards and <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=88'>Sylvanas</a> as a new hero was played.
     </p>
@@ -528,7 +610,7 @@ include_once('../header.php');
         </tbody>
     </table>
 
-    <h3>Lobby Legends #8 (“Hallow's End” at 12/13.11.2022) Top 8 after 6 lobbies:</h3>
+    <h3 id="ll8">Lobby Legends #8 (“Hallow's End” at 12/13.11.2022) Top 8 after 6 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 24.6.2 with quests/rewards, a slightly nerfed Volatile Venom and the returned rewards Golden Hammer and Kidnap Sack was played.
     </p>
@@ -600,7 +682,7 @@ include_once('../header.php');
         </tbody>
     </table>
 
-    <h3>Lobby Legends #9 (“Winter Veil” at 02/03.12.2022) Top 8 after 6 lobbies:</h3>
+    <h3 id="ll9">Lobby Legends #9 (“Winter Veil” at 02/03.12.2022) Top 8 after 6 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Patch 25.0.0 with quests/rewards, the new hero <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=90'>Enhance-o Mechano</a> and the new quest reward Enhance-a-matic was played. The prize pool was doubled to $100,000 for this final event of the year.
     </p>
@@ -674,10 +756,9 @@ include_once('../header.php');
 
     <br><br>
 
-    <!--    <img src="--><? //= PICTURE_LOCAL ?><!--misc/ll_prizepool.webp" alt="Lobby Legends prize pool">-->
     <img src="<?= PICTURE_LOCAL ?>misc/tr_players.webp" alt="Twitch Rivals Invitational players">
 
-    <h3>Twitch Rivals 2022 (“Thijs's HS Tavern” at 16.07.2022) Top 8 after 6 lobbies:</h3>
+    <h3 id="tr2022">Twitch Rivals 2022 (“Thijs's HS Tavern” at 16.07.2022) Top 8 after 6 lobbies <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         At TwitchCon Amsterdam the finals of this 24 player invitational tourney with a total prize pool of $20,000 took place.<br>
         Patch 23.6.2 with a 25% chance of Darkmoon Prizes, as well as <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=222'>Kooky</a> but without <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/hero/?id=3'>Chromawing</a> was played.
