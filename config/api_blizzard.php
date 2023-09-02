a9fb33dd106827d36140696ba5af53d736c5c3c0
O-Nemet <github@nemet.online>
2023-09-02 20:07:08 +0200

    Enhanced workflow with GitHub config secrets Added database config real file Added blizzard api config real file

---
+++ config/api_blizzard.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *    Required vars for the api to work
+ *
+ **/
+
+$client_id     = '__API_ID__';
+$client_secret = '__API_SECRET__';
+$region        = 'EU'; // see https://develop.battle.net/documentation/guides/regionality-and-apis for these settings
+$locale        = 'de_DE'; // al https://develop.battle.net/documentation/guides/regionality-and-apis for avail locals
+$redirect_uri  = 'https://bgknowhow.com/index.php';
