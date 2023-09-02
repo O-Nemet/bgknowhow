0ea93467b06c4d919872c80fb3d024b4d1d2bd27
O-Nemet <github@nemet.online>
2023-09-02 18:35:07 +0200

    Added inactive indicator to all comp/minion boards Optimized standard comp text

--- functions.php
+++ functions.php
@@ -361,6 +361,7 @@ function getMinionsForBoard($board): array
                     $minions[$i]['name']    = $object->name;
                     $minions[$i]['picture'] = $object->pictureSmall;
                     $minions[$i]['url']     = $object->websites->bgknowhow;
+                    $minions[$i]['active']  = $object->isActive;
                     continue 2;
                 }
             } else {
@@ -369,12 +370,14 @@ function getMinionsForBoard($board): array
                     $minions[$i]['name']    = $object->name;
                     $minions[$i]['picture'] = $object->pictureSmall;
                     $minions[$i]['url']     = $object->websites->bgknowhow;
+                    $minions[$i]['active']  = $object->isActive;
                     continue 2;
                 } else if ($object->nameShort === $minionName) {
 //                    echo "short ";
                     $minions[$i]['name']    = $object->name;
                     $minions[$i]['picture'] = $object->pictureSmall;
                     $minions[$i]['url']     = $object->websites->bgknowhow;
+                    $minions[$i]['active']  = $object->isActive;
                     continue 2;
                 }
             }
@@ -394,13 +397,14 @@ function getMinionsForBoard($board): array
 function drawBoard($minions)
 {
     foreach ($minions as $minion) {
-        echo '<a href="' . $minion['url'] . '"><img src="' . $minion['picture'] . '" alt="' . $minion['name'] . '" title=""></a>';
+        echo '<a href="' . $minion['url'] . '"><img src="' . $minion['picture'] . '" class="' . (!$minion['active'] ? 'inactive-img' : '') . '" alt="' . $minion['name'] . '" title=""></a>';
     }
 }
 
 function getCompositionText(): string
 {
-    return "These different compositions are meant to display proven setups to strive for, for the very end game (top 4 and above). In general one of the seven slots will be the 'flex' spot, used to cycle new minions during the tavern rounds. Therefore, your actual board will rarely be as perfect as these listed here. Of course as many minions as possible should be tripled and buffed with Reborn, Taunt, Venomous or Divine Shield. Also primary support units like <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=109'>Brann</a> and <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=121'>Nomi</a> will usually be tossed for the very last fights, but are sometimes displayed here when being integral to the setup. If one of your units is lacking, it is also often beneficial to replace it with a <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=208'>Leeroy</a> or a <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=211'>Mantid Queen</a>, before a deciding fight.";
+    return "These different compositions are meant to display proven setups to strive for, for the very end game (top 4 and above). In general one of the seven slots will be the 'flex' spot, used to cycle new minions during the tavern rounds. Therefore, your actual board will rarely be as perfect as these listed here. Of course as many minions as possible should be tripled and buffed with Reborn, Venomous, Divine Shield or Taunt.<br><br>
+        Also primary support units like <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=109'>Brann</a> and <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=121'>Nomi</a> will usually be tossed for the very last fights, but are sometimes displayed here when being integral to the setup. If one of your units is lacking, it is also often beneficial to replace it with a <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=208'>Leeroy</a> or a <a class='hoverimage' href='https://bgknowhow.com/bgstrategy/minion/?id=211'>Mantid Queen</a>, before a deciding fight. Tier 7 minions are usually ignored for these comps, as <a href='https://bgknowhow.com/bgstrategy/anomaly/?id=8'>Secrets of Norgannon</a> only has a ~20% chance to be active.";
 }
 
 function convertStrategyText(string $text): string
