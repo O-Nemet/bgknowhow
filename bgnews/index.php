1b05865be80001985574025d7e24f4ec7d80a18e
O-Nemet <github@nemet.online>
2023-11-04 02:16:49 +0100

    Optimized news text

--- bgnews/index.php
+++ bgnews/index.php
@@ -9,7 +9,7 @@ include_once('../header.php');
         The official announcement can be found on the <a href="https://hearthstone.blizzard.com/en-gb/news/24008691/introducing-battlegrounds-duos" target="_blank">Blizzard HS website</a> or you can view the <a href="https://www.twitch.tv/videos/1967600933?t=2h32m19s" target="_blank">live on stream reveal</a> or the <a href="https://www.twitch.tv/videos/1967600933?t=7h14m54s" target="_blank">complete preview panel</a>.
     </p>
     <ul>
-        <li>Each duo team will share a single HP/armor pool (HP will be 30, armor will be the combined hero armor values of partner#1 plus partner#2)</li>
+        <li>Each duo team will share a single HP/armor pool (HP will be 30, armor will be the combined hero armor values of both heroes)</li>
         <li>The buy phase is pretty much the same, but you can check out your partners tavern and board anytime, by clicking on a new portal animation on the right side of your screen</li>
         <li>Each player will have to level their tavern tier up by themselves</li>
         <li>Any card in your hand can be sent to your partner for 1 gold each, by moving it onto the new portal animation</li>
@@ -21,6 +21,7 @@ include_once('../header.php');
         <li>The duo mode will add at least 4 new mode-exclusive heroes (Cho'Gall, Madame Goya, Lubittonous Gloop and Nameless One)</li>
         <li>The duo mode will add at least 6 new mode-exclusive minions, featuring special abilites (like triggering on passing a minion through the portal)</li>
         <li>An icon-based ping system for minions and buttons (freeze, level, etc.) has been added to enable voiceless communication options between teammates. The icons available are a question mark, a green check, a red x and a portal symbol</li>
+        <li>There will be seperate queues for traditional BG single player and BG duo teams (with the additional option to also join alone and get teamed up by the system)</li>
         <li>There seems to be a permanent damage cap of 15 at the moment</li>
         <li>Deadguy fight handling seems to be bugged at the moment, as you can fight a duo team again, right after killing it</li>
     </ul>
