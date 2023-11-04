5af7d251eeb10d6621b6f82cf5374d00dad6a64e
O-Nemet <github@nemet.online>
2023-11-04 01:29:11 +0100

    Updated news content Adjusted news CSS

--- bgnews/index.php
+++ bgnews/index.php
@@ -1,10 +1,9 @@
 <?php
 include_once('../header.php');
 ?>
-
 <h2 class="page_title">Battleground News</h2>
 <br>
-<div>
+<div class="news">
     <h3 id="bgduos">04.11.2023 - Battlegrounds Duos announced at BlizzCon <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
         Blizzard just revealed an upcoming two player coop mode for Battlegrounds, which will be released "early in 2024".
@@ -14,24 +13,29 @@ include_once('../header.php');
         <li>The buy phase is pretty much the same, but you can check out your partners tavern and board anytime by clicking on a new portal animation on the right side of your screen</li>
         <li>Each player will have to level their tavern tier up by themselves</li>
         <li>Any card in your hand can be sent to your partner for 1 gold each by moving it onto a new portal animation on the right side of your screen</li>
+        <li>New overlay effects for the tavern have been added, clearly indicating that a minion in your tavern could make a triple for your partner</li>
         <li>The starting player for the combat phase will alternate between you and your partner every turn (indicated by the order of the display on the left side)</li>
         <li>The board of player#2 will spawn in combat after the board of player#1 has been wiped completely (including effects like Deathrattle and Reborn)</li>
-        <li>If the board of player#1 is already enough to win against both boards of the enemy duo team the empty slots on the board will be filled up with minions from p2</li>
-        <li>The mode will add at least 6 new mode-exclusive minions, which for example trigger on passing a minion through the portal, and at least four mode-exclusive heroes (Cho'Gall, Mai Ling, Gloop.</li>
+        <li>If the board of player#1 is already enough to win against both boards of the enemy duo team, the empty slots on the board will be filled up with minions from player#2 (therefore increasing the damage the losing team takes)</li>
+        <li>The duo mode will add at least 4 new mode-exclusive heroes (Cho'Gall, Madame Goya, Lubittonous Gloop and Nameless One)</li>
+        <li>The duo mode will add at least 6 new mode-exclusive minions, which for example trigger on passing a minion through the portal, and at least four mode-exclusive heroes (Cho'Gall, Mai Ling, Gloop.</li>
         <li>An icon-based ping system for minions and buttons (freeze, level, etc) has been added to enable voiceless communication options between teammates. The icons available are a question mark, a green check, a red x and a portal symbol)</li>
         <li>There seems to be a permanent damage cap of 15 at the moment</li>
+        <li>Deadguy fight handling seems to be bugged at the moment, as you can fight a duo team again, right after killing it</li>
     </ul>
     <p>
-        Check out the official communication;
+        Check out the official communication:
     </p>
     <p>
         If you want to see the new mode in action, several well-known streamers have teamed up and streamed from the BlizzCon floor, playing the current beta version:
     </p>
     <br>
+
     <h3 id="season6ending">03.11.2023 - Season of Anomalies coming to an end <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <p>
-        Observant players have reported that the Blizzard launcher lists the current BG season pass available until the 4th of December. This is a pretty decent indicator that the next BG season will start on December 5th. At the moment it is still unknown if that season will start. What is certain though is that everyone's MMR rating will be reset to 0 (your hidden MMR will still be there though) and you will have to buy a new Battle Pass if you want to be able to select more than two heroes at the start of each match.
+        Observant players have reported that the Blizzard launcher lists the current BG season pass available until the 4th of December. This is a pretty decent indicator that the next BG season will start on December 5th. At the moment it is still unknown if that season will start in vanilla mode or with the return of the Buddy or the Quest system. What is certain though is that everyone's MMR rating will be reset to 0 (your hidden MMR will still be there though) and you will have to purchase a new Battle Pass if you want to be able to select more than two heroes at the start of each match.
     </p>
+    <br>
 </div>
 
 <?php
