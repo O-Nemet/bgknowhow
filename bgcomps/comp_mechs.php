3e31a3afc58f46df5f8c4493aa1b71cada3f1716
O-Nemet <github@nemet.online>
2023-12-08 16:43:16 +0100

    Updated Mech comps after patch 28.2

--- bgcomps/comp_mechs.php
+++ bgcomps/comp_mechs.php
@@ -8,7 +8,7 @@ include_once('../header.php');
 </p>
 <br>
 <p>
-    In general Mech compositions are strong against Murlocs and venom/scam comps, and fare better when Quilboars are available to increase the size of shielded mechs in the early- and mid-game. They are weaker against big stats comp like Demons, Elementals or Naga.
+    In general, Mechs have the upside of having numerous <a href="/bgbasics/#shield">Divine Shield</a> minions, the ability to resummon Mechs, and small token units. This gives them an advantage against venom/scam compositions and builds with low stats. Except for <a class="hoverimage" href="/bgstrategy/minion/?id=353">Beatboxer</a> magnetization composition, this makes them weak against high stat builds like Demons, Elementals, and Naga.
 </p>
 <div class="comp_wrapper">
     <h3 id="deflecto">Deflecto Reset Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
@@ -18,10 +18,10 @@ include_once('../header.php');
     drawBoard($minions);
     ?>
     <p>
-        When going for the <strong>Deflect-o-Bot</strong> mech strategy, it is all about the resets for your MVPs, meaning the regaining of your Bots divine shields when another of your minions dies and summons a new mech. The overall strength of your comp will improve by how much you will be able to buff your <strong>Deflect-o-Bots</strong> by utilizing magnetic minions, gems or similar means, during the tavern phase. Which units you pick for the resets is of secondary importance.
+        When going for the <strong>Deflect-o-Bot</strong> mech strategy, the important aspect is to have your other mechs get destroyed so <strong>Deflect-o-Bot</strong> can regain its Divine Shield. This allows your Deflectos to deliver free hits without taking damage, a unique strategy that only Mechs and Quilbaors have. The overall strength of your comp will improve by how much you will be able to buff your <strong>Deflect-o-Bots</strong> by utilizing magnetic minions, gems or similar means, during the tavern phase. Which units you pick for the resets is of secondary importance.
         <br><br>
-        That is one of the beauties of this comp, because it features a very natural progression where your good early game picks like Mummy and Golem are still valuable in the very late game. And even a minion like <a class="hoverimage" href="/bgstrategy/minion/?id=364">Cogwork Copter</a> buffed with a <a class="hoverimage" href="/bgstrategy/minion/?id=62">Replicating Menace</a> can be of great use later. Because you don't need any 6-star or golden minions to win games, you can put great tempo pressure on the other players in the lobby, catching them off-guard before they are able to complete their late-game comps.
-        <!--        <br><br>-->
+        Eventually you want to get cards like <a class="hoverimage" href="/bgstrategy/minion/?id=62">Replicating Menace</a> so minions that do not have Deathrattle or Reborn such as <a class="hoverimage" href="/bgstrategy/minion/?id=364">Cogwork Coptor</a> can also provide a reset. <a class="hoverimage" href="/bgstrategy/minion/?id=73">Annoy-o-Module</a> is also important so Deflect-o-Bot can be protected by redirecting their attacks to the minions you want. Kangor's Apprentice is a very useful card for the build. The advantage if that having Tier-6 minions is not that important as you can find multiple minions to reset Deflect-o-Bot, but you are offset by the lack of scaling in this build. Eventually you will want to transition this build into one of the following compositions."
+        <br><br>
         <!--        The Mecherel on the right should be taunted and shielded by an <a class="hoverimage" href="/bgstrategy/minion/?id=73">Annoy-o-Module</a>. That way you can position all your other shielded minions on the left side and the taunted Mecherel will provide perfect value by getting a new shield every time one of your minions attacks. The second Mecherel is optional, but the first one is kinda essential to play for first place.-->
         <!--        <br><br>-->
         <!--        Another little trick: If your opponent is trying to counter your shields via a <a class="hoverimage" href="/bgstrategy/minion/?id=226">Blaster</a>, your Mecherels will instantly regain their shield as long as another minion with a shield was positioned to the right of the Mecherel.-->
@@ -44,7 +44,9 @@ include_once('../header.php');
     drawBoard($minions);
     ?>
     <p>
-        Magnetization can be given even on full boards. Buy as many as you can to profit of your <strong>Beatboxers</strong> and put it on any Mech you plan to keep until the end game. All of them are decent, so don't be picky. Also, in the past it was possible to run out of Magnetize minions because the Scrapers pulled minions from the shared minion pool. But this was changed in a July 2023 update, now magnetized minions will return to the pool upon usage.
+        Buy as many magnetic minions as you can to profit of your <strong>Beatboxers</strong> and put it on any Mech you plan to keep until the end game. All of them are decent, so don't be picky. The spell <a class="hoverimage" href="/bgstrategy/spell/?id=38">Perfect Vision</a>, multiple <a class="hoverimage" href="/bgstrategy/minion/?id=259">Felemental</a>s, <a class="hoverimage" href="/bgstrategy/minion/?id=364">Cogwork Coptor</a>, and <a class="hoverimage" href="/bgstrategy/minion/?id=315">Scourfin</a> all help this build, as they double their effect due to <strong>Beatboxer</strong>.
+        <br><br>
+        Magnetization can be given even on full boards. Also, in the past it was possible to run out of Magnetize minions because the Scrapers pulled minions from the shared minion pool. But this was changed in a July 2023 update, now magnetized minions will return to the pool upon usage.
     </p>
     <h3 id="deathrattle">Buster/Kangor Deathrattle Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
@@ -57,7 +59,7 @@ include_once('../header.php');
         <br><br>
         It is quite important to taunt the second <strong>Buster</strong>, then you have a 50% chance that your first two dead minions will be <strong>Busters</strong>. Without the taunt, what will usually happen is that the token from the first Buster will die as a second minion and be saved in the <strong>Kangors</strong>. Taunting also greatly reduces the chance that your <strong>Titus</strong> or <strong>Kangors</strong> will be sniped before providing sufficient value.
         <br><br>
-        The Foe Reaper is the least important part of this comp, but obviously increases your chances against venomous or shield lineups. In addition to the <strong>Titus</strong>, tripling a <strong>Buster</strong> is a big plus, whereas a golden <strong>Kangor's</strong> should be avoided as two regular ones are way more valuable.
+        The Foe Reaper is the least important part of this comp, but obviously increases your chances against venomous or shield lineups. In addition to the <strong>Titus</strong>, tripling a <strong>Buster</strong> is a big plus, whereas a golden <strong>Kangor's</strong> should be avoided as two regular ones are way more valuable. Watch out for the very useful spell <a class="hoverimage" href="/bgstrategy/spell/?id=38">Titus' Tribute</a> when running this comp.
     </p>
     <h3 id="deathrattlebeasts">Buster Deathrattle Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
@@ -72,7 +74,7 @@ include_once('../header.php');
         <br><br>
         Golden <strong>Macaws</strong>, a golden <strong>Titus</strong> and golden <strong>Busters</strong> will make the most impact for this composition. If you only have one Buster, taunting the Deflect-o-Bot or even the Reaper as protection for the essential Titus can sometimes be the right call. But with any Titus comp, winning or losing the attack-first coin flip has a huge impact on the following battle.
         <br><br>
-        The Macaws can also be replaced with <a class="hoverimage" href="/bgstrategy/minion/?id=421">Hawkstrider Herald</a>.
+        The Macaws can also be replaced with <a class="hoverimage" href="/bgstrategy/minion/?id=421">Hawkstrider Herald</a>. Watch out for the very useful spell <a class="hoverimage" href="/bgstrategy/spell/?id=38">Titus' Tribute</a> when running this comp.
     </p>
     <h3 id="exodia">Exodia Deathrattle Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
@@ -81,18 +83,18 @@ include_once('../header.php');
     drawBoard($minions);
     ?>
     <p>
-        This is a special case of another Mech Deathrattle variation, for which you will bascially need exactly the pieces displayed here and they should be positioned in this order. Due to the timing interaction of the different components, what will happen is that the <strong>Blaster</strong> will die first and trigger twice, destroying your whole board but the <strong>Titus</strong>. After that mayhem the <strong>Kangor's</strong> will both spawn fresh <strong>Busters</strong> and your board will be 6 big Busters plus the Titus, and every dying Buster will grow the stats on your board again.
+        This is a special case of another Mech Deathrattle variation, for which you will bascially need exactly the pieces displayed here, and they should be positioned in this order. Due to the timing interaction of the different components, what will happen is that the <strong>Blaster</strong> will die first and trigger twice, destroying your whole board but the <strong>Titus</strong>. After that mayhem the <strong>Kangor's</strong> will both spawn fresh <strong>Busters</strong> and your board will be 6 big Busters plus the Titus, and every dying Buster will grow the stats on your board again.
         <br><br>
         This is a very hard to beat comp, especially if you are able to protect your Titus by buffing it.
     </p>
     <h3 id="lighter">Lighter Fighter Build <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Buster', 'Lighter Fighter', 'Lighter Fighter', 'Kangor\'s', 'Kangor\'s', 'Titus', 'Titus'];
+    $board   = ['Lighter Fighter', 'Lighter Fighter', 'Hawkstrider', 'Kangor\'s', 'Kangor\'s', 'Titus', 'Titus'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        <a class="hoverimage" href="/bgstrategy/minion/?id=260">Mecha-Jaraxxus</a> is an important pickup for this comp, to find you <a class="hoverimage" href="/bgstrategy/minion/?id=291">Baltharak</a> to give <strong>Lighter Fighter</strong> reborn.
+        <a class="hoverimage" href="/bgstrategy/minion/?id=260">Mecha-Jaraxxus</a> is an important pickup for this comp, to find you <a class="hoverimage" href="/bgstrategy/minion/?id=291">Baltharak</a> to give <strong>Lighter Fighter</strong> reborn. Taunting the Fighters is advised, of course.
     </p>
 </div>
 
