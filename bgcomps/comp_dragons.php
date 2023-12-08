8c99c33824fa213454c3a918658f5457a465c3ea
O-Nemet <github@nemet.online>
2023-12-08 01:39:19 +0100

    Added spell links to Dragon comp page

--- bgcomps/comp_dragons.php
+++ bgcomps/comp_dragons.php
@@ -26,7 +26,7 @@ include_once('../header.php');
     <p>
         After securing the key minions <strong>Kalecgos</strong> and <strong>Brann</strong>, focus on getting as many battlecries as you can. When you do activate battlecries, especially ones that give stats, put them on <strong>Hunter of Gatherers</strong> so he can give stats to everyone on the board.
         <br><br>
-        Other important cards: <strong>Brann's Blessing</strong>, <strong>Hired Headhunter</strong>, <strong>Dreamer's Embrace</strong> will help your build
+        Other important cards: <a class='hoverimage' href='/bgstrategy/spell/?id=23'>Brann's Blessing</a>, <a class='hoverimage' href='/bgstrategy/spell/?id=27'>Hired Headhunter</a>, <a class='hoverimage' href='/bgstrategy/spell/?id=32'>Dreamer's Embrace</a> will help your build.
     </p>
     <h3 id="drakkari">End of Turn Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
@@ -35,7 +35,7 @@ include_once('../header.php');
     drawBoard($minions);
     ?>
     <p>
-        This build may be a bit slow at first, but can really get going once you have <strong>Drakkari Enchanter</strong> or the spell <strong>Primal Staff</strong>. Just make sure that you leave a free space, so you can use the Smolderwing's you gain from Outback Smolderer. If you do triple everything and still got free space, <a class='hoverimage' href='/bgstrategy/minion/?id=454'>Ignition Specialist</a> can work very well.
+        This build may be a bit slow at first, but can really get going once you have <strong>Drakkari Enchanter</strong> or the spell <a class='hoverimage' href='/bgstrategy/spell/?id=25'>Primal Staff</a>. Just make sure that you leave a free space, so you can use the Smolderwing's you gain from Outback Smolderer. If you do triple everything and still got free space, <a class='hoverimage' href='/bgstrategy/minion/?id=454'>Ignition Specialist</a> can work very well.
     </p>
 </div>
 
