9ca2e85ccb919306e14ec783bd6d01c35a3a9ec7
O-Nemet <github@nemet.online>
2023-12-08 19:13:04 +0100

    Updated Murloc comps after patch 28.2

--- bgcomps/comp_murlocs.php
+++ bgcomps/comp_murlocs.php
@@ -2,33 +2,41 @@
 include_once('../header.php');
 ?>
 
-<h2 class="page_title">Standard Compositions: Murlocs</h2>
-<p>
-    <?= getCompositionText(); ?>
-</p>
-<br>
-<div class="comp_wrapper">
-    <h3 id="battlecry">Battlecry Hand Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
-    <?php
-    $board   = ['Bassgill', 'Bassgill', 'Skyfin', 'Murky', 'Murk-Eye', 'Primalfin', 'Brann'];
-    $minions = getMinionsForBoard($board);
-    drawBoard($minions);
-    ?>
-    <p>
-        Buffs the board but can also support one or two <a class="hoverimage" href="/bgstrategy/minion/?id=333">Bream Counter</a> by playing/cycling lots of Murlocs. The Bassgill is also useful for summoning <a class="hoverimage" href="/bgstrategy/minion/?id=208">Leeroy</a> or <a class="hoverimage" href="/bgstrategy/minion/?id=399">Relentless Mur'ghoul</a>.
-    </p>
-    <h3 id="venomous">Venomous Hand Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
-    <?php
-    $board   = ['Belcher', 'Belcher', 'Bassgill', 'Bassgill', 'Choral', 'Choral', 'Titus'];
-    $minions = getMinionsForBoard($board);
-    drawBoard($minions);
-    ?>
-    <p>
-        Heavily depends on growing two <a class="hoverimage" href="/bgstrategy/minion/?id=333">Bream Counter</a> in your hand or your venom carrying you to victory (don't play this against Undead). The Bassgill is also useful for summoning <a class="hoverimage" href="/bgstrategy/minion/?id=208">Leeroy</a> or <a class="hoverimage" href="/bgstrategy/minion/?id=399">Relentless Mur'ghoul</a>.
+    <h2 class="page_title">Standard Compositions: Murlocs</h2>
+    <p style="text-align: justify">
+        <?php include_once('intro.php'); ?>
     </p>
     <br>
-</div>
+    <div class="comp_wrapper">
+        <h3 id="battlecry">Battlecry Hand Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+        <?php
+        $board   = ['Murky', 'Young Murk-Eye', 'Murky', 'Young Murk-Eye', 'Primalfin Lookout', 'Wayward Grimscale', 'Brann'];
+        $minions = getMinionsForBoard($board);
+        drawBoard($minions);
+        ?>
+        <p>
+            Buffs the board but can also support one or two <a class="hoverimage" href="/bgstrategy/minion/?id=333">Bream Counter</a> by playing/cycling lots of Murlocs. Keep Young Murk-Eye between two battlecries. Ideally, you want to have the 'handloc' package of Bassgill, Choral Mrrrglr, and Bream Counter.
+        </p>
+        <h3 id="venomous">Venomous Hand Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+        <?php
+        $board   = ['Belcher', 'Belcher', 'Bassgill', 'Bassgill', 'Choral', 'Choral', 'Titus'];
+        $minions = getMinionsForBoard($board);
+        drawBoard($minions);
+        ?>
+        <p>
+            Heavily depends on growing one or two <a class="hoverimage" href="/bgstrategy/minion/?id=333">Bream Counter</a> in your hand or your venom carrying you to victory (don't play this against Undead). Diremuck Forager is a good alternative to Bassgil. Constantly recycle Murlocs so Bream Counter gets buffed.
+            <br><br>
+            The Bassgill is also useful for summoning <a class="hoverimage" href="/bgstrategy/minion/?id=208">Wayward Grimscale</a> or <a class="hoverimage" href="/bgstrategy/minion/?id=399">Relentless Mur'ghoul</a>. Also <strong>Titus Rivendare</strong> is very good for synergy with Operatic Belcher, Bassgill, and Scourfin.
+        </p>
+    </div>
 
-<?php
-include_once('../footer.php');
-?>
+    <br>
+    <br>
+    <hr>
+    <br>
+    <p>
+        <?php include_once('outro.php'); ?>
+    </p>
+    <br><br>
+
+<?php include_once('../footer.php'); ?>
