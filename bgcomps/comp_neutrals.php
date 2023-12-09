fb8c0d660b03741e5a33275e79ad8a01a25b8a6e
O-Nemet <github@nemet.online>
2023-12-09 02:49:54 +0100

    Updated Neutral comps after patch 28.2

--- bgcomps/comp_neutrals.php
+++ bgcomps/comp_neutrals.php
@@ -3,8 +3,8 @@ include_once('../header.php');
 ?>
 
 <h2 class="page_title">Standard Compositions: Neutrals</h2>
-<p>
-    <?= getCompositionText(); ?>
+<p style="text-align: justify">
+    <?php include_once('intro.php'); ?>
 </p>
 <br>
 <div class="comp_wrapper">
@@ -19,31 +19,58 @@ include_once('../header.php');
     <!--    </p>-->
     <h3 id="fort">Fort/Taunt Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Leeroy', 'Glowscale', 'Fort', 'Fort', 'Enchanter', 'Barricade', 'Barricade'];
+    $board   = ['Phalanx', 'Glowscale', 'Fort', 'Fort', 'Enchanter', 'Matador', 'Barricade'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        Very flexible in chosing the minions which will be buffed, because it is very easy to buff any minion with Taunt.
+        These are just an example of minions that make use of the stats given from Walking Fort and synergize with one another. You can always make the taunted minions be minions with special abilities like Divine Shield or Cleave. Wayward Grimscale, Impulsive Trickster or Foe Reaper 4000 are just some of the options.
     </p>
-    <h3 id="mythrax">Mythrax Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="naala">Naala Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Reaper', 'Mythrax', 'Mythrax', 'Phalanx', 'Phalanx', 'Enchanter', 'Glowscale'];
+    $board   = ['Naala', 'Fungus', 'Lubber', 'Silivaz', 'Sporebat', 'Specialist', 'Seafarer'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        The Reaper can also be a <a class="hoverimage" href="/bgstrategy/minion/?id=327">Blade Collector</a> when Pirates are available.
+        Other good alternate cards are Bazaar Dealer and Trembling Trolley. You don't necessarily need Ensorcelled Fungus but it does scale quite fast. Try to focus on spells that are cheap, give you access to economy, or find more spells.
     </p>
-    <h3 id="teamaster">Tea Master Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="scam">Scam Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Leeroy', 'Reaper', 'Theo', 'Theo', 'Murghoul', 'Glowscale', 'Phalanx'];
+    $board   = ['Bramblewitch', 'Bramblewitch', 'Thrasher', 'Thrasher', 'Matador', 'Matador', 'Murghoul'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        The Reaper can also be a <a class="hoverimage" href="/bgstrategy/minion/?id=327">Blade Collector</a> when Pirates are available.
+        This is a build used if you just cannot find an actual build that scales. This won't win against every build, but it is very effective if your opponent only has 7 strong minions without Deathrattles, Reborns, Divine Shields, or Cleaves.
     </p>
+    <h3 id="deathrattle">Deathrattle Menagerie Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <?php
+    $board   = ['Phalanx', 'Kel', 'Belcher', 'Kangor\'s', 'S\'thara', 'Hawkstrider', 'Titus'];
+    $minions = getMinionsForBoard($board);
+    drawBoard($minions);
+    ?>
+    <p>
+        If you don't have Archlich Kel'Thuzad, be sure to use Sprightly Scarab or Mecha-Jaraxxus to give it Reborn. If you can't find Kangor's Apprentice or Cultist S'thara, you can still replace them with cards of other minion types.
+    </p>
+    <!--    <h3 id="mythrax">Mythrax Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>-->
+    <!--    --><?php
+    //    $board   = ['Reaper', 'Mythrax', 'Mythrax', 'Phalanx', 'Phalanx', 'Enchanter', 'Glowscale'];
+    //    $minions = getMinionsForBoard($board);
+    //    drawBoard($minions);
+    //    ?>
+    <!--    <p>-->
+    <!--        The Reaper can also be a <a class="hoverimage" href="/bgstrategy/minion/?id=327">Blade Collector</a> when Pirates are available.-->
+    <!--    </p>-->
+    <!--    <h3 id="teamaster">Tea Master Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>-->
+    <!--    --><?php
+    //    $board   = ['Leeroy', 'Reaper', 'Theo', 'Theo', 'Murghoul', 'Glowscale', 'Phalanx'];
+    //    $minions = getMinionsForBoard($board);
+    //    drawBoard($minions);
+    //    ?>
+    <!--    <p>-->
+    <!--        The Reaper can also be a <a class="hoverimage" href="/bgstrategy/minion/?id=327">Blade Collector</a> when Pirates are available.-->
+    <!--    </p>-->
     <!--    <h3 id="boogie">Boogie Monster Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>-->
     <!--    --><?php
     //    $board   = ['Leeroy', 'Reaper', 'Boogie', 'Boogie', 'Murghoul', 'Glowscale', 'Leeroy'];
@@ -53,15 +80,15 @@ include_once('../header.php');
     <!--    <p>-->
     <!--        The Reaper can also be a <a class="hoverimage" href="/bgstrategy/minion/?id=327">Blade Collector</a> when Pirates are available.-->
     <!--    </p>-->
-    <h3 id="braggart">Braggart KekWait Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
-    <?php
-    $board   = ['Braggart', 'Braggart', 'Braggart', 'Braggart', 'Braggart', 'Braggart', 'Braggart'];
-    $minions = getMinionsForBoard($board);
-    drawBoard($minions);
-    ?>
-    <p>
-        Only possible with the anomalies <a href="/bgstrategy/anomaly/?id=13">A Faire Reward</a> and <a href="/bgstrategy/anomaly/?id=8">Secrets of Norgannon</a>. ;-)
-    </p>
+    <!--    <h3 id="braggart">Braggart KekWait Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>-->
+    <!--    --><?php
+    //    $board   = ['Braggart', 'Braggart', 'Braggart', 'Braggart', 'Braggart', 'Braggart', 'Braggart'];
+    //    $minions = getMinionsForBoard($board);
+    //    drawBoard($minions);
+    //    ?>
+    <!--    <p>-->
+    <!--        Only possible with the anomalies <a href="/bgstrategy/anomaly/?id=13">A Faire Reward</a> and <a href="/bgstrategy/anomaly/?id=8">Secrets of Norgannon</a>. ;-)-->
+    <!--    </p>-->
     <!--    <h3 id="lightfang">Lightfang Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>-->
     <!--    --><?php
     //    $board   = ['Reaper', '?Collector', 'Phalanx', 'Glowscale', 'Lightfang', 'Lightfang', 'Enchanter'];
@@ -71,9 +98,16 @@ include_once('../header.php');
     <!--    <p>-->
     <!--        The Reaper can also be a <a class="hoverimage" href="/bgstrategy/minion/?id=327">Blade Collector</a> when Pirates are available.-->
     <!--    </p>-->
-    <br>
+    </p>
 </div>
 
-<?php
-include_once('../footer.php');
-?>
+<br>
+<br>
+<hr>
+<br>
+<p>
+    <?php include_once('outro.php'); ?>
+</p>
+<br><br>
+
+<?php include_once('../footer.php'); ?>
