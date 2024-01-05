02e14bbc32f14fe95eaa87bc21b9fe02583e7d54
O-Nemet <github@nemet.online>
2024-01-05 17:36:27 +0100

    Updated mobile menu link from anomalies to spells

--- header.php
+++ header.php
@@ -62,7 +62,7 @@ $lastURL = isset($_SERVER['HTTP_REFERER']) ?? null;
                 <li><a class="menu-topic" href="/bgstrategy/general.php">&#9654; General Strategy</a></li>
                 <li><a class="menu-topic" href="/bgstrategy/?show=heroes">&#9654; Hero Strategy</a></li>
                 <li><a class="menu-topic" href="/bgstrategy/?show=minions">&#9654; Minion Strategy</a></li>
-                <li><a class="menu-topic" href="/bgstrategy/?show=anomalies">&#9654; Anomaly Strategy</a></li>
+                <li><a class="menu-topic" href="/bgstrategy/?show=spells">&#9654; Spell Strategy</a></li>
                 <li><a class="menu-topic" href="/bgcomps/">&#9654; Compositions</a></li>
                 <li><a class="menu-topic" href="/bgbasics/armor.php">&#9654; Hero Armor</a></li>
                 <li><a class="menu-topic" href="/bgcurves/">&#9654; Curves</a></li>
