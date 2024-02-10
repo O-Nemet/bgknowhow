5edb347830eedcff8281c41b93f6053ced59a8aa
O-Nemet <github@nemet.online>
2024-02-10 01:55:39 +0100

    Updated quilboar and dragon comps

--- bgcomps/comp_dragons.php
+++ bgcomps/comp_dragons.php
@@ -27,15 +27,25 @@
         Other important cards: <a class='hoverimage' href='/bgstrategy/spell/?id=23'>Brann's Blessing</a>, <a class='hoverimage' href='/bgstrategy/spell/?id=27'>Hired Headhunter</a> and <a class='hoverimage' href='/bgstrategy/spell/?id=32'>Dreamer's Embrace</a> will help your build.
     </p>
 
-    <h3 id="drakkari">End of Turn Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="sandstone">Sandstone Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Razorgore', 'Smolderer', 'Drake', 'Drake', 'Smuggler', 'Hunter', 'Enchanter'];
+    $board   = ['Sandstone', 'Sandstone', 'Smuggler', 'Smuggler', 'Master', 'Master', 'Enchanter'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        This build may be a bit slow at first, but can really get going once you have <strong>Drakkari Enchanter</strong> or the spell <a class='hoverimage' href='/bgstrategy/spell/?id=25'>Primal Staff</a> (they do not stack, though). Just make sure that you leave a free space, so you can use the Smolderwings you gain from Outback Smolderer. If you do triple everything and still got free space, <a class='hoverimage' href='/bgstrategy/minion/?id=454'>Ignition Specialist</a> can work very well.
+        Needs the availability of Elementals. Do not triple <strong>Sandstone Drake</strong> or <strong>Master Realities</strong>, they become less effective when put together. <strong>Drakkari Enchanter</strong> accelerates your comp strength into the endgame.</a>.
     </p>
+
+    <!--    <h3 id="drakkari">End of Turn Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>-->
+    <!--    --><?php
+    //    $board   = ['Razorgore', 'Smolderer', 'Ignition Specialist', 'Drake', 'Smuggler', 'Hunter', 'Enchanter'];
+    //    $minions = getMinionsForBoard($board);
+    //    drawBoard($minions);
+    //    ?>
+    <!--    <p>-->
+    <!--        This build may be a bit slow at first, but can really get going once you have <strong>Drakkari Enchanter</strong> or the spell <a class='hoverimage' href='/bgstrategy/spell/?id=25'>Primal Staff</a> (they do not stack, though). Just make sure that you leave a free space, so you can use the Smolderwings you gain from Outback Smolderer. If you do triple everything and still got free space, <a class='hoverimage' href='/bgstrategy/minion/?id=454'>Ignition Specialist</a> can work very well.-->
+    <!--    </p>-->
 </div>
 
 <br><br>
--- bgcomps/comp_quilboars.php
+++ bgcomps/comp_quilboars.php
@@ -19,6 +19,26 @@
     <!--    Quilboar also enable the so called 'Menagerie' builds via <a class="hoverimage" href="/bgstrategy/minion/?id=105">Aggem Thorncurse</a> where you try to pick the best minions of each type.-->
 </p>
 <div class="comp_wrapper">
+    <h3 id="roogug">Roogug Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <?php
+    $board   = ['Tusk', 'Roogug', 'Roogug', 'Smudger', 'Smudger', 'Lubber', 'Piper'];
+    $minions = getMinionsForBoard($board);
+    drawBoard($minions);
+    ?>
+    <p>
+        This is a very good build to use when you are not doing well and want to stick behind at Tier 3/4. Each card is a keypiece, so you have to really balance and know at which time of the game is worth recycling. <a class="hoverimage" href="/bgstrategy/minion/?id=321">Moon-Bacon Jazzer</a>s are especially important, as the additional health to each Blood Gem and the gem procs from <strong>Roogug</strong> will go a long way. <a class="hoverimage" href="/bgstrategy/minion/?id=90">Necrolyte</a> is also important to ensure a divine shield minion gains a bunch of stats.
+    </p>
+
+    <h3 id="roogug">Roogug Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <?php
+    $board   = ['Piper', 'Roogug', 'Roogug', 'Spearhide', 'Bandit', 'Bopper', 'Charlga'];
+    $minions = getMinionsForBoard($board);
+    drawBoard($minions);
+    ?>
+    <p>
+        Any Quilboar that generates Blood Gems is good, so don't think this is the only way to build this comp. You can also have a <a class="hoverimage" href="/bgstrategy/minion/?id=319">Rylak Metalhead</a> be paired next to <a class="hoverimage" href="/bgstrategy/minion/?id=321">Moon-Bacon Jazzer</a> and a golden <a class="hoverimage" href="/bgstrategy/minion/?id=8">Razorfen Geomancer</a>. You can have <a class="hoverimage" href="/bgstrategy/minion/?id=376">Briarback Bookie</a> or use <a class="hoverimage" href="/bgstrategy/minion/?id=45">Bannerboar</a> as well. Either way, you want to put as many Blood Gems onto <strong>Geomagus Roogug</strong> as you can. The only issue with this build is, that the other targets for his gem procs are suboptimal. So do try to put other cards such as a cleave or divine shield.
+    </p>
+
     <h3 id="bristlebach">Bristlebach Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
     $board   = ['Tusk', 'Tusk', 'Piper', 'Spearhide', 'Spearhide', 'Roogug', 'Bristlebach'];
