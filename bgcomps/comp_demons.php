ee9320cc92d4917162b9d4f4edcb61d91d2d7ed2
O-Nemet <github@nemet.online>
2024-04-17 02:02:47 +0200

    Updated demon comps after patch 29.2.0

--- bgcomps/comp_demons.php
+++ bgcomps/comp_demons.php
@@ -13,9 +13,9 @@
     In general Demon compositions utilize their many self-damaging effects available for this minion type and their general stat scaling to build huge minions. They are pretty self-reliant (do not need another minion type to shine) but without picking up an early <a class="hoverimage" href="/bgstrategy/minion/?id=310">Soul Rewinder</a> you will usually take too much damage.
 </p>
 <div class="comp_wrapper">
-    <h3 id="tichondrius">Self Damage Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="tichondrius">Self-Damage Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Weaver', 'Malchezaar', 'Malchezaar', 'Rewinder', 'Watcher', 'Watcher', 'Tichondrius'];
+    $board   = ['Weaver', 'Watcher', 'Rewinder', 'Malchezaar', 'Bazaar Dealer', 'Aranasi', 'Tichondrius'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
@@ -24,52 +24,19 @@
         <br><br>
         Your build becomes stronger, the more self damage you do. Having more than one Wrath Weaver is good, but do not be afraid to triple it if you really need to find your <strong>Tichondrius</strong>. While Tichondrius is great to buff something like Impulsive Trickster, be wary of the board space as you may not have enough room to recycle a card and keep these other demons.
         <br><br>
-        The minions <a class="hoverimage" href="/bgstrategy/minion/?id=394">Dreadbeard</a> and <a class="hoverimage" href="/bgstrategy/minion/?id=437">Bazaar Dealer</a> are suitable for this kind of build during the mid-game, but will usually be replaced later on.
-        <br><br>
         For the very late game <a class="hoverimage" href="/bgstrategy/minion/?id=426">Cultist S'thera</a> with <a class="hoverimage" href="/bgstrategy/minion/?id=256">Titus Rivendare</a> is also good if you have a big demon you want to resummon, but it does get countered by <a class="hoverimage" href="/bgstrategy/minion/?id=398">Transmuted Bramblewitch</a>.
     </p>
 
     <h3 id="felbat">Felbat Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Trickster', 'Felbat', 'Legion Overseer', 'Legion Overseer', 'Felemental', 'S\'thara', 'Titus'];
+    $board   = ['Trickster', 'Fiery', 'Aranasi', 'Felbat', 'S\'thara', 'Titus', 'Urzul'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        Taunt the <strong>Impulsive Trickster</strong> to profit from the Cultist and <strong>Titus</strong>. If Beasts are available, having <a class="hoverimage" href="/bgstrategy/minion/?id=319">Rylak Metalhead</a> inbetween two Felementals will help the build further. <a class="hoverimage" href="/bgstrategy/minion/?id=356">Elemental of Surprise</a> is great to triple the two Felementals as well.
-        <br><br>
-        <a class="hoverimage" href="/bgstrategy/minion/?id=345">Drakkari Enchanter</a> can help, but be wary on how that works with Felemental. You can only have three demons, as each demon will eat twice at the end of the turn. Having two <strong>Felbats</strong> with one Drakkari Enchanter only works if Felbat is your only demon.
+        Taunt the <strong>Impulsive Trickster</strong> to profit from the Cultist and <strong>Titus</strong>. Improve the Tavern's health by playing multiple <a class="hoverimage" href="/bgstrategy/minion/?id=476">Slimy Felblood</a> early on.
     </p>
 
-    <h3 id="urzul">Ur'zul Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
-    <?php
-    $board   = ['Ur\'zul', 'Weaver', 'Rewinder', 'Felbat', 'Legion Overseer', 'S\'thara', 'Ur\'zul'];
-    $minions = getMinionsForBoard($board);
-    drawBoard($minions);
-    ?>
-    <p>
-        Needs a buildup in order to buff all minions in the Tavern via <a class="hoverimage" href="/bgstrategy/minion/?id=259">Felemental</a> and any other means available. Two Insatiable <strong>Ur'zul</strong> are usually better than a golden one due to venomous/poison. Don't forget to utilize <a class="hoverimage" href="/bgstrategy/minion/?id=260">Mecha-Jaraxxus</a> for double Ur'zul triggers and magnetic buffs. Gets weaker if opponents are playing <a class="hoverimage" href="/bgstrategy/minion/?id=398">Transmuted Bramblewitch</a>.
-    </p>
-
-    <h3 id="trickster">Macaw/Trickster Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
-    <?php
-    $board   = ['Macaw', 'Macaw', 'Trickster', 'Hawkstrider', 'Hawkstrider', 'S\'thara', 'Titus'];
-    $minions = getMinionsForBoard($board);
-    drawBoard($minions);
-    ?>
-    <p>
-        Needs the availability of Beasts. Needs a heavily buffed and optimally golden <strong>Impulsive Trickster</strong> as well as either <strong>Titus</strong> or the spell <a class="hoverimage" href="/bgstrategy/spell/?id=38">Titus' Tribute</a>.
-    </p>
-
-    <h3 id="demoneye">Feldrake/Murk-Eye Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
-    <?php
-    $board   = ['Feldrake', 'Murk-Eye', 'Feldrake', 'S\'thara', 'Legion Overseer', 'Titus', 'Queen'];
-    $minions = getMinionsForBoard($board);
-    drawBoard($minions);
-    ?>
-    <p>
-        Needs the availability of Murlocs. Buff the Tavern as much as possible to scale your <strong>Feldrakes</strong> every turn via the <strong>Murk-Eye</strong>.
-    </p>
 </div>
 
 <br><br>
