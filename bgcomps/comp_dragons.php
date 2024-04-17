a32e3a81a722370448210bd2fddb97682b5fdb7f
O-Nemet <github@nemet.online>
2024-04-17 19:09:11 +0200

    Updated dragon comps after patch 29.2.0

--- bgcomps/comp_dragons.php
+++ bgcomps/comp_dragons.php
@@ -15,37 +15,36 @@
 <div class="comp_wrapper">
     <h3 id="kaly">Kalecgos Battlecry Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Warpwing', 'Kaly', 'Kaly', 'Amber Guardian', 'Hunter', 'Brann', 'Smolderer'];
+    $board   = ['Deathscale', 'Yu\'lon', 'Kaly', 'Drakkisath', 'Rylak', 'Drakkisath', 'Brann'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        After securing the key minions <strong>Kalecgos</strong> and <strong>Brann</strong>, focus on getting as many battlecries as you can. When you do activate battlecries, especially ones that give stats, put them on <strong>Hunter of Gatherers</strong> so he can give stats to everyone on the board.
+        After securing the key minions <strong>Kalecgos</strong> and <strong>Brann</strong>, focus on getting as many battlecries as you can.
         <br><br>
-        Instead of the Smolderer it is also possible to combine <a class='hoverimage' href='/bgstrategy/spell/?id=263'>General Drakkisath</a> with either <a class="hoverimage" href="/bgstrategy/minion/?id=319">Rylak Metalhead</a> (when Beasts are available) or <a class='hoverimage' href='/bgstrategy/minion/?id=209'>Young Murk-Eye</a> (when Murlocs are available).
+        <strong>Rylak</strong> can also be replaced with <a class='hoverimage' href='/bgstrategy/minion/?id=209'>Young Murk-Eye</a> (when Murlocs are available).
         <br><br>
-        Other important cards: <a class='hoverimage' href='/bgstrategy/spell/?id=23'>Brann's Blessing</a>, <a class='hoverimage' href='/bgstrategy/spell/?id=27'>Hired Headhunter</a> and <a class='hoverimage' href='/bgstrategy/spell/?id=32'>Dreamer's Embrace</a> will help your build.
+        Other important cards: <a class='hoverimage' href='/bgstrategy/spell/?id=27'>Hired Headhunter</a> and <a class='hoverimage' href='/bgstrategy/spell/?id=32'>Dreamer's Embrace</a> will help your build.
     </p>
 
-    <h3 id="sandstone">Sandstone Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="poet"">Poet Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Sandstone', 'Sandstone', 'Smuggler', 'Smuggler', 'Master', 'Master', 'Enchanter'];
+    $board   = ['Hatespawn', 'Poet', 'Yu\'lon', 'Amber Guardian', 'Kaly', 'Brann', 'Nightbane'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        Needs the availability of Elementals. Do not triple <strong>Sandstone Drake</strong> or <strong>Master Realities</strong>, they become less effective when put together. <strong>Drakkari Enchanter</strong> accelerates your comp strength into the endgame.
     </p>
 
-    <!--    <h3 id="drakkari">End of Turn Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>-->
-    <!--    --><?php
-    //    $board   = ['Razorgore', 'Smolderer', 'Ignition Specialist', 'Drake', 'Smuggler', 'Hunter', 'Enchanter'];
-    //    $minions = getMinionsForBoard($board);
-    //    drawBoard($minions);
-    //    ?>
-    <!--    <p>-->
-    <!--        This build may be a bit slow at first, but can really get going once you have <strong>Drakkari Enchanter</strong> or the spell <a class='hoverimage' href='/bgstrategy/spell/?id=25'>Primal Staff</a> (they do not stack, though). Just make sure that you leave a free space, so you can use the Smolderwings you gain from Outback Smolderer. If you do triple everything and still got free space, <a class='hoverimage' href='/bgstrategy/minion/?id=454'>Ignition Specialist</a> can work very well.-->
-    <!--    </p>-->
+    <h3 id="tarecgosa">Tarecgosa Buff Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <?php
+    $board   = ['Tarecgosa', 'Tarecgosa', 'Promo-Drake', 'Promo-Drake', 'Amber Guardian', 'Poet', 'Rallier'];
+    $minions = getMinionsForBoard($board);
+    drawBoard($minions);
+    ?>
+    <p>
+    </p>
+
 </div>
 
 <br><br>
