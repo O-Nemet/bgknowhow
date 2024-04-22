2e6b3e003ac4403b9bde8396192532789debc8b1
O-Nemet <github@nemet.online>
2024-04-22 20:20:34 +0200

    Added guide page and explanation linking to Jeef's comp spreadsheet/tierlist

--- bgguides/jeef.php
+++ bgguides/jeef.php
@@ -6,7 +6,7 @@ include_once('../header.php');
 <p>
     For many seasons of Battlegrounds US-based streamer <a href="https://www.twitch.tv/jeefhs" target="_blank">Jeef</a> has proven to be one of the best if not the best player in the game. At the end of BG Season 6 he began publishing a <a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vSlzd9R10rNw4rK-7pKDwz2QXUjiOyQpozmukDS31AGoj-pc_rpb_kgYxk0JkdoHJApeU0GYypM6p-D/pubhtml" target="_blank">composition tier list</a>, ranking the different endgame builds by strength and difficulty to play. Every comp features a link pointing out the core minions as well as possible addons to your board.
     <br><br>
-    His two extensive videos on <a href="https://www.youtube.com/watch?v=foZx60jbC74" target="_blank">Direction and <a href="https://www.youtube.com/watch?v=A0EqnsBkClo" target="_blank">Leveling</a> accompany this information and will provide you with insights into his general thought process.
+    His two extensive videos on <a href="https://www.youtube.com/watch?v=foZx60jbC74" target="_blank">Direction</a> and <a href="https://www.youtube.com/watch?v=A0EqnsBkClo" target="_blank">Leveling</a> accompany this information and will provide you with insights into his general thought process.
 </p>
 
 <?php
