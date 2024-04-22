41bd8322c62b3abf62fd16b91f4484639db089d2
O-Nemet <github@nemet.online>
2024-04-22 20:31:07 +0200

    Updated YT guides after patch 29.2.0

--- bgguides/youtube.php
+++ bgguides/youtube.php
@@ -8,15 +8,9 @@ include_once('../header.php');
     </p>
     <div class="streamers">
         <div class="iframe_div">
-            <h3>“How Rank 1 Chooses Quests” by <a href="https://www.youtube.com/@JeefHS/videos" target="_blank">JeefHS</a></h3>
+            <h3>“Beginner's Guide to Hearthstone Battlegrounds Duos” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/7lKi_prLeV8?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
-        <div class="iframe_div">
-            <h3>“Rank 1's Quest Tierlist” by <a href="https://www.youtube.com/@JeefHS/videos" target="_blank">JeefHS</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/zWMhLfe6zC8?autoplay=0&origin=https://bgknowhow.com">
+                    src="https://www.youtube.com/embed/M0hdb_wCZSA?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
         <div class="iframe_div">
@@ -26,27 +20,9 @@ include_once('../header.php');
             </iframe>
         </div>
         <div class="iframe_div">
-            <h3>“Top 5 Comps in Spell Meta” by <a href="https://www.youtube.com/@JeefHS/videos" target="_blank">JeefHS</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/DHXAONa7oAw?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
-        <div class="iframe_div">
-            <h3>“Which Hero to play?! Quest Meta” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/PHbkUcZs2Co?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
-        <div class="iframe_div">
             <h3>“When to Pivot / Counter in HS BG Back to Basics” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/BpPrCqJbA0c?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
-        <div class="iframe_div">
-            <h3>“New To Buddies? What Buddies are Good, and What They Can do!” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/0CqAhKULF3w?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
         <div class="iframe_div">
@@ -62,23 +38,17 @@ include_once('../header.php');
             </iframe>
         </div>
         <div class="iframe_div">
+            <h3>“Leveling explained by a pro” by <a href="https://www.youtube.com/@JeefHS/videos" target="_blank">JeefHS</a></h3>
+            <iframe id="ytplayer" type="text/html" width="800" height="450"
+                    src="https://www.youtube.com/embed/A0EqnsBkClo?autoplay=0&origin=https://bgknowhow.com">
+            </iframe>
+        </div>
+        <div class="iframe_div">
             <h3>“Mastering Hearthstone Battlegrounds: Break Free from Bad Habits” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/J2JOpQhLvhQ?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
-        <!--        <div class="iframe_div">-->
-        <!--            <h3>“Discussing all the quests (Patch 24.2.2)” by <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank">Mewwy</a></h3>-->
-        <!--            <iframe id="ytplayer" type="text/html" width="800" height="450"-->
-        <!--                    src="https://www.youtube.com/embed/KWK8lV1HzUM?autoplay=0&origin=https://bgknowhow.com">-->
-        <!--            </iframe>-->
-        <!--        </div>-->
-        <!--        <div class="iframe_div">-->
-        <!--            <h3>“Discussing all the new quests (Patch 27.2.0)” by <a href="https://www.youtube.com/channel/UCZeaUP1fx636UFuM9DrpGqA/videos" target="_blank">Mewwy</a></h3>-->
-        <!--            <iframe id="ytplayer" type="text/html" width="800" height="450"-->
-        <!--                    src="https://www.youtube.com/embed/EPq0WCKgW5E?autoplay=0&origin=https://bgknowhow.com">-->
-        <!--            </iframe>-->
-        <!--        </div>-->
         <div class="iframe_div">
             <h3>“How to Choose the Best Hero?” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
@@ -95,12 +65,6 @@ include_once('../header.php');
             <h3>“Back to Basics: When to Level?” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/Of6WSiCw_64?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
-        <div class="iframe_div">
-            <h3>“Leveling explained by a pro” by <a href="https://www.youtube.com/@JeefHS/videos" target="_blank">JeefHS</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/A0EqnsBkClo?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
         <div class="iframe_div">
@@ -134,12 +98,6 @@ include_once('../header.php');
             </iframe>
         </div>
         <div class="iframe_div">
-            <h3>“Beast Back to Basics Guide” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/U7usJIncjdc?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
-        <div class="iframe_div">
             <h3>“Menagerie Builds Back to Basics Guide” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/hHKpy6qwpJU?autoplay=0&origin=https://bgknowhow.com">
@@ -149,12 +107,6 @@ include_once('../header.php');
             <h3>“Murloc Back to Basics Guide” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
                     src="https://www.youtube.com/embed/JUhXCzenx6M?autoplay=0&origin=https://bgknowhow.com">
-            </iframe>
-        </div>
-        <div class="iframe_div">
-            <h3>“Dragon Back to Basics Guide” by <a href="https://www.youtube.com/@Heretic721/videos" target="_blank">Heretic721</a></h3>
-            <iframe id="ytplayer" type="text/html" width="800" height="450"
-                    src="https://www.youtube.com/embed/sQSb_uNITn0?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
         <div class="iframe_div">
@@ -199,12 +151,6 @@ include_once('../header.php');
                     src="https://www.youtube.com/embed/u8-3rJ_XjXI?autoplay=0&origin=https://bgknowhow.com">
             </iframe>
         </div>
-        <!--        <div class="iframe_div">-->
-        <!--            <h3>“Wondrous Wisdomball Quest Reward Guide” by <a href="https://www.youtube.com/c/PockyPlays" target="_blank">Pocky</a></h3>-->
-        <!--            <iframe id="ytplayer" type="text/html" width="800" height="450"-->
-        <!--                    src="https://www.youtube.com/embed/xU0PhK_ueKk?autoplay=0&origin=https://bgknowhow.com">-->
-        <!--            </iframe>-->
-        <!--        </div>-->
         <div class="iframe_div">
             <h3>“The Ranking System (MMR) Explained for Hearthstone Battlegrounds” by <a href="https://www.youtube.com/c/ItsBen321" target="_blank">ItsBen</a></h3>
             <iframe id="ytplayer" type="text/html" width="800" height="450"
