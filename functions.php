4380b9dbcf9e5263aa445e4631a3aa15abacf6fe
O-Nemet <github@nemet.online>
2024-05-05 14:18:34 +0200

    Added title for duos page

--- functions.php
+++ functions.php
@@ -146,6 +146,8 @@ function getWebsiteTitle(): string
         $title .= 'Comps (Undeads)';
     } else if (strpos($page, '/bgcomps/comp_neutrals') !== false) {
         $title .= 'Comps (Neutrals)';
+    } else if (strpos($page, '/bgduos/') !== false) {
+        $title .= 'Duos Mode';
     } else if (strpos($page, '/bgcomps/') !== false) {
         $title .= 'Compositions';
     } else if (strpos($page, '/bgcurves/') !== false) {
@@ -446,11 +448,6 @@ function drawBoard($minions): void
     foreach ($minions as $minion) {
         echo '<a href="' . $minion['url'] . '"><img src="' . $minion['picture'] . '" class="' . (!$minion['active'] ? 'inactive-img' : '') . '" alt="' . $minion['name'] . '" title=""></a>';
     }
-}
-
-function getCompositionText(): string
-{
-    return "These different compositions are meant to display proven setups to strive for, for the very end game (top 4 and above). In general one of the seven slots will be the 'flex' spot, used to cycle new minions during the tavern rounds. Therefore, your actual board will rarely be as perfect as these listed here. Of course as many minions as possible should be tripled and buffed with Reborn, Venomous, Divine Shield or Taunt.<br><br>Also primary support units like <a class='hoverimage' href='/bgstrategy/minion/?id=109'>Brann</a> and <a class='hoverimage' href='/bgstrategy/minion/?id=121'>Nomi</a> will usually be tossed for the very last fights, but are sometimes displayed here when being integral to the setup. If one of your units is lacking, it is also often beneficial to replace it with a <a class='hoverimage' href='/bgstrategy/minion/?id=208'>Leeroy</a> or a <a class='hoverimage' href='/bgstrategy/minion/?id=399'>Relentless Mur'ghoul</a>, before a deciding fight.";
 }
 
 function convertStrategyText(string $text): string
