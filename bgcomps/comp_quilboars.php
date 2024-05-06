0285c7789ce6c823b66e5432a8b736db74b05807
mewmatic <72135538+mewmatic@users.noreply.github.com>
2024-05-06 09:32:48 -0700

    Update comp_quilboars.php

--- bgcomps/comp_quilboars.php
+++ bgcomps/comp_quilboars.php
@@ -21,7 +21,7 @@
 <div class="comp_wrapper">
     <h3 id="roogug">Roogug Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Piper', 'Roogug', 'Roogug', 'Bandit', 'Bandit', 'Bopper', 'Charlga'];
+    $board   = ['Piper', 'Roogug', 'Pokey', 'Bandit', 'Bandit', 'Bopper', 'Charlga'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
