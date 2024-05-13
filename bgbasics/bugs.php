b42f5c5813c86535fdc77e9e3d12d955c4ca435e
O-Nemet <github@nemet.online>
2024-05-14 00:31:51 +0200

    Added bugs subpage to the basic navigation

--- bgbasics/bugs.php
+++ bgbasics/bugs.php
@@ -3,7 +3,7 @@ include_once('../header.php');
 ?>
 <h2 class="page_title">Known Bugs</h2>
 <br>
-<div>
+<div class="bugs">
     <p>
         Here is a list of currently known/active bugs with gameplay impact in Hearthstone Battlegrounds:
     </p>
