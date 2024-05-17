fb22b94d7bfe8b0e32d58b2916f1a236870c4038
O-Nemet <github@nemet.online>
2024-05-17 08:48:59 +0200

    Updated bug status

--- bgbasics/bugs.php
+++ bgbasics/bugs.php
@@ -8,9 +8,12 @@ include_once('../header.php');
         Here is a list of currently known/active bugs with gameplay impact in Hearthstone Battlegrounds:
     </p>
     <ul>
-        <li>When trippling <a class="hoverimage" href="/bgstrategy/minion/?id=470">Winged Chimera</a> with <a class="hoverimage" href="/bgstrategy/minion/?id=356">Elemental of Surprise</a> its ability counter will be set to 0 and it will lose its ability to buff your minions, permanently.</li>
         <li>When you have multiple <a class="hoverimage" href="/bgstrategy/minion/?id=475">Timecap'n Hooktail</a> on your board, minions with buff effects triggered by it (like <a class="hoverimage" href="/bgstrategy/minion/?id=42">Whelp Smuggler</a> or <a class="hoverimage" href="/bgstrategy/minion/?id=535">Defiant Shipwright</a>) will not proc for each instance, but only once.</li>
-        <!--        <li></li>-->
+        <li>Duos: The hero power of <a class="hoverimage" href="/bgstrategy/hero/?id=64">Tamsin Roame</a> has a chance to consume two minions instead of one.</li>
+        <li>
+            <del>When trippling <a class="hoverimage" href="/bgstrategy/minion/?id=470">Winged Chimera</a> with <a class="hoverimage" href="/bgstrategy/minion/?id=356">Elemental of Surprise</a> its ability counter will be set to 0 and it will lose its ability to buff your minions, permanently.</del>
+            (fixed in 29.4.1)
+        </li>
     </ul>
     <br><br>
     <p>
