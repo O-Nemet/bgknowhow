587e2f597361ef99608331175d390b52607c506f
Oliver Nemet <103574689+O-Nemet@users.noreply.github.com>
2024-06-18 01:02:38 +0200

    Edited comp link ids

--- bgcomps/comp_demons.php
+++ bgcomps/comp_demons.php
@@ -27,7 +27,7 @@
         For the very late game <a class="hoverimage" href="/bgstrategy/minion/?id=426">Cultist S'thera</a> with <a class="hoverimage" href="/bgstrategy/minion/?id=256">Titus Rivendare</a> is also good if you have a big demon you want to resummon, but it does get countered by <a class="hoverimage" href="/bgstrategy/minion/?id=398">Transmuted Bramblewitch</a>.
     </p>
 
-    <h3 id="felbat">Felbat Deathrattle Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="felbatdr">Felbat Deathrattle Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
     $board   = ['Trickster', 'Fiery', 'Aranasi', 'Felbat', 'S\'thara', 'Titus', 'Ur\'zul'];
     $minions = getMinionsForBoard($board);
@@ -36,15 +36,15 @@
     <p>
         Taunt the <strong>Impulsive Trickster</strong> to profit from the Cultist and <strong>Titus</strong>. Improve the Tavern's health by playing multiple <a class="hoverimage" href="/bgstrategy/minion/?id=476">Slimy Felblood</a> early on.
     </p>
-  <h3 id="felbat">Felbat End of Turn Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
-    <?php
-    $board   = ['Fiery', 'Felbat', 'Felbat', 'Leeroy', 'Bramblewitch', 'S\'thara', 'S\'thara'];
-    $minions = getMinionsForBoard($board);
-    drawBoard($minions);
-    ?>
-    <p>
-        This is a build if you manage to get double <strong>Famished Felbat</strong>. By having two Felbats and one other Demon, it will eat the entire shop. Tier-6 is required so that you can eat 6 minions instead of 5. If you triple the Felbat, then start getting other Demons and <a class="hoverimage" href="/bgstrategy/minion/?id=345">Drakkari Enchanter</a>.
-    </p>        
+      <h3 id="felbateot">Felbat End of Turn Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+        <?php
+        $board   = ['Fiery', 'Felbat', 'Felbat', 'Leeroy', 'Bramblewitch', 'S\'thara', 'S\'thara'];
+        $minions = getMinionsForBoard($board);
+        drawBoard($minions);
+        ?>
+        <p>
+            This is a build if you manage to get double <strong>Famished Felbat</strong>. By having two Felbats and one other Demon, it will eat the entire shop. Tier-6 is required so that you can eat 6 minions instead of 5. If you triple the Felbat, then start getting other Demons and <a class="hoverimage" href="/bgstrategy/minion/?id=345">Drakkari Enchanter</a>.
+        </p>        
 </div>
 
 <br><br>
