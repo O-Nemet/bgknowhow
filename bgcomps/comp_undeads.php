ed46ccb55a4296c69d166c3d5b69514eef69eb2b
Oliver Nemet <103574689+O-Nemet@users.noreply.github.com>
2024-06-18 01:35:46 +0200

    Update bgcomps/comp_undeads.php

--- bgcomps/comp_undeads.php
+++ bgcomps/comp_undeads.php
@@ -10,7 +10,7 @@
 <p>
     <a href="/bgstrategy/?show=minions&type=undead"><i class="bi bi-box-arrow-up-right"></i> All available Undeads and Undead-associated minions</a>
     <br><br>
-    Undead shine with many Reborn and Deathrattle effects, usually providing you more minions than your opponents. They are able to buff the attack of every Undead permanently and the minion <a class="hoverimage" href="/bgstrategy/minion/?id=444">Disguised Graverobber</a> provides a unique way to alter and buff your warband. Prime Mate also helps in almost all of the builds.
+    Undead shine with many Reborn and Deathrattle effects, usually providing you more minions than your opponents. They are able to buff the attack of every Undead permanently and the minion <a class="hoverimage" href="/bgstrategy/minion/?id=444">Disguised Graverobber</a> provides a unique way to alter and buff your warband. <a class="hoverimage" href="/bgstrategy/minion/?id=539">Prime Mate</a> also helps in almost all of the builds.
 </p>
 <div class="comp_wrapper">
     <h3 id="knights">Deathrattle Knight Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
