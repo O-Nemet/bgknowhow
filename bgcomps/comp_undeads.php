045844842f3ee9438acc630809bd117ca5519ef7
mewmatic <72135538+mewmatic@users.noreply.github.com>
2024-06-17 15:13:29 -0700

    Update comp_undeads.php

--- bgcomps/comp_undeads.php
+++ bgcomps/comp_undeads.php
@@ -10,7 +10,7 @@
 <p>
     <a href="/bgstrategy/?show=minions&type=undead"><i class="bi bi-box-arrow-up-right"></i> All available Undeads and Undead-associated minions</a>
     <br><br>
-    Undead shine with many Reborn and Deathrattle effects, usually providing you more minions than your opponents. They are able to buff the attack of every Undead permanently and the minion <a class="hoverimage" href="/bgstrategy/minion/?id=444">Disguised Graverobber</a> provides a unique way to alter and buff your warband.
+    Undead shine with many Reborn and Deathrattle effects, usually providing you more minions than your opponents. They are able to buff the attack of every Undead permanently and the minion <a class="hoverimage" href="/bgstrategy/minion/?id=444">Disguised Graverobber</a> provides a unique way to alter and buff your warband. Prime Mate also helps in almost all of the builds.
 </p>
 <div class="comp_wrapper">
     <h3 id="knights">Deathrattle Knight Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
@@ -25,7 +25,7 @@
 
     <h3 id="moroes">Deathrattle Moroes Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Moroes', 'Kel', 'Mummifier', 'Summoner', 'Summoner', 'Titus', 'Stomper'];
+    $board   = ['Moroes', 'Kel', 'Mummifier', 'Summoner', 'Summoner', 'Titus', 'Acabra'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
@@ -35,7 +35,7 @@
 
     <h3 id="deathrattle">Deathrattle Stomper Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Mummifier', 'Handless', 'Handless', 'Summoner', 'Summoner', 'Champion of the Primus', 'Stomper'];
+    $board   = ['Mummifier', 'Handless', 'Handless', 'Summoner', 'Summoner', 'Champion of the Primus', 'Acabra'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
