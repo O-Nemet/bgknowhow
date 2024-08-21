c563f5d8dd879ec64ee4cfde9f488f795f4668d7
O-Nemet <github@nemet.online>
2024-08-22 01:38:19 +0200

    Updated hero portrait pictures Added duos armor to hero armor list page

--- bgbasics/armor.php
+++ bgbasics/armor.php
@@ -10,7 +10,7 @@ if (!empty($_GET['mode'])) {
 
 <h2 class="page_title">Hero Armor</h2>
 <p>
-    This is a list of all active heroes ordered by their current armor. Notice that the pictures display the armor value for high MMR (~6000 or more), you can check the armor value for low MMR (~6000 or lower) by hovering over or by clicking on a hero portrait:
+    This is a list of all active heroes ordered by their current armor. Notice that the pictures display the armor value for high MMR (~6000 or more), you can check the armor value for low MMR (~6000 or lower) and Duos mode by hovering over or by clicking on a hero portrait:
 </p>
 <br><br>
 <div id="hero_armor_tiers">
@@ -22,7 +22,7 @@ if (!empty($_GET['mode'])) {
             if ($mode === 'txt') {
                 echo "<div><a class='hoverimage' href='" . $hero->websites->bgknowhow . "'>" . $hero->name . "</a></div>";
             } else {
-                echo "<div><a href='" . $hero->websites->bgknowhow . "'><img src='" . $hero->picturePortrait . "' title='" . htmlspecialchars($hero->name, ENT_QUOTES, 'utf-8') . " has " . ($hero->armorHighMMR ?? $hero->armor) . " armor (high MMR) or " . ($hero->armor ?? $hero->armorHighMMR) . " armor (low MMR)' alt='" . htmlspecialchars($hero->name, ENT_QUOTES, 'utf-8') . " has " . ($hero->armorHighMMR ?? $hero->armor) . " armor (high MMR) or " . ($hero->armor ?? $hero->armorHighMMR) . " armor (low MMR)'></img></a></div>";
+                echo "<div><a href='" . $hero->websites->bgknowhow . "'><img src='" . $hero->picturePortrait . "' title='" . htmlspecialchars($hero->name, ENT_QUOTES, 'utf-8') . " has " . ($hero->armorHighMMR ?? $hero->armor) . " armor (high MMR), " . ($hero->armor ?? $hero->armorHighMMR) . " armor (low MMR) or  " . $hero->armorDuos . " armor (Duos)' alt='" . htmlspecialchars($hero->name, ENT_QUOTES, 'utf-8') . " has " . ($hero->armorHighMMR ?? $hero->armor) . " armor (high MMR), " . ($hero->armor ?? $hero->armorHighMMR) . " armor (low MMR) or " . $hero->armorDuos . " armor (Duos) ' ></img></a></div>";
             }
         }
     }
