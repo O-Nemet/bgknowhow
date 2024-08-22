f55c387a10e4016e9ccf7f5fdd44e4997c1d4fdb
mewmatic <72135538+mewmatic@users.noreply.github.com>
2024-08-22 00:20:02 -0700

    Update comp_mechs.php

--- bgcomps/comp_mechs.php
+++ bgcomps/comp_mechs.php
@@ -17,19 +17,17 @@
 <div class="comp_wrapper">
     <h3 id="deflecto">Deflecto Reset Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Deflecto', 'Deflecto', 'Puller', 'Puller', 'Horse', 'Horse', 'Czarina'];
+    $board   = ['Deflecto', 'Deflecto', 'Automaton', 'Automaton', 'Kangor/s', 'Kangor/s', 'Greasebot'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        When going for the <strong>Deflect-o-Bot</strong> mech strategy, the important aspect is to have your other mechs get destroyed so <strong>Deflect-o-Bot</strong> can regain its Divine Shield. This allows your Deflectos to deliver free hits without taking damage, a unique strategy that only Mechs and Quilboars have. The overall strength of your comp will improve by how much you will be able to buff your <strong>Deflect-o-Bots</strong> by utilizing magnetic minions, gems or similar means, during the tavern phase. Which units you pick for the resets is of secondary importance.
-        <br><br>
-        Eventually you want to get cards like <a class="hoverimage" href="/bgstrategy/minion/?id=62">Replicating Menace</a> so minions that do not have Deathrattle or Reborn such as Scrap Scraper can also provide a reset. <a class="hoverimage" href="/bgstrategy/minion/?id=73">Annoy-o-Module</a> is also important so <strong>Deflect-o-Bot</strong> can be protected by redirecting their attacks to the minions you want. Having Tier-6 minions is not that important as you can find multiple minions to reset <strong>Deflect-o-Bot</strong>. But you are offset by the lack of scaling in this build. Eventually you will want to transition into one of the following compositions.
+        When going for the <strong>Deflect-o-Bot</strong> mech strategy, the important aspect is to have your other mechs get destroyed so <strong>Deflect-o-Bot</strong> can regain its Divine Shield. This allows your Deflectos to deliver free hits without taking damage, a unique strategy that only Mechs and Quilboars have. The overall strength of your comp will improve by how much you will be able to buff your <strong>Deflect-o-Bots</strong> by utilizing magnetic minions, gems or similar means, during the tavern phase. Which units you pick for the resets is of secondary importance. Eventually you will want to transition into one of the following compositions.
     </p>
 
     <h3 id="magnetize">Magnetization Beatboxer Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Deflecto', 'Scraper', 'Drone', 'Beatboxer', 'Beatboxer', 'Mk II', 'Scraper'];
+    $board   = ['Deflecto', 'Scraper', 'Drone', 'Beatboxer', 'Beatboxer', 'Jaraxxus', 'Rylak'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
@@ -41,7 +39,7 @@
 
     <h3 id="shield">Shield Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Lasso', 'Lasso', 'Deflecto', 'Deflecto', 'Puller', 'Czarina', 'Czarina'];
+    $board   = ['Lasso', 'Lasso', 'Deflecto', 'Deflecto', 'Czarina', 'Czarina', 'Greasebot'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
@@ -49,6 +47,27 @@
         The key with this comp is obviously to buy the cheapest spells you can. Land Lubber can help with that, but <strong>Lasso</strong> definitely makes it much easier. If you are missing any of these cards, <a class="hoverimage" href="/bgstrategy/minion/?id=133">Reaper</a>, <a class="hoverimage" href="/bgstrategy/minion/?id=353">Beatboxer</a>, <a class="hoverimage" href="/bgstrategy/minion/?id=481">Untameabull</a> can all be good cards as long as you can apply Divine Shield on them. I would not recommend tripling any of these cards other than <strong>Charging Czarina</strong>.
     </p>
 
+    <h3 id="shield">Kaboom Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <?php
+    $board   = ['Macaw', 'Kaboom', 'Kaboom', 'Kangor/s', 'Kangor/s', 'Titus', 'Titus'];
+    $minions = getMinionsForBoard($board);
+    drawBoard($minions);
+    ?>
+    <p>
+        This build NEEDS you to have the Kaboom Bot Portrait Trinket.
+    </p>
+
+ <h3 id="shield">Ancestral Automaton Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <?php
+    $board   = ['Leroy', 'Automaton', 'Automaton', 'Kangor/s', 'Kangor/s', 'Titus', 'Titus'];
+    $minions = getMinionsForBoard($board);
+    drawBoard($minions);
+    ?>
+    <p>
+        Think of this build similarly to Eternal Summoner and Eternal Knights, except they get stronger for the amount of times they get summoned. So cards like Mecha Jaraxxus' Baltharak and Prosthetic Hand helps resummon them. Fortunately, the returning Kangor's Apprentice also resummons a board full with a combination of Titus. Leroy can be any card, but preferably a card that isn't a Mech so Kangor's can't resummon it.
+
+        Trinkets that would go well with this include Automaton Portrait, Rivendare Portrait, Twin Sky Lanterns, Boom Controller, Mechagon Adaptor, and Karazhan Chess Set.
+    </p>
 </div>
 
 <br><br>
