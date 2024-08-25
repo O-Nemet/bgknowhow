d4ae5703a76382226d98c5e821e80ffd43cb8e0c
Oliver Nemet <103574689+O-Nemet@users.noreply.github.com>
2024-08-26 00:28:34 +0200

    Minor fixes

--- bgcomps/comp_demons.php
+++ bgcomps/comp_demons.php
@@ -37,7 +37,7 @@
     ?>
     <p>
         This is what Felbat build should normally look like as you start, but you can always change the cards depending on your situation. Demons are interchangeable, but these are more ideal than others. <a class="hoverimage" href="/bgstrategy/minion/?id=476">Slimy Felblood</a> and <a class="hoverimage" href="/bgstrategy/minion/?id=557">Mirage Conjurer</a> are cards I would recycle so you can keep finding cards that buff the shop.
-
+        <br><br>
         Darnassus Pie, Dalaran Cheese Wheel, Feblood Portrait, Nether Pendant, Bronze Timepiece, Glowing Gauntlet, Felbat Portrait, and Nomi Sticker are ideal trinkets.
     </p>
     <h3 id="felbateot">Felbat End of Turn Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
@@ -47,19 +47,19 @@
     drawBoard($minions);
     ?>
     <p>
-        Some players end up finding two Felbat's. What they don't realize is, having multiple Felbats mean you need less demons. You're going to eat at least 4 minions just by having two of them. So you only need one more demon. If you triple the Felbat, then start getting other Demons and <a class="hoverimage" href="/bgstrategy/minion/?id=345">Drakkari Enchanter</a>.
-
+        Some players end up finding two Felbats. What they don't realize is, having multiple Felbats mean you need less demons. You're going to eat at least 4 minions just by having two of them. So you only need one more demon. If you triple the Felbat, then start getting other Demons and <a class="hoverimage" href="/bgstrategy/minion/?id=345">Drakkari Enchanter</a>.
+        <br><br>
         Darnassus Pie, Dalaran Cheese Wheel, Feblood Portrait, Nether Pendant, Bronze Timepiece, Glowing Gauntlet, Felbat Portrait, and Nomi Sticker are ideal trinkets.
     </p>
     <h3 id="felbateot">Insatiable Ur'zul Self Damage Comp<a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Ur/zul', ''Ur/zul', 'Pilgrimp', 'Pilgrimp', 'Jaraxxus', 'Murk-Eye', 'Jaraxxus'];
+    $board   = ['Ur\'zul', ''Ur\'zul', 'Pilgrimp', 'Pilgrimp', 'Jaraxxus', 'Murk-Eye', 'Jaraxxus'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
         Ur'zul now has a few ways to gain stats fast without needing to sell minions. Mecha Jaraxxus allows you to attach magnetics to your demons, while letting Ur'zul to gain buffs. Pilgrimp is just another option to get free Demons. The only major issue is that Pilgrimp costs health, so you may want <a class="hoverimage" href="/bgstrategy/minion/?id=310">Soul Rewinder</a>. Unfortunately, Rewinder will be useless in combat. Ideally, you want to remove taunt on Ur'zul with <a class="hoverimage" href="/bgstrategy/spell/?id=16">Tricky Trousers</a>. 
-
+        <br><br>
         Darnassus Pie, Dalaran Cheese Wheel, Feblood Portrait, Nether Pendant, Bronze Timepiece, Glowing Gauntlet, Felbat Portrait, and Nomi Sticker are ideal trinkets.
     </p>
 </div>
