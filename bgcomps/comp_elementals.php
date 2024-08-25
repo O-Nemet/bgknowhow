45dd4ea06ad01b9826d8eefa33dbc4325346ac8b
mewmatic <72135538+mewmatic@users.noreply.github.com>
2024-08-25 12:55:46 -0700

    Update comp_elementals.php

--- bgcomps/comp_elementals.php
+++ bgcomps/comp_elementals.php
@@ -15,24 +15,28 @@
 <div class="comp_wrapper">
     <h3 id="rockrock">RockRock Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Wildfire', 'Wraith', 'Magmaloc', 'Magmaloc', 'Rag', 'Rock Rock', 'Rock Rock'];
+    $board   = ['Frostling', 'Wraith', 'Moss', 'Bedrock', 'Rag', 'Rock Rock', 'Rock Rock'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        An alternate way for you to scale in combination with <strong>Rock Rock</strong> is to use <a class='hoverimage' href='/bgstrategy/minion/?id=473'>Greymane's Champion</a>. You can then purchase cards such as <a class='hoverimage' href='/bgstrategy/minion/?id=14'>Sellemental</a>, <a class='hoverimage' href='/bgstrategy/minion/?id=125'>Tavern Tempest</a>, and <a class='hoverimage' href='/bgstrategy/minion/?id=247'>Patient Scout</a>. This however means you need to replace cards like Wildfire Elemental and Gentle Djinni with Tier 2, Tier 4, or Tier 6 Elementals. Remember, this build does not necessarily need to be completely Elementals. <strong>Rock Rock</strong> buffs regardless, so if you want to have a cleave minion such as <a class="hoverimage" href="/bgstrategy/minion/?id=327">Blade Collector</a> or <a class="hoverimage" href="/bgstrategy/minion/?id=133">Foe Reaper 4000</a> then you very well can.
+        Remember, this build does not necessarily need to be completely Elementals. <strong>Rock Rock</strong> buffs regardless, so if you want to have a cleave minion such as <a class="hoverimage" href="/bgstrategy/minion/?id=327">Blade Collector</a> or <a class="hoverimage" href="/bgstrategy/minion/?id=133">Foe Reaper 4000</a> then you very well can.
+
+        Lava Lamp, Primordial Terrarium, Kodo Leather Pouch, Jarred Frostling, Essence of Dreams, Shaman Prayer Beads, Karazhan Chest Set are ideal trinkets.
     </p>
 
     <h3 id="spells">Spell Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Fiery', 'Lubber', 'Lubber', 'Azerite', 'Azerite', 'Shelly', 'Matriarch'];
+    $board   = ['Fiery', 'Lubber', 'Lubber', 'Azerite', 'Azerite', 'Glass', 'Glass'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        The way for this build to scale high is to create triples. Once you make huge stats with <strong>Living Azerite</strong> and the spells used, you need to start replacing weaker minions with the stronger ones in the shop. Then you need to triple the cards so that you have a card with three times the amount of scaling you provided through Living Azerite. You got to pay attention when it's time to start throwing away like <strong>Living Azerite</strong> or <strong>Land Lubber</strong> because over time they start to feel weaker due to lack of onboard scaling. You can replace them with more <strong>Shellemental</strong>s, for example.
+        The way for this build to scale high is to create triples. Once you make huge stats with <strong>Living Azerite</strong> and the spells used, you need to start replacing weaker minions with the stronger ones in the shop. Then you need to triple the cards so that you have a card with three times the amount of scaling you provided through Living Azerite. You got to pay attention when it's time to start throwing away like <strong>Living Azerite</strong> or <strong>Land Lubber</strong> because over time they start to feel weaker due to lack of onboard scaling.
         <br><br>
         This is a build that does not need you to be at Tier 5, but there are plenty of Tier 5 minions that help the build. <a class="hoverimage" href="/bgstrategy/minion/?id=476">Slimy Felblood</a> also is quite useful to use.
+
+        Demonblood Gourd, Darnassus Pie, Dalaran Cheese Wheel, Felblood Portrait, Nomi Sticker, Azerite Portrait, Alliance Keychain, Karazhan Chess Set, Devourer Sticker, Training Certificate, are ideal trinkets.
     </p>
 
 </div>
