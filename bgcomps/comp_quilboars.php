435a8fc2d434f46a5a4a20a5cd7fca88555edd17
mewmatic <72135538+mewmatic@users.noreply.github.com>
2024-08-28 02:36:10 -0700

    Updated Quilboars comps after patch 30.2.0
    * Update comp_quilboars.php
    
    * Small fixes after update
    
    ---------
    
    Co-authored-by: Oliver Nemet <103574689+O-Nemet@users.noreply.github.com>

--- bgcomps/comp_quilboars.php
+++ bgcomps/comp_quilboars.php
@@ -23,21 +23,25 @@
     drawBoard($minions);
     ?>
     <p>
-        Any Quilboar that generates Blood Gems is good, so don't think this is the only way to build this comp. You can also have a <a class="hoverimage" href="/bgstrategy/minion/?id=319">Rylak Metalhead</a> be paired next to <a class="hoverimage" href="/bgstrategy/minion/?id=321">Moon-Bacon Jazzer</a> and a golden <a class="hoverimage" href="/bgstrategy/minion/?id=8">Razorfen Geomancer</a>. You can have <a class="hoverimage" href="/bgstrategy/minion/?id=376">Briarback Bookie</a> or use <a class="hoverimage" href="/bgstrategy/minion/?id=45">Bannerboar</a> as well. Reminder, Pokey Thornmantle does NOT get stronger from Drakkari Enchanter's effect. Feel free to replace this card with something else after he has buffed your Blood Gems to a desirable number.
+        Any Quilboar that generates Blood Gems is good, so don't think this is the only way to build this comp. You can also have a <a class="hoverimage" href="/bgstrategy/minion/?id=319">Rylak Metalhead</a> be paired next to <a class="hoverimage" href="/bgstrategy/minion/?id=321">Moon-Bacon Jazzer</a> and a golden <a class="hoverimage" href="/bgstrategy/minion/?id=8">Razorfen Geomancer</a>. You can have <a class="hoverimage" href="/bgstrategy/minion/?id=569">Foodie</a> or use <a class="hoverimage" href="/bgstrategy/minion/?id=45">Bannerboar</a> as well. Reminder, Pokey Thornmantle does NOT get stronger from Drakkari Enchanter's effect. Feel free to replace this card with something else after he has buffed your Blood Gems to a desirable number.
         <br><br>
         The only issue with this build is, that the targets for all the gems are suboptimal. So do try to move your gems over to a minion with Cleave or Divine Shield by using the spell <a class="hoverimage" href="/bgstrategy/spell/?id=55">Gem Confiscation</a>.
+        <br><br>
+        Lorewalker Scroll, Hoggy Bank, Great Boar Sticker, Jar o'Gems, Blood Golem Sticker, and Conductor Portrait are ideal trinkets for this build.
     </p>
 
     <h3 id="bristlebach">Bristlebach Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Tusk', 'Tusk', 'TLQ', 'Handless', 'Handless', 'TLQ', 'Bristlebach'];
+    $board   = ['Tusk', 'Tusk', 'TLQ', 'Recruiter', 'Recruiter', 'TLQ', 'Bristlebach'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        This is supposed to use <strong>Bristlebach</strong>'s ability to refresh the Divine Shield of the <strong>Tough Tusks</strong>. The Handless can basically be any minion which summons another minion via Deathrattle or Reborn.
+        This is supposed to use <strong>Bristlebach</strong>'s ability to refresh the Divine Shield of the <strong>Tough Tusks</strong>. The Recruiters can basically be any minion which summons another minion via Deathrattle or Reborn.
         <br><br>
         The stat gains from <strong>Bristlebach</strong>'s Avenge ability are only temporary buffs during the combat phase. Make sure your <strong>Tusks</strong> have Divine Shield prior to battle!
+        <br><br>
+        Lorewalker Scroll, Hoggy Bank, Great Boar Sticker, Jar o'Gems, Blood Golem Sticker, and Quilligraphy Set are ideal trinkets for this build.
     </p>
 
     <h3 id="charly">End of Turn Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
@@ -48,6 +52,8 @@
     ?>
     <p>
         The key minions for this build are <strong>Charlga</strong> and <strong>Drakkari Enchanter</strong>. Heavily depends on buffing the size of your gems before concluding on this board. You can use Titus Rivendare with Prickly Piper, and buy multiple Moon-Bacon Jazzers to help with the scaling.
+        <br><br>
+        Lorewalker Scroll, Hoggy Bank, Great Boar Sticker, Jar o'Gems, Blood Golem Sticker, and Conductor Portrait are ideal trinkets for this build.
     </p>
 
     <h3 id="deathrattle">Rylak Battlecry Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
@@ -57,11 +63,11 @@
     drawBoard($minions);
     ?>
     <p>
-        Needs the availability of Beasts. This is a usual setup for you to scale your Blood Gems to high stats. Sadly, this build is quite weak until you actually get Blood Gems. So be sure to know how to find ways to generate them, after you have done enough scaling via <strong>Jazzer</strong> and/or <strong>Piper</strong>. Heavily improves by reborning the <strong>Rylak</strong> by using <a class="hoverimage" href="/bgstrategy/minion/?id=420">Sprightly Scarab</a>.
+        Needs the availability of Beasts. This is a usual setup for you to scale your Blood Gems to high stats. Sadly, this build is quite weak until you actually get Charlga or <a class="hoverimage" href="/bgstrategy/minion/?id=298">Gem Smuggler</a>. So be sure to know how to find ways to generate them, after you have done enough scaling via <strong>Jazzer</strong> and/or <strong>Piper</strong>. Heavily improves by reborning the <strong>Rylak</strong> by using <a class="hoverimage" href="/bgstrategy/minion/?id=420">Sprightly Scarab</a>.
         <br><br>
         Instead of Brann you can also play a <a class="hoverimage" href="/bgstrategy/minion/?id=60">Monstrous Macaw</a> (on position #1 of course).
-        <br><br>
-        Once you get a Charlga or <a class='hoverimage' href='/bgstrategy/minion/?id=348'>Bongo Bopper</a>, you can scale the entire build and replace one of these cards with Drakkari Enchanter. If you can only get individual Blood Gems, put them onto strong minions with Divine Shield or Cleave. <strong>Rylak Metalhead</strong> can be replaced by <a class='hoverimage' href='/bgstrategy/minion/?id=209'>Young Murk-Eye</a> (if Murlocs are available) but isn't as effective. Once you have the Blood Gems buffed, you can use <a class='hoverimage' href='/bgstrategy/minion/?id=545'>Three Lil' Quilboar</a> with Titus Rivendare for big stats on your Quilboar.
+        <br><br>      
+        Lorewalker Scroll, Hoggy Bank, Great Boar Sticker, Jar o'Gems, Blood Golem Sticker, Essence of Dreams, and Conductor Portrait are ideal trinkets for this build.
     </p>
 </div>
 
