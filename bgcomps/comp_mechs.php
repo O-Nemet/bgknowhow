e8848f06f93da3f83bb87052709514eee215d371
mewmatic <72135538+mewmatic@users.noreply.github.com>
2024-12-08 14:25:15 -0800

    Update comp_mechs.php

--- bgcomps/comp_mechs.php
+++ bgcomps/comp_mechs.php
@@ -15,59 +15,65 @@
     <!--    Mechs also got a new mechanic added through the 6 Zilliax cards available on Tier 2. Cards such as <a class="hoverimage" href="/bgstrategy/minion/?id=504">Zilliax: Defense Module</a>, <a class="hoverimage" href="/bgstrategy/minion/?id=503">Zilliax: Crystal Module</a>, and <a class="hoverimage" href="/bgstrategy/minion/?id=506">Zilliax: Whirring Module</a> can add incredibly strong keywords to other Mechs. However, you need to triple them with <a class="hoverimage" href="/bgstrategy/minion/?id=353">Zilliax: Bonding Module</a> to make sure you can put it on other Mechs.-->
 </p>
 <div class="comp_wrapper">
-    <h3 id="deflecto">Deflecto Reset Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="deflecto">Magnetic Mech <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Deflecto', 'Deflecto', 'Automaton', 'Automaton', 'Kangor\'s', 'Kangor\'s', 'Greasebot'];
+    $board   = ['Holo Rover', 'Holo Rover', 'Holy Mecherel', 'Holy Mecherel', 'Mechagnome Interpreter', 'Mechagnome Interpreter', 'Moonsteel Juggernaut'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        When going for the <strong>Deflect-o-Bot</strong> mech strategy, the important aspect is to have your other mechs get destroyed so <strong>Deflect-o-Bot</strong> can regain its Divine Shield. This allows your Deflectos to deliver free hits without taking damage, a unique strategy that only Mechs and Quilboars have. The overall strength of your comp will improve by how much you will be able to buff your <strong>Deflect-o-Bots</strong> by utilizing magnetic minions, gems or similar means, during the tavern phase. Which units you pick for the resets is of secondary importance. Eventually you will want to transition into one of the following compositions.
+        Once you get to Tier-6, you should be getting more Dr. Boom's Monster. This will be your main way of buffing up your minions.
     </p>
 
-    <h3 id="magnetize">Magnetization Beatboxer Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="magnetize">Magnetic Summon Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Deflecto', 'Scraper', 'Drone', 'Beatboxer', 'Beatboxer', 'Jaraxxus', 'Rylak'];
+    $board   = ['Holoy Rover', 'Holo Rover', 'Dr. Boom's Monster', 'Dr. Boom's Monster', 'Kangor's Apprentice', 'Kangor's Apprentice', 'Titus'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        Buy as many magnetic minions as you can to profit of your <strong>Beatboxers</strong> and put it on any Mech you plan to keep until the end game. All of them are decent, so don't be picky. The spell <a class="hoverimage" href="/bgstrategy/spell/?id=38">Perfect Vision</a> can buff magnetic minions in the Tavern.
-        <br><br>
-        Magnetization can be given even on full boards. Also, in the past it was possible to run out of Magnetize minions because the Scrapers pulled minions from the shared minion pool. But this was changed in a July 2023 update, now magnetized minions will return to the pool upon usage.
+        The last build focused on buffing your minions with Dr. Boom's Monster. This time, you will taunt your Dr. Boom's Monster and having Kangor's Apprentice summon as much as you can. This will swarm the board with big sized mechs, assuming you have been magnetizing plenty of cards. 
     </p>
 
-    <h3 id="shield">Shield Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="shield">Czarina Spell Comp<a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Lasso', 'Lasso', 'Deflecto', 'Deflecto', 'Czarina', 'Czarina', 'Greasebot'];
+    $board   = ['Deflecto', 'Deflecto', 'Czarina', 'Marquee Ticker', 'Marquee Ticker', 'Holy Mecherel', 'Holy Mecherel'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        The key with this comp is obviously to buy the cheapest spells you can. Land Lubber can help with that, but <strong>Lasso</strong> definitely makes it much easier. If you are missing any of these cards, <a class="hoverimage" href="/bgstrategy/minion/?id=133">Reaper</a>, <a class="hoverimage" href="/bgstrategy/minion/?id=353">Beatboxer</a>, <a class="hoverimage" href="/bgstrategy/minion/?id=481">Untameabull</a> can all be good cards as long as you can apply Divine Shield on them. I would not recommend tripling any of these cards other than <strong>Charging Czarina</strong>.
+        The key with this comp is obviously to buy the cheapest spells you can. You can use Drakkari Enchanter to gain more spells from Marquee Ticker. Also, be sure to attach Annoy-O-Module onto Holy Mecherel.
     </p>
-
-    <h3 id="shield">Kaboom Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="shield">Holy Mecherel Comp<a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Macaw', 'Kaboom', 'Kaboom', 'Kangor\'s', 'Kangor\'s', 'Titus', 'Titus'];
+    $board   = ['Deflect', 'Deflect', 'Grease', 'Grease', 'Czarina', 'Holy Mecherel', 'Holy Mecherel'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        This build NEEDS you to have the <a class="hoverimage" href="/bgstrategy/trinket/?id=21">Kaboom Bot Portrait</a> trinket.
+        You're trying to have your opponent ping-pong between the Holy Mecherel and other Divine Shield minions to continuously buff them from Grease Bot's effect, and to have unlimited Divine Shields. The only problem with this build is how much Leroy can ruin the plan. So be sure to play around that if your opponent has him.
     </p>
 
     <h3 id="shield">Ancestral Automaton Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Leeroy', 'Automaton', 'Automaton', 'Kangor\'s', 'Kangor\'s', 'Titus', 'Titus'];
+    $board   = ['Leeroy', 'Automaton', 'Automaton', 'Kangor\'s', 'Kangor\'s', 'Chameleon', 'Titus'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        Think of this build similarly to Eternal Summoner and Eternal Knights, except they get stronger for the amount of times they get summoned. So cards like Mecha Jaraxxus, Baltharak and Prosthetic Hand helps resummon them. Fortunately, the returning Kangor's Apprentice also resummons a board full with a combination of Titus. Leeroy can be any card, but preferably a card that isn't a Mech so Kangor's can't resummon it.
-        <br><br>
-        Trinkets that would go well with this include <a class="hoverimage" href="/bgstrategy/trinket/?id=22">Automaton Portrait</a>, <a class="hoverimage" href="/bgstrategy/trinket/?id=69">Rivendare Portrait</a>, <a class="hoverimage" href="/bgstrategy/trinket/?id=66">Twin Sky Lanterns</a>, <a class="hoverimage" href="/bgstrategy/trinket/?id=83">Boom Controller</a>, <a class="hoverimage" href="/bgstrategy/trinket/?id=82">Mechagon Adapter</a>, and <a class="hoverimage" href="/bgstrategy/trinket/?id=109">Karazhan Chess Set</a>.
+You're going to try your best to get Magnetic reborn cards to resummon the Automaton's. You do this by finding Mecha Jaraxxus or Prosthetic Hand. While Leroy is here to fill the rest of the build up, by being a universally strong card, Chameleon is here to be a third Kangor's. Eventually, you can replace Leroy with a third Automaton that is golden by using the spell "Eyes of the Earth Mother"    </p>
     </p>
+    
+    <h3 id="shield">End of Turn Mech <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <?php
+    $board   = ['Moonsteel Juggernaut', 'Moonsteel Juggernaut', 'Mecha Jaraxxus', 'Young Murk-Eye', 'Mecha Jaraxxus', 'Brann', 'Drakkari Enchanter'];
+    $minions = getMinionsForBoard($board);
+    drawBoard($minions);
+    ?>
+    <p>
+The goal here is to get as much Magnetic cards as you can from Mecha Jaraxxus and Moonsteel Juggernaut, but also to get triple discovers from them so you can find Dr. Boom's Monster. This will be the main way to buff up your Mech cards.
+    </p>
+    
 </div>
 
 <br><br>
