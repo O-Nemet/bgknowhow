06c66ec8fc1ac410ea3fa6ce79f7694a45cd517e
mewmatic <72135538+mewmatic@users.noreply.github.com>
2024-12-08 15:12:33 -0800

    Update comp_murlocs.php

--- bgcomps/comp_murlocs.php
+++ bgcomps/comp_murlocs.php
@@ -15,34 +15,32 @@
 <div class="comp_wrapper">
     <h3 id="starter">Murloc Starter Kit Composition <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Striker', 'Prince', 'Prince', 'Burglar', 'Gillmother', 'Gillmother', 'Brann'];
+    $board   = ['Striker', 'Prince', 'Prince', 'Cliffdiver', 'Gillmother', 'Gillmother', 'Brann'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        This is basically a composition to start out Murlocs before you start switching the cards into more endgame like the two builds below. You're going to use <strong>Fairy Gillmother</strong> as a way to find and search for not only battlecries for <strong>'Loc Prince</strong>, but Bream Counter. Once you find Bream Counter, then you start finding Bassgill or Diremuck Forager. If you don't get Bream Counter then you use the battlecries gained to buff up your other Murlocs. Eventually, if you can't get them strong enough you get <a class="hoverimage" href="/bgstrategy/minion/?id=343">Operatic Belcher</a> to give them venomous.
+        This is basically a composition to start out Murlocs before you start switching the cards into more endgame like the two builds below.
     </p>
 
     <h3 id="battlecry">Battlecry Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Choral', 'Choral', 'Bassgill', 'Primalfin', 'Murk-Eye', 'Primalfin', 'Brann'];
+    $board   = ['Neon Agent', 'Neon Agent', 'Primalfin', 'Murk-Eye', 'Primalfin', 'Bubble Gunner', 'Brann'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        Buffs the board but can also support one or two <a class="hoverimage" href="/bgstrategy/minion/?id=333">Bream Counter</a> by playing/cycling lots of Murlocs. Keep <strong>Young Murk-Eye</strong> between two battlecries as they stack with <strong>Brann</strong>. Ideally, you want to have the 'handloc' package of <a class="hoverimage" href="/bgstrategy/minion/?id=332">Bassgill</a>, <a class="hoverimage" href="/bgstrategy/minion/?id=354">Choral Mrrrglr</a>, and <a class="hoverimage" href="/bgstrategy/minion/?id=333">Bream Counter</a>.
+        You're going to gain a lot of Battlecries with this build. Eventually, you're going to replace the Primalfin with King Bagurgle or Murky to gain more stat buffs. 
     </p>
 
-    <h3 id="hand">Handloc Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    <h3 id="hand">Keyword Comp <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
-    $board   = ['Belcher', 'Belcher', 'Bassgill', 'Bassgill', 'Choral', 'Choral', 'Titus'];
+    $board   = ['Adaptive Ancestor', 'Belcher', 'Fountain Chiller', 'Murk-Eye', 'Hackerfin', 'Bubble Gunner', 'Brann'];
     $minions = getMinionsForBoard($board);
     drawBoard($minions);
     ?>
     <p>
-        Heavily depends on growing one or two <a class="hoverimage" href="/bgstrategy/minion/?id=333">Bream Counter</a> in your hand or your venom carrying you to victory (don't play this against Undead). <a class="hoverimage" href="/bgstrategy/minion/?id=417">Diremuck Forager</a> is a good alternative to Bassgil. Constantly recycle Murlocs so Bream Counter gets buffed.
-        <br><br>
-        <strong>Titus Rivendare</strong> is very good for synergy with Operatic Belcher and Bassgill.
+        Try to get as many extra keywords as you can on your minions. Even if that includes taunt. This will give you more buffs from Fountain Chiller and Hackerfin. I would also try to get Bubble Gunner to have extra keywords through Murk-Eye to gain more buffs too.
     </p>
 </div>
 
