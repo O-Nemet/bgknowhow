6999291c43f9141128d356ba32d753fb1a6cd1c7
Oliver Nemet <103574689+O-Nemet@users.noreply.github.com>
2024-12-09 01:02:33 +0100

    Fixed quicklink

--- bgcomps/comp_dragons.php
+++ bgcomps/comp_dragons.php
@@ -31,10 +31,10 @@
     drawBoard($minions);
     ?>
     <p>
-        <a class='hoverimage' href='/bgstrategy/minion/?id=101'>Tarecgosa</a> also can be used but isn't completely necessary. Once <strong>Nadina</strong> permanently gives minions Divine Shield, you can start replacing it with other cards. Persistent Poet is not a card I would want to triple either. You want to make sure you can give as many minions as possible the permanent stats. Lastly, it's never bad to have more than one <strong>Nightbane</strong>, so go for that if you're done with Amber Guardian or want to replace Roaring Rallier.
-    
+        <a class='hoverimage' href='/bgstrategy/minion/?id=101'>Tarecgosa</a> also can be used but isn't completely necessary. Once <strong>Nadina</strong> permanently gives minions Divine Shield, you can start replacing it with other cards. Persistent Poet is not a card I would want to triple either. You want to make sure you can give as many minions as possible the permanent stats. Lastly, it's never bad to have more than one <strong>Nightbane</strong>, so go for that if you're done with Amber Guardian or want to replace Roaring Rallier.    
     </p>
-     <h3 id="persistent">End of Turn Dragons <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
+    
+    <h3 id="endofturn">End of Turn Dragons <a tabindex="0" onclick="CopyLink(this);" title="Copy the link to this section to your clipboard"><i class="bi bi-link-45deg"></i></a></h3>
     <?php
     $board   = ['Nadina', 'Razorgore', 'Outback Smolderer', 'Nether Drake', 'Nether Drake', 'Hunter of Gatherers', 'Drakkari Enchanter'];
     $minions = getMinionsForBoard($board);
@@ -43,7 +43,6 @@
     <p>
         Honestly, a slow build but does very decent on the way to end game. Think of this as a sort of starter kit, as Outback Smolderer is used to make triple discovers, and Razorgore will only be strong alone in the mid game. Be sure to use plenty of Efficient Engineers to trigger end of turn effects.
     </p>
-
 
 </div>
 
